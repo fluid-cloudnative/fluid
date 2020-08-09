cc58bd60b1668271483bfd94df7fcd15fc247f38
cheyang <cheyang@163.com>
2020-08-09 08:16:03 +0800

    detect the levels automatically, to #29680554

--- pkg/utils/tieredstore/tiered_store.go
+++ pkg/utils/tieredstore/tiered_store.go
@@ -16,10 +16,43 @@ limitations under the License.
 package tieredstore
 
 import (
+	"sort"
+
 	datav1alpha1 "github.com/cloudnativefluid/fluid/api/v1alpha1"
 	"github.com/cloudnativefluid/fluid/pkg/common"
+	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/api/resource"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
+
+var log logr.Logger
+
+func init() {
+	log = ctrl.Log.WithName("tieredstore")
+}
+
+type sortMediumType []common.MediumType
+
+func (s sortMediumType) Len() int      { return len(s) }
+func (s sortMediumType) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s sortMediumType) Less(i, j int) bool {
+	return common.GetDefaultTieredStoreOrder(s[i]) < common.GetDefaultTieredStoreOrder(s[j])
+
+}
+
+func makeMediumTypeSorted(mediumTypes []common.MediumType) []common.MediumType {
+	newMediumTypes := make(sortMediumType, 0, len(mediumTypes))
+	knownMediumTypes := map[common.MediumType]bool{}
+	for _, c := range mediumTypes {
+		if _, found := knownMediumTypes[c]; found {
+			continue
+		}
+		newMediumTypes = append(newMediumTypes, c)
+		knownMediumTypes[c] = true
+	}
+	sort.Sort(newMediumTypes)
+	return []common.MediumType(newMediumTypes)
+}
 
 // GetLevelStorageMap gets the level storage map
 func GetLevelStorageMap(runtime *datav1alpha1.AlluxioRuntime) (storage map[common.CacheStoreType]*resource.Quantity) {
@@ -41,4 +74,23 @@ func GetLevelStorageMap(runtime *datav1alpha1.AlluxioRuntime) (storage map[commo
 
 	return storage
 
+}
+
+// GetTieredLevel returns index
+func GetTieredLevel(runtime *datav1alpha1.AlluxioRuntime, mediumType common.MediumType) int {
+	levels := []common.MediumType{}
+	for _, level := range runtime.Spec.Tieredstore.Levels {
+		levels = append(levels, level.MediumType)
+	}
+
+	log.Info("GetTieredLevel", "levels", levels)
+	orderedLevels := makeMediumTypeSorted(levels)
+	log.Info("GetTieredLevel", "orderedLevels", orderedLevels)
+	for index, value := range orderedLevels {
+		if value == mediumType {
+			return index
+		}
+	}
+
+	return -1
 }
