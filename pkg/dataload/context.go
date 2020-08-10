aaf0a93d85342502ddd60c8164ec9a7071fb2905
Trafalgarzzz <trafalgarz@outlook.com>
2020-08-10 23:16:44 +0800

    Add human-readable event records

--- pkg/dataload/context.go
+++ pkg/dataload/context.go
@@ -5,12 +5,14 @@ import (
 	"github.com/cloudnativefluid/fluid/api/v1alpha1"
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/tools/record"
 )
 
 // Context for processing requests of dataload reconciling
 type ReconcileRequestContext struct {
 	context.Context
-	Log      logr.Logger
-	DataLoad v1alpha1.AlluxioDataLoad
+	Log logr.Logger
 	types.NamespacedName
+	Recorder record.EventRecorder
+	DataLoad v1alpha1.AlluxioDataLoad
 }
