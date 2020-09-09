297367d4966a472089a7aa0cce96f80dcee99c1b
biubiu-biub <616119430@qq.com>
2020-09-09 16:02:33 +0800

    add test for du

--- pkg/ddc/alluxio/operations/base_test.go
+++ pkg/ddc/alluxio/operations/base_test.go
@@ -27,6 +27,17 @@ import (
 	"testing"
 )
 
+const (
+	NOT_EXIST      = "not-exist"
+	OTHER_ERR      = "other-err"
+	FINE           = "fine"
+	EXEC_ERR       = "exec-err"
+	TOO_MANY_LINES = "too many lines"
+	DATA_NUM       = "data nums not match"
+	PARSE_ERR      = "parse err"
+)
+
+// a empty logger just for testing ...
 type NullLogger struct{}
 
 func (_ NullLogger) Info(_ string, _ ...interface{}) {
@@ -77,53 +88,84 @@ func (log NullLogger) WithValues(_ ...interface{}) logr.Logger {
 //imeplement nulllogger to bypass go vet check
 
 func TestAlluxioFileUtils_IsExist(t *testing.T) {
-	mockExecTramp := func(p1, p2, p3 string, p4 []string) (stdout string, stderr string, e error) {
-		t.Fatal("done")
-		if strings.Contains(p4[3], "not-exist") {
+
+
+	mockExec := func(p1, p2, p3 string, p4 []string) (stdout string, stderr string, e error) {
+
+		if strings.Contains(p4[3], NOT_EXIST) {
 			return "does not exist", "", errors.New("does not exist")
-		} else if strings.Contains(p4[3], "other-expectedErr") {
-			return "other error", "other error", errors.New("other error")
+
+		} else if strings.Contains(p4[3], OTHER_ERR) {
+			return "", "", errors.New("other error")
 		} else {
 			return "", "", nil
 		}
 	}
 
-	mockExec := func(p1, p2, p3 string, p4 []string) (stdout string, stderr string, e error) {
-
-		if strings.Contains(p4[3], "not-exist") {
-			return "does not exist", "", errors.New("does not exist")
-		} else if strings.Contains(p4[3], "other-expectedErr") {
-			return "other error", "other error", errors.New("other error")
-		} else {
-			return "ok", "ok", nil
-		}
-	}
-
-	err := gohook.Hook(kubeclient.ExecCommandInContainer, mockExec, mockExecTramp)
+	err := gohook.Hook(kubeclient.ExecCommandInContainer, mockExec, nil)
 	if err != nil {
 		t.Fatal(err.Error())
 	}
 
 	var tests = []struct {
-		in          string
-		out         bool
-		expectedErr error
+		in    string
+		out   bool
+		noErr bool
 	}{
-		{"not-exist", false, nil},
-		{"other-expectedErr", false, errors.New("error")},
-		{"fine", true, nil},
+		{NOT_EXIST, false, true},
+		{OTHER_ERR, false, false},
+		{FINE, true, true},
 	}
 	for _, test := range tests {
 		found, err := AlluxioFileUtils{log: NullLogger{}}.IsExist(test.in)
-
 		if found != test.out {
 			t.Errorf("input parameter is %s,expected %t, got %t", test.in, test.out, found)
 		}
-		if test.expectedErr == nil && err != nil {
-			t.Errorf("input parameter is %s,and expectedErr should be nil", test.in)
+		var noErr bool = (err == nil)
+		if test.noErr != noErr {
+			t.Errorf("input parameter is %s,expected noerr is %t", test.in, test.noErr)
+		}
+	}
+}
 
-			if test.expectedErr != nil && err == nil {
-				t.Error("wrong")
+func TestAlluxioFileUtils_Du(t *testing.T) {
+	mockExec := func(p1, p2, p3 string, p4 []string) (stdout string, stderr string, e error) {
+
+		if strings.Contains(p4[4], EXEC_ERR) {
+			return "does not exist", "", errors.New("exec-error")
+		} else if strings.Contains(p4[4], TOO_MANY_LINES) {
+			return "1\n2\n3\n4\n", "1\n2\n3\n4\n", nil
+		} else if strings.Contains(p4[4], DATA_NUM) {
+			return "1\n2\t3", "1\n2\t3", nil
+		} else if strings.Contains(p4[4], PARSE_ERR) {
+			return "1\n1\tdududu\tbbb\t", "1\n1\t2\tbbb\t", nil
+		} else {
+			return "first line!\n111\t222\t(%233)\t2333", "first line!\n111\t222\t(666)", nil
+		}
+	}
+
+	gohook.Hook(kubeclient.ExecCommandInContainer, mockExec, nil)
+	var tests = []struct {
+		in         string
+		out1, out2 int64
+		out3       string
+		noErr      bool
+	}{
+		{EXEC_ERR, 0, 0, "", false},
+		{TOO_MANY_LINES, 0, 0, "", false},
+		{DATA_NUM, 0, 0, "", false},
+		{PARSE_ERR, 0, 0, "", false},
+		{FINE, 111, 222, "%233", true},
+	}
+	for _, test := range tests {
+		o1, o2, o3, err := AlluxioFileUtils{log: NullLogger{}}.Du(test.in)
+		var noErr bool = (err == nil)
+		if test.noErr != noErr {
+			t.Errorf("input parameter is %s,expected noerr is %t", test.in, test.noErr)
+		}
+		if test.noErr {
+			if o1 != test.out1 || o2 != test.out2 || o3 != test.out3 {
+				t.Fatalf("input parameter is %s,output is %d,%d, %s", test.in, o1, o2, o3)
 			}
 		}
 	}
