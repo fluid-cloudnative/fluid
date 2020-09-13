1c57a1684f080952542a37adf8363af4826e2cbf
biubiu-biub <616119430@qq.com>
2020-09-13 15:22:43 +0800

    unhook exec after tests to facilitate following tests

--- pkg/ddc/alluxio/operations/base_test.go
+++ pkg/ddc/alluxio/operations/base_test.go
@@ -16,9 +16,14 @@ limitations under the License.
 package operations
 
 import (
+	"errors"
+	"fmt"
+	"github.com/brahma-adshonor/gohook"
+	"github.com/fluid-cloudnative/fluid/pkg/utils/kubeclient"
 	"github.com/go-logr/logr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"strings"
 	"testing"
 )
 
@@ -83,3 +88,139 @@ func (log NullLogger) WithValues(_ ...interface{}) logr.Logger {
 
 //imeplement nulllogger to bypass go vet check
 
+func TestAlluxioFileUtils_IsExist(t *testing.T) {
+
+	mockExec := func(p1, p2, p3 string, p4 []string) (stdout string, stderr string, e error) {
+
+		if strings.Contains(p4[3], NOT_EXIST) {
+			return "does not exist", "", errors.New("does not exist")
+
+		} else if strings.Contains(p4[3], OTHER_ERR) {
+			return "", "", errors.New("other error")
+		} else {
+			return "", "", nil
+		}
+	}
+
+	err := gohook.Hook(kubeclient.ExecCommandInContainer, mockExec, nil)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	defer gohook.UnHook(kubeclient.ExecCommandInContainer)
+
+	var tests = []struct {
+		in    string
+		out   bool
+		noErr bool
+	}{
+		{NOT_EXIST, false, true},
+		{OTHER_ERR, false, false},
+		{FINE, true, true},
+	}
+	for _, test := range tests {
+		found, err := AlluxioFileUtils{log: NullLogger{}}.IsExist(test.in)
+		if found != test.out {
+			t.Errorf("input parameter is %s,expected %t, got %t", test.in, test.out, found)
+		}
+		var noErr bool = (err == nil)
+		if test.noErr != noErr {
+			t.Errorf("input parameter is %s,expected noerr is %t", test.in, test.noErr)
+		}
+	}
+}
+
+func TestAlluxioFileUtils_Du(t *testing.T) {
+	out1, out2, out3 := 111, 222, "%233"
+	mockExec := func(p1, p2, p3 string, p4 []string) (stdout string, stderr string, e error) {
+
+		if strings.Contains(p4[4], EXEC_ERR) {
+			return "does not exist", "", errors.New("exec-error")
+		} else if strings.Contains(p4[4], TOO_MANY_LINES) {
+			return "1\n2\n3\n4\n", "1\n2\n3\n4\n", nil
+		} else if strings.Contains(p4[4], DATA_NUM) {
+			return "1\n2\t3", "1\n2\t3", nil
+		} else if strings.Contains(p4[4], PARSE_ERR) {
+			return "1\n1\tdududu\tbbb\t", "1\n1\t2\tbbb\t", nil
+		} else {
+			return fmt.Sprintf("first line!\n%d\t%d\t(%s)\t2333", out1, out2, out3), "", nil
+		}
+	}
+
+	err := gohook.HookByIndirectJmp(kubeclient.ExecCommandInContainer, mockExec, nil)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	defer gohook.UnHook(kubeclient.ExecCommandInContainer)
+
+	var tests = []struct {
+		in         string
+		out1, out2 int64
+		out3       string
+		noErr      bool
+	}{
+		{EXEC_ERR, 0, 0, "", false},
+		{TOO_MANY_LINES, 0, 0, "", false},
+		{DATA_NUM, 0, 0, "", false},
+		{PARSE_ERR, 0, 0, "", false},
+		{FINE, int64(out1), int64(out2), out3, true},
+	}
+	for _, test := range tests {
+		o1, o2, o3, err := AlluxioFileUtils{log: NullLogger{}}.Du(test.in)
+		var noErr bool = (err == nil)
+		if test.noErr != noErr {
+			t.Errorf("input parameter is %s,expected noerr is %t", test.in, test.noErr)
+		}
+		if test.noErr {
+			if o1 != test.out1 || o2 != test.out2 || o3 != test.out3 {
+				t.Fatalf("input parameter is %s,output is %d,%d, %s", test.in, o1, o2, o3)
+			}
+		}
+	}
+}
+
+func TestAlluxioFileUtils_Count(t *testing.T) {
+	out1, out2, out3 := 111, 222, 333
+	mockExec := func(p1, p2, p3 string, p4 []string) (stdout string, stderr string, e error) {
+
+		if strings.Contains(p4[3], EXEC_ERR) {
+			return "does not exist", "", errors.New("exec-error")
+		} else if strings.Contains(p4[3], TOO_MANY_LINES) {
+			return "1\n2\n3\n4\n", "1\n2\n3\n4\n", nil
+		} else if strings.Contains(p4[3], DATA_NUM) {
+			return "1\n2\t3", "1\n2\t3", nil
+		} else if strings.Contains(p4[3], PARSE_ERR) {
+			return "1\n1\tdududu\tbbb\t", "1\n1\t2\tbbb\t", nil
+		} else {
+			return fmt.Sprintf("first line!\n%d\t%d\t%d", out1, out2, out3), "", nil
+		}
+	}
+
+	err := gohook.HookByIndirectJmp(kubeclient.ExecCommandInContainer, mockExec, nil)
+	defer gohook.UnHook(kubeclient.ExecCommandInContainer)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	var tests = []struct {
+		in               string
+		out1, out2, out3 int64
+		noErr            bool
+	}{
+		{EXEC_ERR, 0, 0, 0, false},
+		{TOO_MANY_LINES, 0, 0, 0, false},
+		{DATA_NUM, 0, 0, 0, false},
+		{PARSE_ERR, 0, 0, 0, false},
+		{FINE, int64(out1), int64(out2), int64(out3), true},
+	}
+	for _, test := range tests {
+		o1, o2, o3, err := AlluxioFileUtils{log: NullLogger{}}.Count(test.in)
+		var noErr bool = (err == nil)
+		if test.noErr != noErr {
+			t.Errorf("input parameter is %s,expected noerr is %t", test.in, test.noErr)
+		}
+		if test.noErr {
+			if o1 != test.out1 || o2 != test.out2 || o3 != test.out3 {
+				t.Fatalf("input parameter is %s,output is %d,%d, %d", test.in, o1, o2, o3)
+			}
+		}
+	}
+}
