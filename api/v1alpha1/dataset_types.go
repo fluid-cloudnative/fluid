00f103b6ac9cb393c53e5bb6fd77ab9d54f785a5
cheyang <cheyang@163.com>
2020-10-23 23:59:47 +0800

    Support HCFS, Fix #30651485

--- api/v1alpha1/dataset_types.go
+++ api/v1alpha1/dataset_types.go
@@ -173,6 +173,7 @@ type DatasetCondition struct {
 // +kubebuilder:printcolumn:name="Cache Capacity",type="string",JSONPath=`.status.cacheStates.cacheCapacity`
 // +kubebuilder:printcolumn:name="Cached Percentage",type="string",JSONPath=`.status.cacheStates.cachedPercentage`
 // +kubebuilder:printcolumn:name="Phase",type="string",JSONPath=`.status.phase`
+// +kubebuilder:printcolumn:name="HCFS URL",type="string",JSONPath=`.status.hcfs.endpoint`,priority=10
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=`.metadata.creationTimestamp`
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
