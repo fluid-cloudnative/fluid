931312f554ed770fe06769273c382c3dab2ba81d
Bernchess <53571342+Bernchess@users.noreply.github.com>
2020-10-27 20:46:59 +0800

    Update crtl_utils.go
    Revise the comment.

--- pkg/utils/crtl_utils.go
+++ pkg/utils/crtl_utils.go
@@ -51,7 +51,7 @@ func RequeueIfError(err error) (ctrl.Result, error) {
 	return ctrl.Result{}, err
 }
 
-// RequeueImmediatelyUnlessGenerationChanged which requeues immediately if the object generation has not changed.
+// RequeueImmediatelyUnlessGenerationChanged requeues immediately if the object generation has not changed.
 // Otherwise, since the generation change will trigger an immediate update anyways, this will not requeue.
 // This prevents some cases where two reconciliation loops will occur.
 func RequeueImmediatelyUnlessGenerationChanged(prevGeneration, curGeneration int64) (ctrl.Result, error) {
