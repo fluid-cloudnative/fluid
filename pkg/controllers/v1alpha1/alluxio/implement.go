b29c1dc22ce00120658c57ab330d3e7f9150ffeb
cheyang <cheyang@163.com>
2020-11-25 21:35:36 +0800

    Fix engine destroys and updates image (#400)
    * Make engine id common, to #31235919
    
    * Make engine id common, to #31235919
    
    * Make engine id common, to #31235919
    
    * Make engine id common, to #31235919

--- pkg/controllers/v1alpha1/alluxio/implement.go
+++ pkg/controllers/v1alpha1/alluxio/implement.go
@@ -16,8 +16,6 @@ limitations under the License.
 package alluxio
 
 import (
-	"fmt"
-
 	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
 	cruntime "github.com/fluid-cloudnative/fluid/pkg/runtime"
 
@@ -43,9 +41,7 @@ func (r *RuntimeReconciler) getRuntime(ctx cruntime.ReconcileRequestContext) (*d
 func (r *RuntimeReconciler) GetOrCreateEngine(
 	ctx cruntime.ReconcileRequestContext) (engine base.Engine, err error) {
 	found := false
-	id := fmt.Sprintf("%s-%s",
-		ctx.NamespacedName.Namespace,
-		ctx.NamespacedName.Name)
+	id := ddc.GenerateEngineID(ctx.NamespacedName)
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
 	if engine, found = r.engines[id]; !found {
@@ -67,8 +63,6 @@ func (r *RuntimeReconciler) GetOrCreateEngine(
 func (r *RuntimeReconciler) RemoveEngine(ctx cruntime.ReconcileRequestContext) {
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
-	id := fmt.Sprintf("%s-%s",
-		ctx.NamespacedName.Namespace,
-		ctx.NamespacedName.Name)
+	id := ddc.GenerateEngineID(ctx.NamespacedName)
 	delete(r.engines, id)
 }
