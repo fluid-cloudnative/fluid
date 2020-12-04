9508bf613aa163573a1563538617be1a60689b2f
cheyang <cheyang@163.com>
2020-12-04 17:43:52 +0800

    Fix static check issue (#440)
    * Make path spec common, To #30961382
    
    Signed-off-by: cheyang <cheyang@163.com>
    
    * Update docker images, To #30961382
    
    Signed-off-by: cheyang <cheyang@163.com>

--- pkg/utils/kubeclient/pod.go
+++ pkg/utils/kubeclient/pod.go
@@ -18,15 +18,14 @@ package kubeclient
 import (
 	"context"
 
-	corev1 "k8s.io/api/core/v1"
-	v1 "k8s.io/api/core/v1"
+	"k8s.io/api/core/v1"
 	apierrs "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // IsCompletePod determines if the pod is complete
-func IsCompletePod(pod *corev1.Pod) bool {
+func IsCompletePod(pod *v1.Pod) bool {
 	if pod == nil {
 		return false
 	}
@@ -35,25 +34,25 @@ func IsCompletePod(pod *corev1.Pod) bool {
 		return true
 	}
 
-	if pod.Status.Phase == corev1.PodSucceeded || pod.Status.Phase == corev1.PodFailed {
+	if pod.Status.Phase == v1.PodSucceeded || pod.Status.Phase == v1.PodFailed {
 		return true
 	}
 	return false
 }
 
 // IsFailedPod determines if the pod is failed
-func IsFailedPod(pod *corev1.Pod) bool {
-	return pod != nil && pod.Status.Phase == corev1.PodFailed
+func IsFailedPod(pod *v1.Pod) bool {
+	return pod != nil && pod.Status.Phase == v1.PodFailed
 }
 
 // GetPodByName gets pod with given name and namespace of the pod.
-func GetPodByName(client client.Client, name, namespace string) (pod *corev1.Pod, err error) {
+func GetPodByName(client client.Client, name, namespace string) (pod *v1.Pod, err error) {
 	key := types.NamespacedName{
 		Name:      name,
 		Namespace: namespace,
 	}
 
-	pod = &corev1.Pod{}
+	pod = &v1.Pod{}
 
 	if err = client.Get(context.TODO(), key, pod); err != nil {
 		if apierrs.IsNotFound(err) {
@@ -67,7 +66,7 @@ func GetPodByName(client client.Client, name, namespace string) (pod *corev1.Pod
 }
 
 //DeletePod deletes the given pod if it exists
-func DeletePod(client client.Client, pod *corev1.Pod) error {
+func DeletePod(client client.Client, pod *v1.Pod) error {
 	err := client.Delete(context.TODO(), pod)
 	if apierrs.IsNotFound(err) {
 		err = nil
