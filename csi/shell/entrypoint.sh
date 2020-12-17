2d0c6430e2927f70a21f1e6288b919ff159e8fc8
汪洋 <yang.wang01@luckincoffee.com>
2020-12-17 15:59:05 +0800

    add env KUBELET_ROOTDIR in csi container

--- csi/shell/entrypoint.sh
+++ csi/shell/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -xe
 
-rm -f /var/lib/kubelet/csi-plugins/fuse.csi.fluid.io/csi.sock
-mkdir -p /var/lib/kubelet/csi-plugins/fuse.csi.fluid.io
+rm -f $KUBELET_ROOTDIR/csi-plugins/fuse.csi.fluid.io/csi.sock
+mkdir -p $KUBELET_ROOTDIR/csi-plugins/fuse.csi.fluid.io
 
 fluid-csi start $@
