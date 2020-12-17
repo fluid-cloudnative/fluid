4447de13b02f8abfb386fe8e9baaf4fd7ce8d670
XIAO-HOU <48202473+XIAO-HOU@users.noreply.github.com>
2020-12-17 23:30:20 +0800

    Support secrets for dataset (#480)
    * support secrets for dataset
    
    * support secrets for dataset(0)
    
    * add comments
    
    * use my own secret struct
    
    * not use pointer
    
    * test string
    
    * change the port
    
    * do not need to decrpyt data
    
    * update the comments and change the port of dataset
    
    * update crd
    
    * standardized annotation

---
+++ pkg/utils/secret.go
@@ -0,0 +1,38 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package utils
+
+import (
+	"context"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// GetSecret gets the secret.
+// It returns a pointer to the secret if successful.
+func GetSecret(client client.Client, name, namespace string) (*v1.Secret, error) {
+
+	key := types.NamespacedName{
+		Name:      name,
+		Namespace: namespace,
+	}
+	var secret v1.Secret
+	if err := client.Get(context.TODO(), key, &secret); err != nil {
+		return nil, err
+	}
+	return &secret, nil
+}
