0f2b5ff98e37539790702302b0bcbeb9ed786952
wangtao <frank.wt@alibaba-inc.com>
2020-12-22 14:08:27 +0800

    remove unused code

--- pkg/ddc/jindo/cache.go
+++ pkg/ddc/jindo/cache.go
@@ -8,28 +8,6 @@ import (
 
 // queryCacheStatus checks the cache status
 func (e *JindoEngine) queryCacheStatus() (states cacheStates, err error) {
-	/*summary, err := e.reportSummary()
-	if err != nil {
-		e.Log.Error(err, "Failed to get jindofs summary when query cache status")
-		return states, err
-	}
-	strs := strings.Split(summary, "\n")
-	for _, str := range strs {
-		str = strings.TrimSpace(str)
-		if strings.HasPrefix(str, SUMMARY_PREFIX_TOTAL_CAPACITY) {
-			totalCacheCapacityAlluxio, _ := utils.FromHumanSize(strings.TrimPrefix(str, SUMMARY_PREFIX_TOTAL_CAPACITY))
-			// Convert Alluxio's binary byte units to Fluid's binary byte units
-			// e.g. 10KB -> 10KiB, 2GB -> 2GiB
-			states.cacheCapacity = utils.BytesSize(float64(totalCacheCapacityAlluxio))
-		}
-		if strings.HasPrefix(str, SUMMARY_PREFIX_USED_CAPACITY) {
-			usedCacheCapacityAlluxio, _ := utils.FromHumanSize(strings.TrimPrefix(str, SUMMARY_PREFIX_USED_CAPACITY))
-			// Convert Alluxio's binary byte units to Fluid's binary byte units
-			// e.g. 10KB -> 10KiB, 2GB -> 2GiB
-			states.cached = utils.BytesSize(float64(usedCacheCapacityAlluxio))
-		}
-	}*/
-
 	dataset, err := utils.GetDataset(e.Client, e.name, e.namespace)
 	if err != nil {
 		e.Log.Info("Failed to get dataset when query cache status")
