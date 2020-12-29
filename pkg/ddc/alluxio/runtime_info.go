0fdbe5b3ddd20cd1728ce5e35b60cb71c5a229af
cheyang <cheyang@163.com>
2020-12-29 16:26:50 +0800

    remove exlusive from engine, To #30961382
    Signed-off-by: cheyang <cheyang@163.com>

--- pkg/ddc/alluxio/runtime_info.go
+++ pkg/ddc/alluxio/runtime_info.go
@@ -15,7 +15,10 @@ limitations under the License.
 
 package alluxio
 
-import "github.com/fluid-cloudnative/fluid/pkg/ddc/base"
+import (
+	"github.com/fluid-cloudnative/fluid/pkg/ddc/base"
+	"github.com/fluid-cloudnative/fluid/pkg/utils"
+)
 
 // getRuntimeInfo gets runtime info
 func (e *AlluxioEngine) getRuntimeInfo() (base.RuntimeInfoInterface, error) {
@@ -27,5 +30,14 @@ func (e *AlluxioEngine) getRuntimeInfo() (base.RuntimeInfoInterface, error) {
 
 		e.runtimeInfo = base.BuildRuntimeInfo(e.name, e.namespace, e.runtimeType, runtime.Spec.Tieredstore)
 	}
+
+	dataset, err := utils.GetDataset(e.Client, e.name, e.namespace)
+	if err != nil {
+		e.Log.Info("Failed to get dataset when getruntimeInfo")
+		return e.runtimeInfo, err
+	}
+
+	e.runtimeInfo.SetupWithDataset(dataset)
+
 	return e.runtimeInfo, nil
 }
