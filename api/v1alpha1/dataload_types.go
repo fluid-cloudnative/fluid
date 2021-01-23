e795f32702315727912daa5a40dd827ed7dac19a
houhaojun <48202473+XIAO-HOU@users.noreply.github.com>
2021-01-23 15:28:31 +0800

    Show dataload duration time (#540)
    * add duration time for dataload
    
    * add duration time for dataload
    
    * delete omitempty of durationTime
    
    * change inital state ro Unfinished
    
    * update crd
    
    * update crd completely
    
    * change endTimeStamp

--- api/v1alpha1/dataload_types.go
+++ api/v1alpha1/dataload_types.go
@@ -79,11 +79,15 @@ type DataLoadStatus struct {
 
 	// Conditions consists of transition information on DataLoad's Phase
 	Conditions []DataLoadCondition `json:"conditions"`
+
+	// DurationTime describes the duration time of the dataload process.
+	DurationTime string `json:"durationTime"`
 }
 
 // +kubebuilder:printcolumn:name="Dataset",type="string",JSONPath=`.spec.dataset.name`
 // +kubebuilder:printcolumn:name="Phase",type="string",JSONPath=`.status.phase`
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=`.metadata.creationTimestamp`
+// +kubebuilder:printcolumn:name="Duration",type="string",JSONPath=`.status.durationTime`
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 // +genclient
