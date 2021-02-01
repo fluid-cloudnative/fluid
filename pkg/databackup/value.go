8f086f0d2098c561da86b86464f9df0a0bff8912
cheyang <cheyang@163.com>
2021-02-01 23:42:18 +0800

    Build docker image with latest code (#557)
    * go fmt
    
    Signed-off-by: cheyang <cheyang@163.com>
    
    * build docker image
    
    Signed-off-by: cheyang <cheyang@163.com>

--- pkg/databackup/value.go
+++ pkg/databackup/value.go
@@ -7,13 +7,13 @@ type DataBackupValue struct {
 
 // DataBackup defines values used in DataBackup helm chart
 type DataBackup struct {
-	Namespace  string `yaml:"namespace,omitempty"`
-	Dataset    string `yaml:"dataset,omitempty"`
-	Name       string `yaml:"name,omitempty"`
-	NodeName   string `yaml:"nodeName,omitempty"`
-	Image      string `yaml:"image,omitempty"`
-	JavaEnv    string `yaml:"javaEnv,omitempty"`
-	Workdir    string `yaml:"workdir,omitempty"`
-	PVCName    string `yaml:"pvcName,omitempty"`
-	Path       string `yaml:"path,omitempty"`
+	Namespace string `yaml:"namespace,omitempty"`
+	Dataset   string `yaml:"dataset,omitempty"`
+	Name      string `yaml:"name,omitempty"`
+	NodeName  string `yaml:"nodeName,omitempty"`
+	Image     string `yaml:"image,omitempty"`
+	JavaEnv   string `yaml:"javaEnv,omitempty"`
+	Workdir   string `yaml:"workdir,omitempty"`
+	PVCName   string `yaml:"pvcName,omitempty"`
+	Path      string `yaml:"path,omitempty"`
 }
