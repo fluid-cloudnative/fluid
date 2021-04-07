7fcb27cf18d114a2fac94215ab8c1e4dad1751f6
cheyang <cheyang@163.com>
2021-04-07 10:45:01 +0800

    update conduct of code
    Signed-off-by: cheyang <cheyang@163.com>

--- pkg/ddc/alluxio/transform_api_gateway.go
+++ pkg/ddc/alluxio/transform_api_gateway.go
@@ -1,17 +1,15 @@
 package alluxio
 
 import (
-	"strconv"
-
 	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
 )
 
 // transformAPIGateway transforms the given value
 func (e *AlluxioEngine) transformAPIGateway(runtime *datav1alpha1.AlluxioRuntime, value *Alluxio) (err error) {
 
-	if runtime.Spec.APIGateway.Enabled {
-		setDefaultProperties(runtime, value, "alluxio.proxy.web.port", strconv.Itoa(value.APIGateway.Ports.Rest))
-	}
+	// if runtime.Spec.APIGateway.Enabled {
+	// 	setDefaultProperties(runtime, value, "alluxio.proxy.web.port", strconv.Itoa(value.APIGateway.Ports.Rest))
+	// }
 
 	value.APIGateway.Enabled = runtime.Spec.APIGateway.Enabled
 	return nil
