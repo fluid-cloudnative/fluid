96d8cf1c6818abf97bc5df07da32a2e768fe4f26
TzZtzt <trafalgarz@outlook.com>
2021-04-08 21:37:21 +0800

    Remove deprecated AlluxioDataLoad CRD (#694)
    * Remove deprecated DataLoad CRD
    
    * Remove files related to deprecated DataLoad CRD

--- charts/fluid-dataloader/docker/loader/read_file.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-echo THREADS=$THREADS
-echo DATAPATH=$DATA_PATH
-echo python multithread_read_benchmark.py --threads=$THREADS --path=$DATA_PATH
-python multithread_read_benchmark.py --threads=$THREADS --path=$DATA_PATH
