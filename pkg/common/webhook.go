753b4406d5135842fff18e40d1977d2876b72601
yangyuliufeng <30432274+yangyuliufeng@users.noreply.github.com>
2021-05-16 03:50:28 -0700

    add mutate webhook of pod (#725)
    * add mutate webhook of pod
    
    * change the schedule to plugins
    
    * fix ci bug
    
    * every schedule plugin has the access to edit pod

---
+++ pkg/common/webhook.go
@@ -0,0 +1,10 @@
+package common
+
+const (
+	WEBHOOK_NAME         = "fluid-pod-admission-webhook"
+	WEBHOOK_NAMESPACE    = "fluid-system"
+	WEBHOOK_SERVICE_NAME = "fluid-pod-admission-webhook"
+	WEBHOOK_SERVICE_PATH = "/mutate-v1-pod"
+
+	CERT_PATH = "/opt/certs"
+)
