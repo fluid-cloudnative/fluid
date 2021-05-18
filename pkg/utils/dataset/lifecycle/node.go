a70ee7aa8b6756481cfdc3939c10d4aac089c8f2
TrafalgarZZZ <trafalgarz@outlook.com>
2021-05-18 13:37:37 +0800

    infinite wait loop of polling

--- pkg/utils/dataset/lifecycle/node.go
+++ pkg/utils/dataset/lifecycle/node.go
@@ -182,21 +182,29 @@ func LabelCacheNode(nodeToLabel v1.Node, runtimeInfo base.RuntimeInfoInterface, 
 		return err
 	}
 
-	// Wait at most 30s for cache in controller-runtime successfully catching up with api-server
+	// Wait infinitely with 30-second loops for cache in controller-runtime successfully catching up with api-server
 	// This is to ensure the controller can get up-to-date cluster status during the following scheduling
 	// loop.
 	pollStartTime := time.Now()
-	if err := wait.Poll(1*time.Second, 30*time.Second, func() (done bool, err error) {
-		node, err := kubeclient.GetNode(client, nodeName)
-		if err != nil {
-			return false, fmt.Errorf("failed to get node: %w", err)
+	for i := 1; ; i++ {
+		if err := wait.Poll(1*time.Second, 30*time.Second, func() (done bool, err error) {
+			node, err := kubeclient.GetNode(client, nodeName)
+			if err != nil {
+				return false, fmt.Errorf("failed to get node: %w", err)
+			}
+			return utils.ContainsAll(node.Labels, updatedLabels), nil
+		}); err == nil {
+			break
 		}
-		return utils.ContainsAll(node.Labels, updatedLabels), nil
-	}); err != nil {
+		// if timeout, retry infinitely
+		if err == wait.ErrWaitTimeout {
+			log.Error(err, fmt.Sprintf("client cache can't catch up with api-server after %v secs", i*30), "nodeName", nodeName)
+			continue
+		}
 		log.Error(err, "wait polling in LabelCacheNode")
-	} else {
-		utils.TimeTrack(pollStartTime, "polling up-to-date cache status when scheduling", "nodeToLabel", nodeToLabel.Name)
+		return err
 	}
+	utils.TimeTrack(pollStartTime, "polling up-to-date cache status when scheduling", "nodeToLabel", nodeToLabel.Name)
 
 	return nil
 }
