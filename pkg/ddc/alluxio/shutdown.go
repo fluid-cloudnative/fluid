004b972f33974dcb4b7f78af608ee200d6bcd36e
littletiger123 <mg20330006@smail.nju.edu.cn>
2021-05-29 22:52:21 +0800

    update the name
    Signed-off-by: littletiger123 <mg20330006@smail.nju.edu.cn>

--- pkg/ddc/alluxio/shutdown.go
+++ pkg/ddc/alluxio/shutdown.go
@@ -286,7 +286,7 @@ func (e *AlluxioEngine) destroyWorkers(expectedWorkers int32) (currentWorkers in
 				labelNames = append(labelNames, labelExclusiveName)
 			}
 
-			isDeleted, err := lifecycle.DatasetNumUpdateOrDelete(toUpdate, runtimeInfo)
+			isDeleted, err := lifecycle.UpdateOrDeleteDatasetNum(toUpdate, runtimeInfo)
 			if err != nil {
 				return err
 			}
