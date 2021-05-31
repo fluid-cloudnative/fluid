e7f9e0431dfaed5468fec6f65ecc24eb49b722e2
littletiger123 <mg20330006@smail.nju.edu.cn>
2021-05-31 09:15:00 +0800

    add the test for UpdateOrDeleteDatasetNum
    Signed-off-by: littletiger123 <mg20330006@smail.nju.edu.cn>

---
+++ pkg/utils/dataset/lifecycle/node_test.go
@@ -0,0 +1,39 @@
+package lifecycle
+
+import (
+	"github.com/fluid-cloudnative/fluid/pkg/ddc/base"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"testing"
+)
+
+func TestUpdateOrDeleteDatasetNum(t *testing.T) {
+	var testCase = []struct {
+		node           *v1.Node
+		runtimeInfo    base.RuntimeInfo
+		expectedResult bool
+	}{
+		{
+			node: &v1.Node{
+				ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"fluid.io/dataset-num": "2"}},
+				Spec:       v1.NodeSpec{},
+			},
+			runtimeInfo: base.RuntimeInfo{},
+			expectedResult: false,
+		},
+		{
+			node: &v1.Node{
+				ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"fluid.io/dataset-num": "1"}},
+				Spec:       v1.NodeSpec{},
+			},
+			runtimeInfo: base.RuntimeInfo{},
+			expectedResult: true,
+		},
+	}
+
+	for _, test := range testCase {
+		if result, _ := UpdateOrDeleteDatasetNum(test.node, &test.runtimeInfo); result != test.expectedResult {
+			t.Errorf("expected %v, got %v", test.expectedResult, result)
+		}
+	}
+}
