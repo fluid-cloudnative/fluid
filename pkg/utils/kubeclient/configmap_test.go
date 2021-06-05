55596c3faae4060fbaa2cb1ded5ac3368d9e1c81
cheyang <cheyang@163.com>
2021-06-05 12:40:18 +0800

    Add ut for configmap, To #34515520
    Signed-off-by: cheyang <cheyang@163.com>

--- pkg/utils/kubeclient/configmap_test.go
+++ pkg/utils/kubeclient/configmap_test.go
@@ -77,3 +77,57 @@ func TestIsConfigMapExist(t *testing.T) {
 	}
 
 }
+
+func TestGetConfigmapByName(t *testing.T) {
+
+	namespace := "default"
+	testConfigMapInputs := []*v1.ConfigMap{{
+		ObjectMeta: metav1.ObjectMeta{Name: "test1",
+			Namespace: namespace},
+	}, {
+		ObjectMeta: metav1.ObjectMeta{Name: "test2"},
+	}}
+
+	testConfigMaps := []runtime.Object{}
+
+	for _, ns := range testConfigMapInputs {
+		testConfigMaps = append(testConfigMaps, ns.DeepCopy())
+	}
+
+	client := fake.NewFakeClientWithScheme(testScheme, testConfigMaps...)
+
+	type args struct {
+		name      string
+		namespace string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "ConfigMap doesn't exist",
+			args: args{
+				name:      "notExist",
+				namespace: namespace,
+			},
+			want: false,
+		},
+		{
+			name: "ConfigMap exists",
+			args: args{
+				name:      "test1",
+				namespace: namespace,
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if _, err := GetConfigmapByName(client, tt.args.name, tt.args.namespace); err != nil {
+				t.Errorf("testcase %v GetConfigmapByName()'s err is %v", tt.name, err)
+			}
+		})
+	}
+
+}
