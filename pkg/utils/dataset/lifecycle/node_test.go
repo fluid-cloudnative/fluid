b288f1d760c79fd8673f8e18bc05c8dff7cc88e9
littletiger123 <32997459+littletiger123@users.noreply.github.com>
2021-06-08 14:00:21 +0800

    Add unit test (#856)
    * add unit test
    
    Signed-off-by: littletiger123 <mg20330006@smail.nju.edu.cn>
    
    * modify the for clouse
    
    Signed-off-by: littletiger123 <mg20330006@smail.nju.edu.cn>
    
    * add the unit test
    
    Signed-off-by: littletiger123 <mg20330006@smail.nju.edu.cn>

--- pkg/utils/dataset/lifecycle/node_test.go
+++ pkg/utils/dataset/lifecycle/node_test.go
@@ -1,13 +1,146 @@
 package lifecycle
 
 import (
+	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
+	"github.com/fluid-cloudnative/fluid/pkg/common"
 	"github.com/fluid-cloudnative/fluid/pkg/ddc/base"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"testing"
 )
 
-func TestUpdateOrDeleteDatasetNum(t *testing.T) {
+func TestAlreadyAssigned(t *testing.T) {
+	runtimeInfoNotExclusive, err := base.BuildRuntimeInfo("hbase", "fluid", "alluxio", datav1alpha1.Tieredstore{})
+	if err != nil {
+		t.Errorf("fail to create the runtimeInfo with error %v", err)
+	}
+	runtimeInfoNotExclusive.SetupWithDataset(&datav1alpha1.Dataset{
+		Spec: datav1alpha1.DatasetSpec{PlacementMode: datav1alpha1.ShareMode},
+	})
+
+	runtimeInfoExclusive, err := base.BuildRuntimeInfo("hbase", "fluid", "alluxio", datav1alpha1.Tieredstore{})
+	if err != nil {
+		t.Errorf("fail to create the runtimeInfo with error %v", err)
+	}
+	runtimeInfoExclusive.SetupWithDataset(&datav1alpha1.Dataset{
+		Spec: datav1alpha1.DatasetSpec{PlacementMode: datav1alpha1.ExclusiveMode},
+	})
+
+	var testCase = []struct {
+		runtimeInfo base.RuntimeInfoInterface
+		node        v1.Node
+		want        bool
+	}{
+		{
+			runtimeInfo: runtimeInfoNotExclusive,
+			node: v1.Node{
+				ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"fluid.io/s-fluid-hbase": "true"}},
+				Spec:       v1.NodeSpec{},
+			},
+			want: true,
+		},
+		{
+			runtimeInfo: runtimeInfoNotExclusive,
+			node: v1.Node{
+				ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"fluid.io/s-fluid-spark": "true"}},
+				Spec:       v1.NodeSpec{},
+			},
+			want: false,
+		},
+		{
+			runtimeInfo: runtimeInfoExclusive,
+			node: v1.Node{
+				ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"fluid.io/s-fluid-hbase": "true"}},
+				Spec:       v1.NodeSpec{},
+			},
+			want: true,
+		},
+		{
+			runtimeInfo: runtimeInfoExclusive,
+			node: v1.Node{
+				ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"fluid.io/s-fluid-spark": "true"}},
+				Spec:       v1.NodeSpec{},
+			},
+			want: true,
+		},
+	}
+
+	for _, test := range testCase {
+		if result := AlreadyAssigned(test.runtimeInfo, test.node); result != test.want {
+			t.Errorf("expected %v, got %v", test.want, result)
+		}
+	}
+}
+
+func TestCanbeAssigned(t *testing.T) {
+	tireStore := datav1alpha1.Tieredstore{
+		Levels: []datav1alpha1.Level{
+			{
+				MediumType: common.Memory,
+				Quota:  resource.NewQuantity(2,resource.BinarySI),
+			},
+		},
+	}
+	runtimeInfoNotExclusive, err := base.BuildRuntimeInfo("hbase", "default", "alluxio", tireStore)
+	if err != nil {
+		t.Errorf("fail to create the runtimeInfo with error %v", err)
+	}
+	runtimeInfoNotExclusive.SetupWithDataset(&datav1alpha1.Dataset{
+		Spec: datav1alpha1.DatasetSpec{PlacementMode: datav1alpha1.ShareMode},
+	})
+
+	var testCase = []struct {
+		runtimeInfo base.RuntimeInfoInterface
+		node        v1.Node
+		want        bool
+	}{
+		{
+			runtimeInfo: runtimeInfoNotExclusive,
+			node: v1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{"fluid_exclusive": "default_hbase"},
+				},
+				Status: v1.NodeStatus{},
+			},
+			want: false,
+		},
+		{
+			runtimeInfo: runtimeInfoNotExclusive,
+			node: v1.Node{
+				ObjectMeta: metav1.ObjectMeta{},
+				Spec: v1.NodeSpec{},
+				Status: v1.NodeStatus{
+					Allocatable: v1.ResourceList{
+						v1.ResourceMemory: *resource.NewQuantity(3,resource.BinarySI),
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			runtimeInfo: runtimeInfoNotExclusive,
+			node: v1.Node{
+				ObjectMeta: metav1.ObjectMeta{},
+				Spec: v1.NodeSpec{},
+				Status: v1.NodeStatus{
+					Allocatable: v1.ResourceList{
+						v1.ResourceMemory: *resource.NewQuantity(1,resource.BinarySI),
+					},
+				},
+			},
+			want: false,
+		},
+	}
+
+	for _, test := range testCase {
+		if result := CanbeAssigned(test.runtimeInfo, test.node); result != test.want {
+			t.Errorf("expected %v, got %v", test.want, result)
+		}
+	}
+}
+
+func TestDecreaseDatasetNum(t *testing.T) {
 	var testCase = []struct {
 		node           *v1.Node
 		runtimeInfo    base.RuntimeInfo
