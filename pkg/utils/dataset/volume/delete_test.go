1163967dc43c747447293a58863e3656ff69686b
littletiger123 <mg20330006@smail.nju.edu.cn>
2021-06-15 10:10:27 +0800

    add ut for delete and deprecated
    Signed-off-by: littletiger123 <mg20330006@smail.nju.edu.cn>

---
+++ pkg/utils/dataset/volume/delete_test.go
@@ -0,0 +1,137 @@
+package volume
+
+import (
+	"context"
+	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
+	"github.com/fluid-cloudnative/fluid/pkg/ddc/base"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"reflect"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"testing"
+)
+
+func TestDeleteFusePersistentVolume(t *testing.T) {
+	runtimeInfoHbase, err := base.BuildRuntimeInfo("hbase", "fluid", "alluxio", datav1alpha1.Tieredstore{})
+	if err != nil {
+		t.Errorf("fail to create the runtimeInfo with error %v", err)
+	}
+
+	runtimeInfoHadoop, err := base.BuildRuntimeInfo("hadoop", "fluid", "alluxio", datav1alpha1.Tieredstore{})
+	if err != nil {
+		t.Errorf("fail to create the runtimeInfo with error %v", err)
+	}
+
+	testPVInputs := []*v1.PersistentVolume{{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "fluid-hbase",
+			Annotations: map[string]string{
+				"CreatedBy": "fluid",
+			},
+		},
+		Spec: v1.PersistentVolumeSpec{},
+	}}
+
+	testPVs := []runtime.Object{}
+	for _, pvInput := range testPVInputs {
+		testPVs = append(testPVs, pvInput.DeepCopy())
+	}
+	client := fake.NewFakeClientWithScheme(testScheme, testPVs...)
+	var testCase = []struct {
+		runtimeInfo    base.RuntimeInfoInterface
+		expectedResult v1.PersistentVolume
+	}{
+		{
+			runtimeInfo: runtimeInfoHadoop,
+			expectedResult: v1.PersistentVolume{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "PersistentVolume",
+					APIVersion: "v1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "fluid-hbase",
+					Annotations: map[string]string{
+						"CreatedBy": "fluid",
+					},
+				},
+				Spec: v1.PersistentVolumeSpec{},
+			},
+		},
+		{
+			runtimeInfo:    runtimeInfoHbase,
+			expectedResult: v1.PersistentVolume{},
+		},
+	}
+	for _, test := range testCase {
+		var log = ctrl.Log.WithName("delete")
+		_ = DeleteFusePersistentVolume(client, test.runtimeInfo, log)
+
+		key := types.NamespacedName{
+			Name: "fluid-hbase",
+		}
+		pv := &v1.PersistentVolume{}
+		_ = client.Get(context.TODO(), key, pv)
+		if !reflect.DeepEqual(test.expectedResult, *pv) {
+			t.Errorf("fail to exec the function with the error")
+		}
+	}
+}
+
+func TestDeleteFusePersistentVolumeIfExists(t *testing.T) {
+	testPVInputs := []*v1.PersistentVolume{{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "hbase",
+			Annotations: map[string]string{
+				"CreatedBy": "fluid",
+			},
+		},
+		Spec: v1.PersistentVolumeSpec{},
+	}}
+
+	testPVs := []runtime.Object{}
+	for _, pvInput := range testPVInputs {
+		testPVs = append(testPVs, pvInput.DeepCopy())
+	}
+	client := fake.NewFakeClientWithScheme(testScheme, testPVs...)
+	var testCase = []struct {
+		pvName         string
+		expectedResult v1.PersistentVolume
+	}{
+		{
+			pvName: "hadoop",
+			expectedResult: v1.PersistentVolume{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "PersistentVolume",
+					APIVersion: "v1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "hbase",
+					Annotations: map[string]string{
+						"CreatedBy": "fluid",
+					},
+				},
+				Spec: v1.PersistentVolumeSpec{},
+			},
+		},
+		{
+			pvName:         "hbase",
+			expectedResult: v1.PersistentVolume{},
+		},
+	}
+	for _, test := range testCase {
+		var log = ctrl.Log.WithName("delete")
+		_ = deleteFusePersistentVolumeIfExists(client, test.pvName, log)
+
+		key := types.NamespacedName{
+			Name: "hbase",
+		}
+		pv := &v1.PersistentVolume{}
+		_ = client.Get(context.TODO(), key, pv)
+		if !reflect.DeepEqual(test.expectedResult, *pv) {
+			t.Errorf("fail to exec the function with the error")
+		}
+	}
+}
---
+++ pkg/utils/dataset/volume/deprecated_test.go
@@ -0,0 +1,69 @@
+package volume
+
+import (
+	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
+	"github.com/fluid-cloudnative/fluid/pkg/ddc/base"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"testing"
+)
+
+var (
+	testScheme *runtime.Scheme
+)
+
+func init() {
+	testScheme = runtime.NewScheme()
+	_ = v1.AddToScheme(testScheme)
+}
+
+func TestHasDeprecatedPersistentVolumeName(t *testing.T) {
+	testPVInputs := []*v1.PersistentVolume{{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "hbase",
+			Annotations: map[string]string{
+				"CreatedBy": "fluid",
+			},
+		},
+		Spec: v1.PersistentVolumeSpec{},
+	}}
+
+	runtimeInfoSpark, err := base.BuildRuntimeInfo("spark", "fluid", "alluxio", datav1alpha1.Tieredstore{})
+	if err != nil {
+		t.Errorf("fail to create the runtimeInfo with error %v", err)
+	}
+
+	runtimeInfoHbase, err := base.BuildRuntimeInfo("hbase", "fluid", "alluxio", datav1alpha1.Tieredstore{})
+	if err != nil {
+		t.Errorf("fail to create the runtimeInfo with error %v", err)
+	}
+
+	testPVs := []runtime.Object{}
+	for _, pvInput := range testPVInputs {
+		testPVs = append(testPVs, pvInput.DeepCopy())
+	}
+	client := fake.NewFakeClientWithScheme(testScheme, testPVs...)
+
+	var testCase = []struct {
+		runtimeInfo    base.RuntimeInfoInterface
+		expectedResult bool
+	}{
+		{
+			runtimeInfo:    runtimeInfoSpark,
+			expectedResult: false,
+		},
+		{
+			runtimeInfo:    runtimeInfoHbase,
+			expectedResult: true,
+		},
+	}
+	for _, test := range testCase {
+		var log = ctrl.Log.WithName("deprecated")
+		if result, _ := HasDeprecatedPersistentVolumeName(client, test.runtimeInfo, log); result != test.expectedResult {
+			t.Errorf("fail to exec the function with the error %v", err)
+		}
+	}
+}
