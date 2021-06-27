255a9eca1de37b98c31366fb96d073acfbbfca7d
cheyang <cheyang@163.com>
2021-06-27 18:03:12 +0800

    Add ut, To #34516710
    Signed-off-by: cheyang <cheyang@163.com>

--- pkg/webhook/plugins/requirenodewithfuse/require_node_with_fuse_test.go
+++ pkg/webhook/plugins/requirenodewithfuse/require_node_with_fuse_test.go
@@ -65,13 +65,27 @@ func TestGetRequiredSchedulingTermWithGlobalMode(t *testing.T) {
 }
 
 func TestGetRequiredSchedulingTermWithDefaultMode(t *testing.T) {
-	_, err := base.BuildRuntimeInfo("test", "fluid", "alluxio", datav1alpha1.Tieredstore{})
+	runtimeInfo, err := base.BuildRuntimeInfo("test", "fluid", "alluxio", datav1alpha1.Tieredstore{})
 	if err != nil {
 		t.Errorf("fail to create the runtimeInfo with error %v", err)
 	}
 
-	// runtimeInfo.SetupFuseDeployMode(false, map[string]string{})
-	// terms := getRequiredSchedulingTerm(runtimeInfo)
+	runtimeInfo.SetupFuseDeployMode(false, map[string]string{})
+	terms, _ := getRequiredSchedulingTerm(runtimeInfo)
+
+	expectTerms := corev1.NodeSelectorTerm{
+		MatchExpressions: []corev1.NodeSelectorRequirement{
+			{
+				Key:      "fluid.io/s-fluid-test",
+				Operator: corev1.NodeSelectorOpIn,
+				Values:   []string{"true"},
+			},
+		},
+	}
+
+	if !reflect.DeepEqual(terms, expectTerms) {
+		t.Errorf("getRequiredSchedulingTerm failure, want:%v, got:%v", expectTerms, terms)
+	}
 }
 
 func TestMutate(t *testing.T) {
