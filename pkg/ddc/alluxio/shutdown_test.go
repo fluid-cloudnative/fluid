3dcc49a5948435c5521f8dd18c3a5870f43f453a
xieydd <xieydd@gmail.com>
2021-06-27 23:40:18 +0800

    Add Test for worker
    Signed-off-by: xieydd <xieydd@gmail.com>

--- pkg/ddc/alluxio/shutdown_test.go
+++ pkg/ddc/alluxio/shutdown_test.go
@@ -1,16 +1,17 @@
 package alluxio
 
 import (
+	"reflect"
+	"testing"
+
 	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
 	"github.com/fluid-cloudnative/fluid/pkg/ddc/base"
 	"github.com/fluid-cloudnative/fluid/pkg/utils/kubeclient"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"reflect"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-	"testing"
 )
 
 var (
