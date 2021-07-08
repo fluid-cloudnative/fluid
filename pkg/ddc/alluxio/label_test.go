d47423a56e54c18ecf569490384c2b12c994e8ef
LDawn <lsm@smail.nju.edu.cn>
2021-07-08 18:41:21 +0800

    add ut for label and fix hcfs_test
    Signed-off-by: LDawn <lsm@smail.nju.edu.cn>

---
+++ pkg/ddc/alluxio/label_test.go
@@ -0,0 +1,37 @@
+package alluxio
+
+import "testing"
+
+func TestGetCommonLabelname(t *testing.T) {
+	testCases := []struct{
+		name 		string
+		namespace 	string
+		out 		string
+	}{
+		{
+			name: 			"hbase",
+			namespace: 		"fluid",
+			out:			"fluid.io/s-fluid-hbase",
+		},
+		{
+			name: 			"hadoop",
+			namespace: 		"fluid",
+			out:			"fluid.io/s-fluid-hadoop",
+		},
+		{
+			name: 			"common",
+			namespace: 		"default",
+			out:			"fluid.io/s-default-common",
+		},
+	}
+	for _,testCase := range testCases{
+		engine := &AlluxioEngine{
+			name:testCase.name,
+			namespace: testCase.namespace,
+		}
+		out := engine.getCommonLabelname()
+		if out!=testCase.out{
+			t.Errorf("in: %s-%s, expect: %s, got: %s",testCase.namespace,testCase.name,testCase.out,out)
+		}
+	}
+}
