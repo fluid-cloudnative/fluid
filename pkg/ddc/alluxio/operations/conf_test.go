90bdd430ac3cb888026a8577e9afa59da9fefea8
yangyuliufeng <30432274+yangyuliufeng@users.noreply.github.com>
2021-07-07 19:23:00 -0700

    fix nil pointer bug and add ut (#907)
    Signed-off-by: yangyuliufeng <qlw705706@gmail.com>

--- pkg/ddc/alluxio/operations/conf_test.go
+++ pkg/ddc/alluxio/operations/conf_test.go
@@ -8,16 +8,15 @@ import (
 	"testing"
 )
 
-
 func TestAlluxioFileUtils_GetConf(t *testing.T) {
-	mockExec := func(podName string, containerName string, namespace string, cmd []string)(stdout string, stderr string, e error) {
-		if strings.Contains(cmd[2],OTHER_ERR) {
+	mockExec := func(podName string, containerName string, namespace string, cmd []string) (stdout string, stderr string, e error) {
+		if strings.Contains(cmd[2], OTHER_ERR) {
 			return "", "", errors.New("other error")
-		} else{
+		} else {
 			return "conf", "", nil
 		}
 	}
-	err := gohook.Hook(kubeclient.ExecCommandInContainer,mockExec,nil)
+	err := gohook.Hook(kubeclient.ExecCommandInContainer, mockExec, nil)
 	if err != nil {
 		t.Fatal(err.Error())
 	}
@@ -29,22 +28,22 @@ func TestAlluxioFileUtils_GetConf(t *testing.T) {
 	}
 	defer wrappedUnhook()
 
-	var tests = []struct{
-		in string
-		out string
+	var tests = []struct {
+		in    string
+		out   string
 		noErr bool
 	}{
-		{in:OTHER_ERR,out: "",noErr: false},
-		{in:FINE,out:"conf",noErr: true},
+		{in: OTHER_ERR, out: "", noErr: false},
+		{in: FINE, out: "conf", noErr: true},
 	}
 
-	for _,test := range tests{
+	for _, test := range tests {
 		stdout, err := AlluxioFileUtils{log: NullLogger{}}.GetConf(test.in)
-		if stdout!=test.out {
+		if stdout != test.out {
 			t.Errorf("input parameter is %s,expected %s, got %s", test.in, test.out, stdout)
 		}
 		noerror := err == nil
-		if noerror!=test.noErr {
+		if noerror != test.noErr {
 			t.Errorf("input parameter is %s,expected noerr is %t", test.in, test.noErr)
 		}
 	}
