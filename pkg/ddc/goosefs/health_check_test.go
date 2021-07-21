0ad6bbb8292627681dff6df736082ddd8ee65835
xieydd <xieydd@gmail.com>
2021-07-22 00:26:43 +0800

    Add ut
    Signed-off-by: xieydd <xieydd@gmail.com>

--- pkg/ddc/goosefs/health_check_test.go
+++ pkg/ddc/goosefs/health_check_test.go
@@ -1,10 +1,12 @@
 package goosefs
 
 import (
-	"fmt"
+	"context"
+	"reflect"
 	"testing"
 
 	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
+	"github.com/fluid-cloudnative/fluid/pkg/common"
 	"github.com/fluid-cloudnative/fluid/pkg/utils"
 	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -12,6 +14,205 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 )
+
+func TestCheckRuntimeHealthy(t *testing.T) {
+	var statefulsetInputs = []appsv1.StatefulSet{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "hbase-master",
+				Namespace: "fluid",
+			},
+			Status: appsv1.StatefulSetStatus{
+				Replicas:      3,
+				ReadyReplicas: 3,
+			},
+		},
+	}
+	testObjs := []runtime.Object{}
+	for _, statefulset := range statefulsetInputs {
+		testObjs = append(testObjs, statefulset.DeepCopy())
+	}
+
+	var daemonSetInputs = []appsv1.DaemonSet{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "hbase-worker",
+				Namespace: "fluid",
+			},
+			Status: appsv1.DaemonSetStatus{
+				NumberUnavailable: 0,
+				NumberReady:       1,
+				NumberAvailable:   1,
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "hbase-fuse",
+				Namespace: "fluid",
+			},
+			Status: appsv1.DaemonSetStatus{
+				NumberUnavailable: 0,
+				NumberReady:       1,
+				NumberAvailable:   1,
+			},
+		},
+	}
+	for _, daemonSet := range daemonSetInputs {
+		testObjs = append(testObjs, daemonSet.DeepCopy())
+	}
+
+	var goosefsRuntimeInputs = []datav1alpha1.GooseFSRuntime{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "hbase",
+				Namespace: "fluid",
+			},
+			Spec: datav1alpha1.GooseFSRuntimeSpec{
+				Master: datav1alpha1.GooseFSCompTemplateSpec{
+					Replicas: 1,
+				},
+			},
+			Status: datav1alpha1.RuntimeStatus{
+				CacheStates: map[common.CacheStateName]string{
+					common.Cached: "true",
+				},
+			},
+		},
+	}
+	for _, goosefsRuntime := range goosefsRuntimeInputs {
+		testObjs = append(testObjs, goosefsRuntime.DeepCopy())
+	}
+
+	var datasetInputs = []*datav1alpha1.Dataset{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "hbase",
+				Namespace: "fluid",
+			},
+			Spec: datav1alpha1.DatasetSpec{},
+			Status: datav1alpha1.DatasetStatus{
+				HCFSStatus: &datav1alpha1.HCFSStatus{
+					Endpoint:                    "test Endpoint",
+					UnderlayerFileSystemVersion: "Underlayer HCFS Compatible Version",
+				},
+			},
+		},
+	}
+	for _, dataset := range datasetInputs {
+		testObjs = append(testObjs, dataset.DeepCopy())
+	}
+
+	client := fake.NewFakeClientWithScheme(testScheme, testObjs...)
+
+	engines := []GooseFSEngine{
+		{
+			Client:    client,
+			Log:       log.NullLogger{},
+			namespace: "fluid",
+			name:      "hbase",
+			runtime:   &goosefsRuntimeInputs[0],
+		},
+	}
+
+	var testCase = []struct {
+		engine                             GooseFSEngine
+		expectedErrorNil                   bool
+		expectedMasterPhase                datav1alpha1.RuntimePhase
+		expectedWorkerPhase                datav1alpha1.RuntimePhase
+		expectedRuntimeWorkerNumberReady   int32
+		expectedRuntimeWorkerAvailable     int32
+		expectedRuntimeFuseNumberReady     int32
+		expectedRuntimeFuseNumberAvailable int32
+		expectedDataset                    datav1alpha1.Dataset
+	}{
+		{
+			engine:                             engines[0],
+			expectedErrorNil:                   true,
+			expectedMasterPhase:                datav1alpha1.RuntimePhaseReady,
+			expectedWorkerPhase:                "",
+			expectedRuntimeWorkerNumberReady:   1,
+			expectedRuntimeWorkerAvailable:     1,
+			expectedRuntimeFuseNumberReady:     1,
+			expectedRuntimeFuseNumberAvailable: 1,
+			expectedDataset: datav1alpha1.Dataset{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "hbase",
+					Namespace: "fluid",
+				},
+				Status: datav1alpha1.DatasetStatus{
+					Phase: datav1alpha1.BoundDatasetPhase,
+					CacheStates: map[common.CacheStateName]string{
+						common.Cached: "true",
+					},
+					HCFSStatus: &datav1alpha1.HCFSStatus{
+						Endpoint:                    "test Endpoint",
+						UnderlayerFileSystemVersion: "Underlayer HCFS Compatible Version",
+					},
+				},
+			},
+		},
+	}
+	for _, test := range testCase {
+		err := test.engine.CheckRuntimeHealthy()
+		if err != nil && test.expectedErrorNil == true ||
+			err == nil && test.expectedErrorNil == false {
+			t.Errorf("fail to exec the checkMasterHealthy function with err %v", err)
+			return
+		}
+		if test.expectedErrorNil == false {
+			continue
+		}
+
+		goosefsRuntime, err := test.engine.getRuntime()
+		if err != nil {
+			t.Errorf("fail to get the runtime with the error %v", err)
+			return
+		}
+		if goosefsRuntime.Status.MasterPhase != test.expectedMasterPhase {
+			t.Errorf("fail to update the runtime status, get %s, expect %s", goosefsRuntime.Status.MasterPhase, test.expectedMasterPhase)
+			return
+		}
+		if goosefsRuntime.Status.WorkerNumberReady != test.expectedRuntimeWorkerNumberReady ||
+			goosefsRuntime.Status.WorkerNumberAvailable != test.expectedRuntimeWorkerAvailable {
+			t.Errorf("fail to update the runtime")
+			return
+		}
+		if goosefsRuntime.Status.FuseNumberReady != test.expectedRuntimeFuseNumberReady ||
+			goosefsRuntime.Status.FuseNumberAvailable != test.expectedRuntimeFuseNumberAvailable {
+			t.Errorf("fail to update the runtime")
+			return
+		}
+
+		_, cond := utils.GetRuntimeCondition(goosefsRuntime.Status.Conditions, datav1alpha1.RuntimeMasterReady)
+		if cond == nil {
+			t.Errorf("fail to update the condition")
+			return
+		}
+		_, cond = utils.GetRuntimeCondition(goosefsRuntime.Status.Conditions, datav1alpha1.RuntimeWorkersReady)
+		if cond == nil {
+			t.Errorf("fail to update the condition")
+			return
+		}
+		_, cond = utils.GetRuntimeCondition(goosefsRuntime.Status.Conditions, datav1alpha1.RuntimeFusesReady)
+		if cond == nil {
+			t.Errorf("fail to update the condition")
+			return
+		}
+
+		var datasets datav1alpha1.DatasetList
+		err = client.List(context.TODO(), &datasets)
+		if err != nil {
+			t.Errorf("fail to list the datasets with error %v", err)
+			return
+		}
+		if !reflect.DeepEqual(datasets.Items[0].Status.Phase, test.expectedDataset.Status.Phase) ||
+			!reflect.DeepEqual(datasets.Items[0].Status.CacheStates, test.expectedDataset.Status.CacheStates) ||
+			!reflect.DeepEqual(datasets.Items[0].Status.HCFSStatus, test.expectedDataset.Status.HCFSStatus) {
+			t.Errorf("fail to exec the function with error %v", err)
+			return
+		}
+	}
+}
 
 func TestCheckMasterHealthy(t *testing.T) {
 	var statefulsetInputs = []appsv1.StatefulSet{
@@ -117,7 +318,6 @@ func TestCheckMasterHealthy(t *testing.T) {
 			continue
 		}
 		goosefsruntime, err := test.engine.getRuntime()
-		fmt.Println(goosefsruntime)
 		if err != nil {
 			t.Errorf("fail to get the runtime with the error %v", err)
 			return
--- pkg/ddc/goosefs/metadata_test.go
+++ pkg/ddc/goosefs/metadata_test.go
@@ -1,9 +1,12 @@
 package goosefs
 
 import (
+	"errors"
 	"testing"
 
+	"github.com/brahma-adshonor/gohook"
 	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
+	"github.com/fluid-cloudnative/fluid/pkg/ddc/goosefs/operations"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
@@ -72,4 +75,172 @@ func TestShouldSyncMetadata(t *testing.T) {
 			t.Errorf("fail to exec the function")
 		}
 	}
+}
+
+func TestShouldRestoreMetadata(t *testing.T) {
+	datasetInputs := []datav1alpha1.Dataset{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "hbase",
+				Namespace: "fluid",
+			},
+			Spec: datav1alpha1.DatasetSpec{
+				DataRestoreLocation: &datav1alpha1.DataRestoreLocation{
+					Path:     "local:///host1/erf",
+					NodeName: "test-node",
+				},
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "spark",
+				Namespace: "fluid",
+			},
+		},
+	}
+	testObjs := []runtime.Object{}
+	for _, datasetInput := range datasetInputs {
+		testObjs = append(testObjs, datasetInput.DeepCopy())
+	}
+
+	client := fake.NewFakeClientWithScheme(testScheme, testObjs...)
+
+	engines := []GooseFSEngine{
+		{
+			name:      "hbase",
+			namespace: "fluid",
+			Client:    client,
+			Log:       log.NullLogger{},
+		},
+		{
+			name:      "spark",
+			namespace: "fluid",
+			Client:    client,
+			Log:       log.NullLogger{},
+		},
+	}
+
+	var testCases = []struct {
+		engine         GooseFSEngine
+		expectedShould bool
+	}{
+		{
+			engine:         engines[0],
+			expectedShould: true,
+		},
+		{
+			engine:         engines[1],
+			expectedShould: false,
+		},
+	}
+	for _, test := range testCases {
+		should, err := test.engine.shouldRestoreMetadata()
+		if err != nil || should != test.expectedShould {
+			t.Errorf("fail to exec the function")
+		}
+	}
+}
+
+func TestRestoreMetadataInternal(t *testing.T) {
+	QueryMetaDataInfoIntoFileCommon := func(a operations.GooseFSFileUtils, key operations.KeyOfMetaDataFile, filename string) (value string, err error) {
+		return "1024", nil
+	}
+	QueryMetaDataInfoIntoFileErr := func(a operations.GooseFSFileUtils, key operations.KeyOfMetaDataFile, filename string) (value string, err error) {
+		return "", errors.New("fail to query MetaDataInfo")
+	}
+
+	wrappedUnhookQueryMetaDataInfoIntoFile := func() {
+		err := gohook.UnHook(operations.GooseFSFileUtils.QueryMetaDataInfoIntoFile)
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	}
+
+	datasetInputs := []datav1alpha1.Dataset{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "hbase",
+				Namespace: "fluid",
+			},
+			Spec: datav1alpha1.DatasetSpec{
+				DataRestoreLocation: &datav1alpha1.DataRestoreLocation{
+					Path:     "local:///host1/erf",
+					NodeName: "test-node",
+				},
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "spark",
+				Namespace: "fluid",
+			},
+			Spec: datav1alpha1.DatasetSpec{
+				DataRestoreLocation: &datav1alpha1.DataRestoreLocation{
+					Path:     "pvc://pvc1/erf",
+					NodeName: "test-node",
+				},
+			},
+		},
+	}
+	testObjs := []runtime.Object{}
+	for _, datasetInput := range datasetInputs {
+		testObjs = append(testObjs, datasetInput.DeepCopy())
+	}
+
+	client := fake.NewFakeClientWithScheme(testScheme, testObjs...)
+
+	engines := []GooseFSEngine{
+		{
+			name:      "hbase",
+			namespace: "fluid",
+			Client:    client,
+			Log:       log.NullLogger{},
+		},
+		{
+			name:      "hbase",
+			namespace: "fluid",
+			Client:    client,
+			Log:       log.NullLogger{},
+		},
+	}
+
+	err := gohook.Hook(operations.GooseFSFileUtils.QueryMetaDataInfoIntoFile, QueryMetaDataInfoIntoFileErr, nil)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	err = engines[0].RestoreMetadataInternal()
+	if err == nil {
+		t.Errorf("fail to catch the error")
+	}
+	wrappedUnhookQueryMetaDataInfoIntoFile()
+
+	err = gohook.Hook(operations.GooseFSFileUtils.QueryMetaDataInfoIntoFile, QueryMetaDataInfoIntoFileCommon, nil)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	var testCases = []struct {
+		engine                  GooseFSEngine
+		expectedDatasetUfsTotal string
+		expectedDatasetFileNum  string
+	}{
+		{
+			engine:                  engines[0],
+			expectedDatasetUfsTotal: "1.00KiB",
+			expectedDatasetFileNum:  "1024",
+		},
+		{
+			engine:                  engines[1],
+			expectedDatasetUfsTotal: "1.00KiB",
+			expectedDatasetFileNum:  "1024",
+		},
+	}
+
+	for _, test := range testCases {
+		err = test.engine.RestoreMetadataInternal()
+		if err != nil {
+			t.Errorf("fail to exec function RestoreMetadataInternal")
+		}
+	}
+	wrappedUnhookQueryMetaDataInfoIntoFile()
 }
