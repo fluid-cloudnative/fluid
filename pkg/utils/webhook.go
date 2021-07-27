8a6c357f4639893a202b30ca368a5897c92ae4b6
ldd91 <lvdongdong30@gmail.com>
2021-07-27 13:57:02 +0800

    fix ci error
    Signed-off-by: ldd91 <lvdongdong30@gmail.com>

--- pkg/utils/webhook.go
+++ pkg/utils/webhook.go
@@ -62,9 +62,7 @@ func InjectNodeSelectorTerms(requiredSchedulingTerms []corev1.NodeSelectorTerm, 
 		pod.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms = requiredSchedulingTerms
 	} else {
 		for i := 0; i < len(requiredSchedulingTerms); i++ {
-			for _, value := range requiredSchedulingTerms[i].MatchExpressions {
-				pod.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms[0].MatchExpressions = append(pod.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms[0].MatchExpressions, value)
-			}
+			pod.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms[0].MatchExpressions = append(pod.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms[0].MatchExpressions, requiredSchedulingTerms[i].MatchExpressions...)
 		}
 	}
 
