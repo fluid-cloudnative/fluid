7d3b3a91aa064a43441036ee987600ddb2c70b36
TzZtzt <trafalgarz@outlook.com>
2021-09-05 23:34:41 +0800

    Retry to check port usage
    Signed-off-by: TzZtzt <trafalgarz@outlook.com>

--- charts/alluxio/docker/init-users/check_port.sh
+++ charts/alluxio/docker/init-users/check_port.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
-set -e
+#set -x
 
 function printUsage() {
     echo -e "Usage: Run command with related environment variable set"
     echo
     echo -e 'Environment Variable "PORTS_TO_CHECK" is set:'
     echo -e " PORT1:PORT2:PORT3..."
+}
+
+function check_port() {
+  ports=$1
+  for port in "${ports[@]}"; do
+    echo "Checking if port $port is in use:"
+    echo "> netstat -ntp | grep $port"
+    # ignore grep not found
+    netstat -ntp | awk '{print $4,"\t",$6,"\t",$7}' | grep "$port"
+    if [[ $? -eq 0 ]]; then
+      # Found any port is in use
+      return 1
+    fi
+    echo
+  done
+
+  # No port in use
+  return 0
 }
 
 function main() {
@@ -17,14 +35,24 @@ function main() {
         printUsage
         exit 1
     fi
-    for port in "${ports[@]}"; do
-      echo "Checking if port $port is in use:"
-      echo "> netstat -nltp | grep $port"
-      # ignore grep not found
-      netstat -nltp | grep "$port" || [[ $? == 1 ]]
+
+    # Timeout for 60 * 10s(10min)
+    count=1
+    while [[ count -lt 61 ]]; do
       echo
+      echo "Retry to check port usage for the $count time"
+      check_port $ports
+      if [[ $? == 0 ]]; then
+        echo "No port conflict found. Exiting..."
+        exit 0
+      fi
+
+      count=`expr $count + 1`
+      sleep 10
     done
 
+    echo "Timeout for port conflicts"
+    exit 1
 }
 
 main "$@"
