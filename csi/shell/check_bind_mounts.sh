a3f39b11cae58b65a3766bce9bee9f39ba4e5ade
TzZtzt <trafalgarz@outlook.com>
2021-09-15 22:08:40 +0800

    Remove kubelet root directory in MountPattern
    Signed-off-by: TzZtzt <trafalgarz@outlook.com>

--- csi/shell/check_bind_mounts.sh
+++ csi/shell/check_bind_mounts.sh
@@ -4,6 +4,6 @@ set -ex
 
 VolumeName="$1"
 
-MountPattern="/var/lib/kubelet/pods/.*/volumes/kubernetes\.io~csi/$VolumeName/mount"
+MountPattern="pods/.*/volumes/kubernetes\.io~csi/$VolumeName/mount"
 
 mount | grep $MountPattern
