b397fd05bdaa44f7cb63fbe2a01fe43779b96515
cheyang <cheyang@163.com>
2021-10-09 19:52:56 +0800

    Hand over schedule to K8s, To #36006921
    Signed-off-by: cheyang <cheyang@163.com>

--- pkg/ddc/jindo/replicas.go
+++ pkg/ddc/jindo/replicas.go
@@ -2,81 +2,42 @@ package jindo
 
 import (
 	"context"
-	"reflect"
 
-	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
-	"github.com/fluid-cloudnative/fluid/pkg/common"
 	cruntime "github.com/fluid-cloudnative/fluid/pkg/runtime"
-	"github.com/fluid-cloudnative/fluid/pkg/utils"
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/util/retry"
 )
 
 func (e JindoEngine) SyncReplicas(ctx cruntime.ReconcileRequestContext) (err error) {
 
-	runtime, err := e.getRuntime()
-	if err != nil {
-		return err
-	}
-
-	if runtime.Replicas() > runtime.Status.CurrentWorkerNumberScheduled {
-		err = e.SetupWorkers()
-		if err != nil {
-			return err
-		}
-		_, err = e.CheckWorkersReady()
-		if err != nil {
-			e.Log.Error(err, "Check if the workers are ready")
-			return err
-		}
-
-	} else if runtime.Replicas() < runtime.Status.CurrentWorkerNumberScheduled {
-		replicas := runtime.Replicas()
-		e.Log.Info("Scaling in Jindo workers", "expectedReplicas", replicas)
-		curReplicas, err := e.destroyWorkers(replicas)
+	var (
+		workerName string = e.getWorkertName()
+		namespace  string = e.namespace
+	)
+	err = retry.RetryOnConflict(retry.DefaultBackoff, func() error {
+		workers, err := e.getStatefulset(workerName, namespace)
 		if err != nil {
 			return err
 		}
 
-		if curReplicas > replicas {
-			ctx.Recorder.Eventf(runtime, corev1.EventTypeWarning, common.RuntimeScaleInFailed,
-				"Jindo workers are being used by some pods, can't scale in (expected replicas: %v, current replicas: %v)",
-				replicas, curReplicas)
+		runtime, err := e.getRuntime()
+		if err != nil {
+			return err
 		}
 
-		err = retry.RetryOnConflict(retry.DefaultBackoff, func() error {
-			runtime, err := e.getRuntime()
+		desireReplicas := runtime.Replicas()
+		if *workers.Spec.Replicas != desireReplicas {
+			workerToUpdate := workers.DeepCopy()
+			workerToUpdate.Spec.Replicas = &desireReplicas
+			err = e.Client.Update(context.TODO(), workerToUpdate)
 			if err != nil {
-				e.Log.Error(err, "scale in when sync replicas")
 				return err
 			}
-
-			runtimeToUpdate := runtime.DeepCopy()
-
-			if len(runtimeToUpdate.Status.Conditions) == 0 {
-				runtimeToUpdate.Status.Conditions = []datav1alpha1.RuntimeCondition{}
-			}
-
-			runtimeToUpdate.Status.DesiredWorkerNumberScheduled = replicas
-			runtimeToUpdate.Status.CurrentWorkerNumberScheduled = curReplicas
-			cond := utils.NewRuntimeCondition(datav1alpha1.RuntimeWorkerScaledIn, datav1alpha1.RuntimeWorkersScaledInReason,
-				"The workers scaled in.", corev1.ConditionTrue)
-			runtimeToUpdate.Status.Conditions =
-				utils.UpdateRuntimeCondition(runtimeToUpdate.Status.Conditions, cond)
-
-			if !reflect.DeepEqual(runtime.Status, runtimeToUpdate.Status) {
-				return e.Client.Status().Update(context.TODO(), runtimeToUpdate)
-			}
-
-			return nil
-		})
-
-		if err != nil {
-			return err
+		} else {
+			e.Log.V(1).Info("Nothing to do for syncing")
 		}
-	} else {
-		e.Log.V(1).Info("Nothing to do")
-	}
+
+		return nil
+	})
 
 	return
 }
