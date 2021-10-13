75c7e613d5bef6f89c15134591a010c7f3b89e5a
yangyuliufeng <30432274+yangyuliufeng@users.noreply.github.com>
2021-10-13 10:52:14 +0800

    add a test case (#1036)
    Signed-off-by: yangyuliufeng <qlw705706@gmail.com>

---
+++ pkg/ddc/base/pv_test.go
@@ -0,0 +1,46 @@
+package base
+
+import (
+	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
+	"testing"
+)
+
+func TestGetPersistentVolumeName(t *testing.T) {
+	var testCases = []struct {
+		runtimeName    string
+		runtimeNamespace string
+		isDeprecatedPVName bool
+		expectedPVName string
+	}{
+		{
+			runtimeName:    "spark",
+			runtimeNamespace: "fluid",
+			isDeprecatedPVName: false,
+			expectedPVName: "fluid-spark",
+		},
+		{
+			runtimeName:    "hadoop",
+			runtimeNamespace: "test",
+			isDeprecatedPVName: false,
+			expectedPVName: "test-hadoop",
+		},
+		{
+			runtimeName:    "hbase",
+			runtimeNamespace: "fluid",
+			isDeprecatedPVName: true,
+			expectedPVName: "hbase",
+		},
+
+	}
+	for _, testCase := range testCases {
+		runtimeInfo, err := BuildRuntimeInfo(testCase.runtimeName, testCase.runtimeNamespace, "alluxio", datav1alpha1.TieredStore{})
+		if err != nil {
+			t.Errorf("fail to create the runtimeInfo with error %v", err)
+		}
+		runtimeInfo.SetDeprecatedPVName(testCase.isDeprecatedPVName)
+		result := runtimeInfo.GetPersistentVolumeName()
+		if result != testCase.expectedPVName {
+			t.Errorf("get failure, expected %s, get %s", testCase.expectedPVName, result)
+		}
+	}
+}
