c1b4362c42def9a4155195925cf841cd2521a993
LDawn <38764512+LDawns@users.noreply.github.com>
2021-11-02 10:25:34 +0800

    add ut for healthcheck. (#1051)
    Signed-off-by: LDawns <lsm@smail.nju.edu.cn>

---
+++ pkg/ddc/jindo/health_check_test.go
@@ -0,0 +1,13 @@
+package jindo
+
+import (
+	"testing"
+)
+
+func TestCheckRuntimeHealthy(t *testing.T){
+	engine := &JindoEngine{}
+	err := engine.CheckRuntimeHealthy()
+	if err != nil{
+		t.Errorf("check runtime healthy failed,err:%s",err.Error())
+	}
+}
