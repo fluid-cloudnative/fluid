72ddf2014c4160cc7818cf8e48a413a2a6d18928
huangxu.chase <huangxu@smail.nju.edu.cn>
2021-11-10 12:12:41 +0800

    add lisence at pkg/common/storage.go
    Signed-off-by: huangxu.chase <huangxu@smail.nju.edu.cn>

--- pkg/common/storage.go
+++ pkg/common/storage.go
@@ -1,4 +1,5 @@
 /*
+Copyright 2021 The Fluid Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -11,7 +12,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-
 */
 
 package common
