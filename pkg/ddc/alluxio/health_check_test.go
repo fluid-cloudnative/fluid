6f79c11c56103584d2f2d9e172315d724e736bde
“chenf1234” <1054213730@qq.com>
2021-11-15 16:08:24 +0800

    revise Copyright
    Signed-off-by: “chenf1234” <1054213730@qq.com>

--- pkg/ddc/alluxio/health_check_test.go
+++ pkg/ddc/alluxio/health_check_test.go
@@ -1,6 +1,6 @@
 /*
 
-Copyright <year> The <project-name> Author.
+Copyright 2021 The Fluid Author.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
