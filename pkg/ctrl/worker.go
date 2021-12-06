d6265c2a326b52c51cd36ea1de1bf216a24e2251
cheyang <cheyang@163.com>
2021-12-06 11:34:23 +0800

    Add event handler, To #37688693
    Signed-off-by: cheyang <cheyang@163.com>

--- pkg/ctrl/worker.go
+++ pkg/ctrl/worker.go
@@ -17,12 +17,25 @@ limitations under the License.
 package ctrl
 
 import (
+	"context"
+	"fmt"
+	"reflect"
+
 	fluiderrs "github.com/fluid-cloudnative/fluid/pkg/errors"
-	"github.com/fluid-cloudnative/fluid/pkg/utils/kubeclient"
-	appsv1 "k8s.io/api/apps/v1"
+
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
+	"github.com/fluid-cloudnative/fluid/pkg/ddc/base"
+	"github.com/fluid-cloudnative/fluid/pkg/utils"
+	"github.com/fluid-cloudnative/fluid/pkg/utils/kubeclient"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/record"
 
 	apierrs "k8s.io/apimachinery/pkg/api/errors"
 )
@@ -46,4 +59,78 @@ func GetWorkersAsStatefulset(client client.Client, key types.NamespacedName) (wo
 	}
 
 	return
+}
+
+// CheckworkersHealthy checks the sts healthy with role
+func (e *Helper) CheckWorkerHealthy(runtime base.RuntimeInterface,
+	currentStatus datav1alpha1.RuntimeStatus,
+	sts *appsv1.StatefulSet, recorder record.EventRecorder) (err error) {
+	var healthy bool
+	if sts.Status.Replicas == sts.Status.ReadyReplicas {
+		healthy = true
+	}
+
+	statusToUpdate := runtime.GetStatus()
+	if len(statusToUpdate.Conditions) == 0 {
+		statusToUpdate.Conditions = []datav1alpha1.RuntimeCondition{}
+	}
+
+	if healthy {
+		cond := utils.NewRuntimeCondition(datav1alpha1.RuntimeWorkersReady, "The worker are ready.",
+			"The worker are ready.", corev1.ConditionTrue)
+		_, oldCond := utils.GetRuntimeCondition(statusToUpdate.Conditions, cond.Type)
+
+		if oldCond == nil || oldCond.Type != cond.Type {
+			statusToUpdate.Conditions =
+				utils.UpdateRuntimeCondition(statusToUpdate.Conditions,
+					cond)
+		}
+		statusToUpdate.WorkerPhase = datav1alpha1.RuntimePhaseReady
+
+	} else {
+		// 1. Update the status
+		cond := utils.NewRuntimeCondition(datav1alpha1.RuntimeWorkersReady, "The workers are not ready.",
+			fmt.Sprintf("The workers %s in %s are not ready.", sts.Name, sts.Namespace), corev1.ConditionFalse)
+		_, oldCond := utils.GetRuntimeCondition(statusToUpdate.Conditions, cond.Type)
+
+		if oldCond == nil || oldCond.Type != cond.Type {
+			statusToUpdate.Conditions =
+				utils.UpdateRuntimeCondition(statusToUpdate.Conditions,
+					cond)
+		}
+		statusToUpdate.WorkerPhase = datav1alpha1.RuntimePhaseNotReady
+
+		// 2. Record the event
+		selector, err := metav1.LabelSelectorAsSelector(sts.Spec.Selector)
+		if err != nil {
+			return fmt.Errorf("error converting StatefulSet %s in namespace %s selector: %v", sts.Name, sts.Namespace, err)
+		}
+
+		unavailablePodNames, err := kubeclient.GetunavailablePodNamesForStatefulSet(e.client, sts, selector)
+		if err != nil {
+			return err
+		}
+
+		// 3. Set error
+		msg := fmt.Sprintf("the workers %s in %s are not ready. The expected number is %d, the actual number is %d, the unhealthy pods are %v",
+			sts.Name,
+			sts.Namespace,
+			sts.Status.Replicas,
+			sts.Status.ReadyReplicas,
+			unavailablePodNames)
+
+		recorder.Eventf(runtime, corev1.EventTypeWarning, "workersUnhealthy", msg)
+	}
+
+	if err != nil {
+		return
+	}
+
+	status := *statusToUpdate
+	if !reflect.DeepEqual(status, currentStatus) {
+		return e.client.Status().Update(context.TODO(), runtime)
+	}
+
+	return
+
 }
--- pkg/utils/kubeclient/daemonset.go
+++ pkg/utils/kubeclient/daemonset.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -17,4 +19,44 @@ func GetDaemonset(c client.Client, name string, namespace string) (ds *appsv1.Da
 	}, ds)
 
 	return ds, err
+}
+
+// GetDaemonPods gets pods of the specified daemonset
+func GetDaemonPods(c client.Client, ds *appsv1.DaemonSet) (pods []*v1.Pod, err error) {
+	selector, err := metav1.LabelSelectorAsSelector(ds.Spec.Selector)
+	if err != nil {
+		return nil, err
+	}
+
+	podList := &v1.PodList{}
+	err = c.List(context.TODO(), podList, &client.ListOptions{
+		LabelSelector: selector,
+	})
+
+	if err != nil {
+		log.Error(err, "Failed to list pods for daemonset")
+		return
+	}
+
+	for _, pod := range podList.Items {
+		pods = append(pods, &pod)
+	}
+
+	return
+}
+
+// GetDaemonPods gets pods of the specified daemonset
+func GetUnavailableDaemonPods(c client.Client, ds *appsv1.DaemonSet) (unavailablePods []*v1.Pod, err error) {
+	pods, err := GetDaemonPods(c, ds)
+	if err != nil {
+		return
+	}
+
+	for _, pod := range pods {
+		if !isRunningAndReady(pod) {
+			unavailablePods = append(unavailablePods, pod)
+		}
+	}
+
+	return
 }
