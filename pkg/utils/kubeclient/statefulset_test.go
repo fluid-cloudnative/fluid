1f221cea1ac219342a725f87d380ff3fd203e532
cheyang <cheyang@163.com>
2021-12-08 16:05:54 +0800

    Add event watcher, To #37688693
    Signed-off-by: cheyang <cheyang@163.com>

--- pkg/utils/kubeclient/statefulset_test.go
+++ pkg/utils/kubeclient/statefulset_test.go
@@ -17,12 +17,15 @@ limitations under the License.
 package kubeclient
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/fluid-cloudnative/fluid/pkg/utils/fake"
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 
 	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
 )
@@ -167,6 +170,53 @@ func TestGetPhaseFromStatefulset(t *testing.T) {
 			// t.Errorf("testcase %v IsPersistentVolumeClaimExist() = %v, want %v", tt.name, got, tt.want)
 
 		})
+	}
+
+}
+
+func TestGetUnavailablePodNamesForStatefulSet(t *testing.T) {
+	namespace := "default"
+	sts := &appsv1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{Name: "unavailableSts",
+			Namespace: namespace},
+		Spec: appsv1.StatefulSetSpec{},
+	}
+
+	podList := &corev1.PodList{
+		Items: []corev1.Pod{
+			{
+				ObjectMeta: metav1.ObjectMeta{Name: "unavailableSts-0",
+					Namespace: namespace},
+				Spec: corev1.PodSpec{},
+			},
+		},
+	}
+
+	// testRuntimes := []runtime.Object{}
+	// testRuntimes = append(testRuntimes, sts.DeepCopy())
+	// testRuntimes = append(testRuntimes, podList)
+
+	// for _, pod := range podList.Items {
+	// 	testRuntimes = append(testRuntimes, &pod)
+	// }
+	
+	client := fake.NewFakeClientWithScheme(testScheme, podList, sts.DeepCopy())
+	selector, _ := metav1.LabelSelectorAsSelector(sts.Spec.Selector)
+	podNames, err := GetUnavailablePodNamesForStatefulSet(client, sts, selector)
+
+	if err != nil {
+		t.Errorf("failed due to %v", err)
+	}
+
+	expectPodNames := []types.NamespacedName{
+		{
+			Namespace: namespace,
+			Name:      "unavailableSts-0",
+		},
+	}
+
+	if !reflect.DeepEqual(podNames, expectPodNames) {
+		t.Errorf("The Pod names %v and expected name %v are not equal.", podNames, expectPodNames)
 	}
 
 }
