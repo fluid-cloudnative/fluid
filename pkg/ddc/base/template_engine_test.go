332a98db6de618fe6e6c72c8e569907f35241583
cheyang <cheyang@163.com>
2021-12-09 18:20:08 +0800

    Avoid high frequently retries, To #38346130
    Signed-off-by: cheyang <cheyang@163.com>

--- pkg/ddc/base/template_engine_test.go
+++ pkg/ddc/base/template_engine_test.go
@@ -17,6 +17,7 @@ package base_test
 
 import (
 	"context"
+	"os"
 
 	"reflect"
 	"testing"
@@ -80,6 +81,7 @@ var _ = Describe("TemplateEngine", func() {
 	BeforeEach(func() {
 		ctrl = gomock.NewController(GinkgoT())
 		impl = enginemock.NewMockImplement(ctrl)
+		os.Setenv("FLUID_SYNC_RETRY_DURATION", "0s")
 		t = base.NewTemplateEngine(impl, "default-test", fakeCtx)
 	})
 
