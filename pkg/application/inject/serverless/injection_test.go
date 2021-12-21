7ff0dd4c15fca56396908ceeec54ce45bf41c1ad
cheyang <cheyang@163.com>
2021-12-21 15:23:15 +0800

    Add injection, To #37688693
    Signed-off-by: cheyang <cheyang@163.com>

--- pkg/application/inject/serverless/injection_test.go
+++ pkg/application/inject/serverless/injection_test.go
@@ -20,16 +20,92 @@ func TestInjectObject(t *testing.T) {
 		wantErr  bool
 	}
 
+	hostPathCharDev := corev1.HostPathCharDev
+
 	testcases := []testCase{
 		{
 			name: "inject_success",
 			in: &corev1.Pod{
-				ObjectMeta: metav1.ObjectMeta{},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Image: "test",
+							Name:  "test",
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "dataset1",
+									MountPath: "/data",
+								},
+							},
+						},
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: "dataset1",
+							VolumeSource: corev1.VolumeSource{
+								PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+									ClaimName: "dataset1",
+									ReadOnly:  true,
+								},
+							},
+						},
+					},
+				},
 			},
 			template: common.ServerlessInjectionTemplate{
-				FuseContainer: corev1.Container{},
+				FuseContainer: corev1.Container{Name: "fuse",
+					Args: []string{
+						"-oroot_ns=jindo", "-okernel_cache", "-oattr_timeout=9000", "-oentry_timeout=9000",
+					},
+					Command: []string{"/entrypoint.sh"},
+					Image:   "test"},
+				VolumesToUpdate: []corev1.Volume{
+					{
+						Name: "dataset1",
+						VolumeSource: corev1.VolumeSource{
+							HostPath: &corev1.HostPathVolumeSource{
+								Path: "/runtime_mnt/dataset1",
+							},
+						},
+					},
+				},
+				VolumesToAdd: []corev1.Volume{
+					{
+						VolumeSource: corev1.VolumeSource{
+							HostPath: &corev1.HostPathVolumeSource{
+								Path: "/dev/fuse",
+								Type: &hostPathCharDev,
+							},
+						},
+					},
+				},
 			},
-			want:    &corev1.Pod{},
+			want: &corev1.Pod{
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name: "fuse",
+							Args: []string{
+								"-oroot_ns=jindo", "-okernel_cache", "-oattr_timeout=9000", "-oentry_timeout=9000",
+							},
+							Command: []string{"/entrypoint.sh"},
+							Image:   "test",
+						}, {
+							Image: "test",
+							Name:  "test",
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "dataset1",
+									MountPath: "/data",
+								},
+							},
+						},
+					},
+				},
+			},
 			wantErr: true,
 		},
 	}
