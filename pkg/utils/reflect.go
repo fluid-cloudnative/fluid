95ce575ada5fc71f445c3196993b7cbab32fc3f9
cheyang <cheyang@163.com>
2021-12-23 11:58:23 +0800

    Add testing for reflection, To #37688693
    Signed-off-by: cheyang <cheyang@163.com>

--- pkg/utils/reflect.go
+++ pkg/utils/reflect.go
@@ -1,43 +1,57 @@
 package utils
 
 import (
-	"fmt"
 	"reflect"
 )
 
-func FieldNameByType(original interface{}, targetObject interface{}) (targetNames []string, found bool) {
-	targetNames = []string{}
+type fieldNameByTypeSearcher struct {
+	targetNames []string
+}
+
+func FieldNameByType(original interface{}, targetObject interface{}) (targetNames []string) {
 	targetType := reflect.TypeOf(targetObject)
 	originalType := reflect.TypeOf(original)
-	found = fieldNameByType(originalType, targetNames, targetType)
+	search := &fieldNameByTypeSearcher{}
+	targetNames = search.fieldNameByType(originalType, "", targetType)
 	return
 }
 
+func (f *fieldNameByTypeSearcher) fieldNameByType(currentType reflect.Type, currentName string, targetType reflect.Type) (targetNames []string) {
 
-func fieldNameByType(original reflect.Type, targetNames []string, targetType reflect.Type) (match bool) {
+	log.V(1).Info("fieldNameByType enter", "currentType", currentType.String(), "currentName", currentName, "targetNames", f.targetNames)
 
-	switch original.Kind() {
-	// The first cases handle nested structures and translate them recursively
+	switch currentType.Kind() {
+	// The first cases handle nested structures and search them recursively
 
 	// If it is a pointer, interface we need to unwrap and call once again
-	case reflect.Ptr, reflect.Slice, reflect.Interface:
+	case reflect.Ptr, reflect.Interface:
 		// To get the actual value of the original we have to call Elem()
 		// At the same time this unwraps the pointer so we don't end up in
 		// an infinite recursion
-		originalType := original.Elem()
-		return fieldNameByType(originalType, targetNames, targetType)
+		originalType := currentType.Elem()
+		f.fieldNameByType(originalType, currentName, targetType)
 
-	// If it is a struct we translate each field
+	case reflect.Slice:
+		originalType := currentType.Elem()
+		f.fieldNameByType(originalType, currentName, targetType)
+
+	// If it is a struct we serarch each field
 	case reflect.Struct:
-		for i := 0; i < original.NumField(); i += 1 {
-			field := original.Field(i)
+		for i := 0; i < currentType.NumField(); i += 1 {
+			field := currentType.Field(i)
 			if field.Type == targetType {
-				targetNames = append(targetNames, field.Name)
+				f.targetNames = append(f.targetNames, field.Name)
 			} else {
-				return fieldNameByType(field.Type, targetNames, targetType)
+				f.fieldNameByType(field.Type, field.Name, targetType)
 			}
+		}
+	default:
+		if currentType == targetType {
+			f.targetNames = append(f.targetNames, currentName)
 		}
 	}
 
-	return match
+	log.V(1).Info("fieldNameByType exit", "currentType", currentType.String(), "currentName", currentName, "targetNames", f.targetNames)
+
+	return f.targetNames
 }
--- pkg/utils/reflect_test.go
+++ pkg/utils/reflect_test.go
@@ -9,18 +9,28 @@ import (
 
 func TestFieldNameByType(t *testing.T) {
 	type testCase struct {
-		name        string
-		original    interface{}
-		target      interface{}
-		expect      []string
-		expectFound bool
+		name     string
+		original interface{}
+		target   interface{}
+		expect   []string
 	}
 
 	testcases := []testCase{
 		{
+			name:     "Original is Ptr, and search pod",
+			original: &corev1.Pod{},
+			target:   corev1.PodSpec{},
+			expect: []string{
+				"Spec",
+			},
+		},
+		{
 			name:     "Original is Ptr",
 			original: &corev1.Pod{},
 			target:   corev1.Container{},
+			expect: []string{
+				"InitContainers", "Containers",
+			},
 		}, {
 			name:     "Both struct",
 			original: corev1.Pod{},
@@ -33,13 +43,10 @@ func TestFieldNameByType(t *testing.T) {
 	}
 
 	for _, testcase := range testcases {
-		got, found := FieldNameByType(testcase.original, testcase.target)
-		if found != testcase.expectFound {
-			t.Errorf("testcase %s failed due to expected %b, but got %b", testcase.name, testcase.expectFound, found)
-		}
+		got := FieldNameByType(testcase.original, testcase.target)
 
 		if !reflect.DeepEqual(got, testcase.expect) {
-			t.Errorf("testcase %s failed due to expected %v, but got %v", testcase.name, got, testcase.expect)
+			t.Errorf("testcase %s failed due to expected %v, but got %v", testcase.name, testcase.expect, got)
 		}
 	}
 
