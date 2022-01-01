8011cd7d68301b5f09d35092b7b137c0e4584002
cheyang <cheyang@163.com>
2022-01-01 20:52:16 +0800

    Add unstructured, To #37688693
    Signed-off-by: cheyang <cheyang@163.com>

--- pkg/utils/unstructured/unstructured_test.go
+++ pkg/utils/unstructured/unstructured_test.go
@@ -71,12 +71,6 @@ func TestLocateContainers(t *testing.T) {
 	}
 
 	for _, testcase := range testcases {
-		// got := ValueByType(testcase.original, testcase.target)
-
-		// result := differenceMap(got, testcase.expect)
-		// if len(result) > 0 {
-		// 	t.Errorf("testcase %s failed due to expected %v, but got %v", testcase.name, testcase.expect, got)
-		// }
 		obj := &unstructured.Unstructured{}
 
 		dec := k8syaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
@@ -87,6 +81,71 @@ func TestLocateContainers(t *testing.T) {
 
 		app := NewUnstructuredApplication(obj)
 		got, err := app.LocateContainers()
+		if err != nil {
+			t.Errorf("testcase %s failed due to error %v", testcase.name, err)
+		}
+
+		if !reflect.DeepEqual(got, testcase.expect) {
+			t.Errorf("testcase %s failed due to expected %v, but got %v", testcase.name, testcase.expect, got)
+		}
+
+	}
+
+}
+
+func TestLocateVolumes(t *testing.T) {
+	type testCase struct {
+		name    string
+		content string
+		expect  []common.Anchor
+	}
+
+	testcases := []testCase{
+		{
+			name:    "statefulset",
+			content: stsYaml,
+			expect: []common.Anchor{
+				UnstructuredAnchor{}, UnstructuredAnchor{},
+			},
+		},
+		{
+			name:    "tfjob",
+			content: tfjobYaml,
+			expect: []common.Anchor{
+				UnstructuredAnchor{}, UnstructuredAnchor{},
+			},
+		}, {
+			name:    "pytorch",
+			content: pytorchYaml,
+			expect: []common.Anchor{
+				UnstructuredAnchor{}, UnstructuredAnchor{},
+			},
+		}, {
+			name:    "argo",
+			content: argoYaml,
+			expect: []common.Anchor{
+				UnstructuredAnchor{}, UnstructuredAnchor{},
+			},
+		}, {
+			name:    "spark",
+			content: sparkYaml,
+			expect: []common.Anchor{
+				UnstructuredAnchor{}, UnstructuredAnchor{},
+			},
+		},
+	}
+
+	for _, testcase := range testcases {
+		obj := &unstructured.Unstructured{}
+
+		dec := k8syaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
+		_, gvk, err := dec.Decode([]byte(testcase.content), nil, obj)
+		if err != nil {
+			t.Errorf("Failed to decode due to %v and gvk is %v", err, gvk)
+		}
+
+		app := NewUnstructuredApplication(obj)
+		got, err := app.LocateVolumes()
 		if err != nil {
 			t.Errorf("testcase %s failed due to error %v", testcase.name, err)
 		}
