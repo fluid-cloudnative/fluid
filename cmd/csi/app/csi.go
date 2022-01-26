7bb49785ef7aaa0ceee7bee4ed609705c30b0954
TzZtzt <trafalgarz@outlook.com>
2022-01-26 14:40:11 +0800

    Refactor Fluid CSI Plugin (#1395)
    * Refactor Fluid CSI Plugin
    
    Signed-off-by: TrafalgarZZZ <trafalgarz@outlook.com>
    
    * Go fmt
    
    Signed-off-by: TrafalgarZZZ <trafalgarz@outlook.com>
    
    * Option to enable fuse mount point recovery
    
    Signed-off-by: TrafalgarZZZ <trafalgarz@outlook.com>
    
    * Refactor Fluid CSI Plugin
    
    - Move package 'mountinfo' to utils
    - Move package 'fuse' to plugin
    - Add registry functions
    
    Signed-off-by: TrafalgarZZZ <trafalgarz@outlook.com>
    
    * Minor fix for fuse recover period
    
    Signed-off-by: TrafalgarZZZ <trafalgarz@outlook.com>
    
    * Remove duplicate license
    
    Signed-off-by: TrafalgarZZZ <trafalgarz@outlook.com>
    
    * Declare registraion funcs to enable/disable feature gates
    
    Signed-off-by: TrafalgarZZZ <trafalgarz@outlook.com>
    
    * Add tests
    
    Signed-off-by: TrafalgarZZZ <trafalgarz@outlook.com>
    
    * Add tests
    
    Signed-off-by: TrafalgarZZZ <trafalgarz@outlook.com>
    
    * Fix copyright
    
    Signed-off-by: TrafalgarZZZ <trafalgarz@outlook.com>

--- cmd/csi/app/csi.go
+++ cmd/csi/app/csi.go
@@ -23,24 +23,16 @@ import (
 	"fmt"
 	"github.com/fluid-cloudnative/fluid"
 	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
-	csi "github.com/fluid-cloudnative/fluid/pkg/csi/fuse"
-	"github.com/fluid-cloudnative/fluid/pkg/csi/recover"
-	"github.com/fluid-cloudnative/fluid/pkg/utils"
-	"github.com/fluid-cloudnative/fluid/pkg/utils/kubelet"
+	"github.com/fluid-cloudnative/fluid/pkg/csi"
+	"github.com/fluid-cloudnative/fluid/pkg/csi/config"
 	"github.com/golang/glog"
 	"github.com/spf13/cobra"
-	"io/ioutil"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/util/wait"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/record"
 	"net/http"
 	"net/http/pprof"
 	"os"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"strconv"
 	"time"
 )
 
@@ -51,8 +43,6 @@ var (
 	pprofAddr         string
 	recoverFusePeriod int
 )
-
-const defaultKubeletTimeout = 10
 
 var scheme = runtime.NewScheme()
 
@@ -143,74 +133,21 @@ func handle() {
 	})
 
 	if err != nil {
-		panic(fmt.Sprintf("csi: unable to create controller recover due to error %v", err))
+		panic(fmt.Sprintf("csi: unable to create controller manager due to error %v", err))
+	}
+
+	config := config.Config{
+		NodeId:            nodeID,
+		Endpoint:          endpoint,
+		RecoverFusePeriod: recoverFusePeriod,
+	}
+
+	if err = csi.SetupWithManager(mgr, config); err != nil {
+		panic(fmt.Sprintf("unable to set up manager due to error %v", err))
 	}
 
 	ctx := ctrl.SetupSignalHandler()
-	go func() {
-		if err := mgr.Start(ctx); err != nil {
-			panic(fmt.Sprintf("unable to start controller recover due to error %v", err))
-		}
-	}()
-
-	if recoverFusePeriod > 0 {
-		if err := recoverStart(mgr.GetClient(), mgr.GetEventRecorderFor("FuseRecover")); err != nil {
-			panic(fmt.Sprintf("unable to start recover due to error %v", err))
-		}
+	if err = mgr.Start(ctx); err != nil {
+		panic(fmt.Sprintf("unable to start controller recover due to error %v", err))
 	}
-
-	d := csi.NewDriver(nodeID, endpoint, mgr.GetClient())
-	d.Run()
-}
-
-func recoverStart(kubeClient client.Client, recorder record.EventRecorder) (err error) {
-	glog.V(3).Infoln("start csi recover")
-	mountRoot, err := utils.GetMountRoot()
-	if err != nil {
-		return
-	}
-	glog.V(3).Infof("Get mount root: %s", mountRoot)
-
-	// get CSI sa token
-	tokenByte, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/token")
-	if err != nil {
-		panic(fmt.Errorf("in cluster mode, find token failed, error: %v", err))
-	}
-	token := string(tokenByte)
-
-	glog.V(3).Infoln("start kubelet client")
-	nodeIp := os.Getenv("NODE_IP")
-	kubeletClientCert := os.Getenv("KUBELET_CLIENT_CERT")
-	kubeletClientKey := os.Getenv("KUBELET_CLIENT_KEY")
-	var kubeletTimeout int
-	if os.Getenv("KUBELET_TIMEOUT") != "" {
-		if kubeletTimeout, err = strconv.Atoi(os.Getenv("KUBELET_TIMEOUT")); err != nil {
-			glog.Errorf("parse kubelet timeout error: %v", err)
-			return
-		}
-	} else {
-		kubeletTimeout = defaultKubeletTimeout
-	}
-	glog.V(3).Infof("get node ip: %s", nodeIp)
-	kubeletClient, err := kubelet.NewKubeletClient(&kubelet.KubeletClientConfig{
-		Address: nodeIp,
-		Port:    10250,
-		TLSClientConfig: rest.TLSClientConfig{
-			ServerName: "kubelet",
-			CertFile:   kubeletClientCert,
-			KeyFile:    kubeletClientKey,
-		},
-		BearerToken: token,
-		HTTPTimeout: time.Duration(kubeletTimeout) * time.Second,
-	})
-	if err != nil {
-		glog.Error(err)
-		return
-	}
-	m := recover.NewFuseRecoder(kubeClient, kubeletClient, recorder)
-	// do recovering at beginning
-	// recover set containerStat in memory, it's none when start
-	m.Recover()
-	go m.Run(recoverFusePeriod, wait.NeverStop)
-	return
 }
