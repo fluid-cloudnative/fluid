3bd357f010f3130b85fe82f3cf1f988af0780c95
TzZtzt <trafalgarz@outlook.com>
2022-05-31 19:17:25 +0800

    Add tests
    Signed-off-by: TzZtzt <trafalgarz@outlook.com>

---
+++ pkg/utils/capabilities_test.go
@@ -0,0 +1,51 @@
+package utils
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	"reflect"
+	"testing"
+)
+
+func TestTrimCapabilities(t *testing.T) {
+	type args struct {
+		inputs       []corev1.Capability
+		excludeNames []string
+	}
+	tests := []struct {
+		name        string
+		args        args
+		wantOutputs []corev1.Capability
+	}{
+		{
+			name: "SYS_ADMIN_only",
+			args: args{
+				inputs:       []corev1.Capability{"SYS_ADMIN"},
+				excludeNames: []string{"SYS_ADMIN"},
+			},
+			wantOutputs: []corev1.Capability{},
+		},
+		{
+			name: "with_other_capabilities",
+			args: args{
+				inputs:       []corev1.Capability{"SYS_ADMIN", "CHOWN"},
+				excludeNames: []string{"SYS_ADMIN"},
+			},
+			wantOutputs: []corev1.Capability{"CHOWN"},
+		},
+		{
+			name: "exclude_multiple_capabilities",
+			args: args{
+				inputs:       []corev1.Capability{"SYS_ADMIN", "CHOWN", "SETPCAP"},
+				excludeNames: []string{"SYS_ADMIN", "SETPCAP"},
+			},
+			wantOutputs: []corev1.Capability{"CHOWN"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotOutputs := TrimCapabilities(tt.args.inputs, tt.args.excludeNames); !reflect.DeepEqual(gotOutputs, tt.wantOutputs) {
+				t.Errorf("TrimCapabilities() = %v, want %v", gotOutputs, tt.wantOutputs)
+			}
+		})
+	}
+}
