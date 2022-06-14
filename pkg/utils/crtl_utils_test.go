df5e11ee4e4af4512dc7094c499167ad45cf74eb
TzZtzt <trafalgarz@outlook.com>
2022-06-14 16:42:27 +0800

    Add unit tests
    Signed-off-by: TzZtzt <trafalgarz@outlook.com>

--- pkg/utils/crtl_utils_test.go
+++ pkg/utils/crtl_utils_test.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
+	apierrs "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"testing"
 	"time"
 )
@@ -262,6 +264,70 @@ func TestContainsSubString(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := ContainsSubString(tt.args.slice, tt.args.s); got != tt.want {
 				t.Errorf("ContainsSubString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIgnoreAlreadyExists(t *testing.T) {
+	tests := []struct {
+		name    string
+		err     error
+		wantErr bool
+	}{
+		{
+			name: "already_exists_error",
+			err: apierrs.NewAlreadyExists(schema.GroupResource{
+				Group:    "",
+				Resource: "pod",
+			}, "mypod"),
+			wantErr: false,
+		},
+		{
+			name: "not_found_error",
+			err: apierrs.NewNotFound(schema.GroupResource{
+				Group:    "",
+				Resource: "pod",
+			}, "mypod"),
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := IgnoreAlreadyExists(tt.err); (err != nil) != tt.wantErr {
+				t.Errorf("IgnoreAlreadyExists() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestIgnoreNotFound(t *testing.T) {
+	tests := []struct {
+		name    string
+		err     error
+		wantErr bool
+	}{
+		{
+			name: "already_exists_error",
+			err: apierrs.NewAlreadyExists(schema.GroupResource{
+				Group:    "",
+				Resource: "pod",
+			}, "mypod"),
+			wantErr: true,
+		},
+		{
+			name: "not_found_error",
+			err: apierrs.NewNotFound(schema.GroupResource{
+				Group:    "",
+				Resource: "pod",
+			}, "mypod"),
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := IgnoreNotFound(tt.err); (err != nil) != tt.wantErr {
+				t.Errorf("IgnoreNotFound() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
