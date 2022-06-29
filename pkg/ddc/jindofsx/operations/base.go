e31bd8a3950ed3787d2a90cdd1b528ee7f38cd76
frankleaf <62129564+frankleaf@users.noreply.github.com>
2022-06-29 18:05:55 +0800

    Fix JindoFSx dataset ufsTotal issue (#1957)
    * add dataset ufsTotal
    
    Signed-off-by: frankleaf <frank.wt@alibaba-inc.com>
    
    * fix compile
    
    Signed-off-by: frankleaf <frank.wt@alibaba-inc.com>

--- pkg/ddc/jindofsx/operations/base.go
+++ pkg/ddc/jindofsx/operations/base.go
@@ -64,6 +64,27 @@ func (a JindoFileUtils) exec(command []string, verbose bool) (stdout string, std
 	return
 }
 
+// exec with timeout
+func (a JindoFileUtils) execWithTimeOut(command []string, verbose bool, second int64) (stdout string, stderr string, err error) {
+	ctx, cancel := context.WithTimeout(context.TODO(), time.Second*time.Duration(second))
+	ch := make(chan string, 1)
+	defer cancel()
+
+	go func() {
+		stdout, stderr, err = a.execWithoutTimeout(command, verbose)
+		ch <- "done"
+	}()
+
+	select {
+	case <-ch:
+		a.log.V(1).Info("execute in time", "command", command)
+	case <-ctx.Done():
+		err = fmt.Errorf("timeout when executing %v", command)
+	}
+
+	return
+}
+
 // execWithoutTimeout
 func (a JindoFileUtils) execWithoutTimeout(command []string, verbose bool) (stdout string, stderr string, err error) {
 	stdout, stderr, err = kubeclient.ExecCommandInContainer(a.podName, a.container, a.namespace, command)
@@ -140,14 +161,15 @@ func (a JindoFileUtils) Mount(mountName string, ufsPath string) (err error) {
 	return nil
 }
 
-func (a JindoFileUtils) GetUfsTotalSize(url string, useStsSecret bool) (summary string, err error) {
+func (a JindoFileUtils) GetUfsTotalSize(url string) (summary string, err error) {
 	var (
 		command = []string{"hadoop", "fs", "-count", url}
 		stdout  string
 		stderr  string
 	)
 
-	stdout, stderr, err = a.execWithoutTimeout(command, false)
+	// default 2min
+	stdout, stderr, err = a.execWithTimeOut(command, false, 120)
 
 	str := strings.Fields(stdout)
 
