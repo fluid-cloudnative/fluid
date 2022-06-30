e24d4e8b35b94333b5fea0dff391b352ed468ea0
cheyang <cheyang@163.com>
2022-06-30 12:17:56 +0800

    Add script, To #40605168
    Signed-off-by: cheyang <cheyang@163.com>

--- pkg/ddc/jindofsx/sync_runtime.go
+++ pkg/ddc/jindofsx/sync_runtime.go
@@ -17,13 +17,27 @@ limitations under the License.
 package jindofsx
 
 import (
+	"context"
+	"reflect"
+
+	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
+	"github.com/fluid-cloudnative/fluid/pkg/ctrl"
+	fluiderrs "github.com/fluid-cloudnative/fluid/pkg/errors"
 	cruntime "github.com/fluid-cloudnative/fluid/pkg/runtime"
+	"github.com/fluid-cloudnative/fluid/pkg/utils/kubeclient"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/util/retry"
 )
 
 // SyncRuntime syncs the runtime spec
 func (e *JindoFSxEngine) SyncRuntime(ctx cruntime.ReconcileRequestContext) (changed bool, err error) {
+	runtime, err := e.getRuntime()
+	if err != nil {
+		return changed, err
+	}
 
-	masterChanged, err := e.syncMasterSpec(ctx)
+	// 1. sync master
+	masterChanged, err := e.syncMasterSpec(ctx, runtime)
 	if err != nil {
 		return
 	}
@@ -32,7 +46,8 @@ func (e *JindoFSxEngine) SyncRuntime(ctx cruntime.ReconcileRequestContext) (chan
 		return masterChanged, err
 	}
 
-	workerChanged, err := e.syncWorkerSpec(ctx)
+	// 2. sync workers
+	workerChanged, err := e.syncWorkerSpec(ctx, runtime)
 	if err != nil {
 		return
 	}
@@ -41,7 +56,8 @@ func (e *JindoFSxEngine) SyncRuntime(ctx cruntime.ReconcileRequestContext) (chan
 		return workerChanged, err
 	}
 
-	fuseChanged, err := e.syncFuseSpec(ctx)
+	// 3. sync fuse
+	fuseChanged, err := e.syncFuseSpec(ctx, runtime)
 	if err != nil {
 		return
 	}
@@ -53,31 +69,134 @@ func (e *JindoFSxEngine) SyncRuntime(ctx cruntime.ReconcileRequestContext) (chan
 	return
 }
 
-func (e *JindoFSxEngine) syncMasterSpec(ctx cruntime.ReconcileRequestContext) (changed bool, err error) {
+func (e *JindoFSxEngine) syncMasterSpec(ctx cruntime.ReconcileRequestContext, runtime *datav1alpha1.JindoRuntime) (changed bool, err error) {
 	e.Log.V(1).Info("syncMasterSpec")
-	runtime, err := e.getRuntime()
-	if err != nil {
-		return changed, err
-	}
 	if runtime.Spec.Master.Disabled {
 		return
 	}
+
+	err = retry.RetryOnConflict(retry.DefaultBackoff, func() error {
+		master, err := kubeclient.GetStatefulSet(e.Client, e.getMasterName(), e.namespace)
+		if err != nil {
+			return err
+		}
+
+		if len(runtime.Spec.Master.Resources.Limits) == 0 && len(runtime.Spec.Master.Resources.Requests) == 0 {
+			e.Log.V(1).Info("The resource requirement is not set, skip")
+			return nil
+		}
+
+		masterToUpdate := master.DeepCopy()
+		if len(masterToUpdate.Spec.Template.Spec.Containers) == 1 {
+			if !reflect.DeepEqual(masterToUpdate.Spec.Template.Spec.Containers[0].Resources, runtime.Spec.Master.Resources) {
+				e.Log.Info("The resource requirement is different.", "worker sts", masterToUpdate.Spec.Template.Spec.Containers[0].Resources,
+					"runtime", runtime.Spec.Master.Resources)
+				masterToUpdate.Spec.Template.Spec.Containers[0].Resources = runtime.Spec.Master.Resources
+				changed = true
+			} else {
+				e.Log.V(1).Info("The resource requirement is the same.")
+			}
+			if changed {
+				err = e.Client.Update(context.TODO(), masterToUpdate)
+				if err != nil {
+					e.Log.Error(err, "Failed to update the sts spec")
+				}
+			}
+		}
+
+		return err
+	})
+
+	if fluiderrs.IsDeprecated(err) {
+		e.Log.Info("Warning: the current runtime is created by runtime controller before v0.7.0, update specs are not supported. To support these features, please create a new dataset", "details", err)
+		return false, nil
+	}
+
 	return
 }
 
-func (e *JindoFSxEngine) syncWorkerSpec(ctx cruntime.ReconcileRequestContext) (changed bool, err error) {
+func (e *JindoFSxEngine) syncWorkerSpec(ctx cruntime.ReconcileRequestContext, runtime *datav1alpha1.JindoRuntime) (changed bool, err error) {
 	e.Log.V(1).Info("syncWorkerSpec")
-	runtime, err := e.getRuntime()
-	if err != nil {
-		return changed, err
-	}
 	if runtime.Spec.Worker.Disabled {
 		return
 	}
+	err = retry.RetryOnConflict(retry.DefaultBackoff, func() error {
+		workers, err := ctrl.GetWorkersAsStatefulset(e.Client,
+			types.NamespacedName{Namespace: e.namespace, Name: e.getWorkerName()})
+		if err != nil {
+			return err
+		}
+
+		if len(runtime.Spec.Worker.Resources.Limits) == 0 && len(runtime.Spec.Worker.Resources.Requests) == 0 {
+			e.Log.V(1).Info("The resource requirement is not set, skip")
+			return nil
+		}
+
+		workersToUpdate := workers.DeepCopy()
+		if len(workersToUpdate.Spec.Template.Spec.Containers) == 1 {
+			if !reflect.DeepEqual(workersToUpdate.Spec.Template.Spec.Containers[0].Resources, runtime.Spec.Worker.Resources) {
+				e.Log.Info("The resource requirement is different.", "worker sts", workersToUpdate.Spec.Template.Spec.Containers[0].Resources,
+					"runtime", runtime.Spec.Worker.Resources)
+				workersToUpdate.Spec.Template.Spec.Containers[0].Resources = runtime.Spec.Worker.Resources
+				changed = true
+			} else {
+				e.Log.Info("The resource requirement is the same.")
+			}
+
+			if changed {
+				err = e.Client.Update(context.TODO(), workersToUpdate)
+				if err != nil {
+					e.Log.Error(err, "Failed to update the sts spec")
+				}
+			}
+		}
+
+		return err
+	})
+
+	if fluiderrs.IsDeprecated(err) {
+		e.Log.Info("Warning: the current runtime is created by runtime controller before v0.7.0, update specs are not supported. To support these features, please create a new dataset", "details", err)
+		return false, nil
+	}
+
 	return
 }
 
-func (e *JindoFSxEngine) syncFuseSpec(ctx cruntime.ReconcileRequestContext) (changed bool, err error) {
+func (e *JindoFSxEngine) syncFuseSpec(ctx cruntime.ReconcileRequestContext, runtime *datav1alpha1.JindoRuntime) (changed bool, err error) {
 	e.Log.V(1).Info("syncFuseSpec")
+	err = retry.RetryOnConflict(retry.DefaultBackoff, func() error {
+		fuses, err := kubeclient.GetDaemonset(e.Client, e.getFuseName(), e.namespace)
+		if err != nil {
+			return err
+		}
+
+		if len(runtime.Spec.Fuse.Resources.Limits) == 0 && len(runtime.Spec.Fuse.Resources.Requests) == 0 {
+			e.Log.V(1).Info("The resource requirement is not set, skip")
+			return nil
+		}
+
+		fusesToUpdate := fuses.DeepCopy()
+		if len(fusesToUpdate.Spec.Template.Spec.Containers) == 1 {
+			if !reflect.DeepEqual(fusesToUpdate.Spec.Template.Spec.Containers[0].Resources, runtime.Spec.Fuse.Resources) {
+				e.Log.Info("The resource requirement is different.", "fuse ds", fuses.Spec.Template.Spec.Containers[0].Resources,
+					"runtime", runtime.Spec.Fuse.Resources)
+				fusesToUpdate.Spec.Template.Spec.Containers[0].Resources = runtime.Spec.Fuse.Resources
+			}
+
+			if changed {
+				err = e.Client.Update(context.TODO(), fusesToUpdate)
+				if err != nil {
+					e.Log.Error(err, "Failed to update the sts spec")
+				}
+			}
+		}
+
+		return err
+	})
+
+	if fluiderrs.IsDeprecated(err) {
+		e.Log.Info("Warning: the current runtime is created by runtime controller before v0.7.0, update specs are not supported. To support these features, please create a new dataset", "details", err)
+		return false, nil
+	}
 	return
 }
