bbacaec6302ed22282c754c23c2c91022f336096
frankleaf <frank.wt@alibaba-inc.com>
2022-07-06 20:38:51 +0800

    fix container network
    Signed-off-by: frankleaf <frank.wt@alibaba-inc.com>

--- pkg/ddc/jindofsx/types.go
+++ pkg/ddc/jindofsx/types.go
@@ -79,6 +79,7 @@ type Worker struct {
 	Tolerations      []v1.Toleration   `yaml:"tolerations,omitempty"`
 	// Affinity         v1.Affinity       `yaml:"affinity,omitempty"`
 	Labels map[string]string `yaml:"labels,omitempty"`
+	Path   string            `yaml:"dataPath"`
 }
 
 type Ports struct {
