62c40e94502928f303a4430f59058726109f4931
frankleaf <62129564+frankleaf@users.noreply.github.com>
2022-07-08 16:44:07 +0800

    Fix JindoFSx master service issue (#1984)
    * fix jindofsx service
    
    Signed-off-by: frankleaf <frank.wt@alibaba-inc.com>
    
    * fix jindofsx service
    
    Signed-off-by: frankleaf <frank.wt@alibaba-inc.com>
    
    * fix ServiceCount
    
    Signed-off-by: frankleaf <frank.wt@alibaba-inc.com>
    
    * fix ServiceCount
    
    Signed-off-by: frankleaf <frank.wt@alibaba-inc.com>
    
    * fix ServiceCount
    
    Signed-off-by: frankleaf <frank.wt@alibaba-inc.com>

--- pkg/ddc/jindofsx/types.go
+++ pkg/ddc/jindofsx/types.go
@@ -68,6 +68,7 @@ type Master struct {
 	DnsServer           string            `yaml:"dnsServer,omitempty"`
 	NameSpace           string            `yaml:"namespace,omitempty"`
 	Labels              map[string]string `yaml:"labels,omitempty"`
+	ServiceCount        int               `yaml:"svccount"`
 }
 
 type Worker struct {
