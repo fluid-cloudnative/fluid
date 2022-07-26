f19b8304e012945d7350db9fb507ee02c0693b6d
TzZtzt <trafalgarz@outlook.com>
2022-07-26 16:57:52 +0800

    Fix runtime identity yaml marshal bug (#2024)
    Signed-off-by: dongyun.xzh <dongyun.xzh@alibaba-inc.com>

--- pkg/common/runtime_identity.go
+++ pkg/common/runtime_identity.go
@@ -1,6 +1,6 @@
 package common
 
 type RuntimeIdentity struct {
-	Namespace string `yaml:"namespace"`
-	Name      string `yaml:"name"`
+	Namespace string `yaml:"namespace" json:"namespace"`
+	Name      string `yaml:"name" json:"name"`
 }
