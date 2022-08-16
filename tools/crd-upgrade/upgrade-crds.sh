dc1b30e6e32386171776ab5b85e13120cd306a0a
TzZtzt <trafalgarz@outlook.com>
2022-08-16 19:45:18 +0800

    Support crd auto upgrader with helm hook (#2062)
    * Support crd auto upgrader with helm hook
    
    Signed-off-by: dongyun.xzh <dongyun.xzh@alibaba-inc.com>
    
    * Add empty line
    
    Signed-off-by: dongyun.xzh <dongyun.xzh@alibaba-inc.com>
    
    * Add upgrade crd scirpts into docker image
    
    Signed-off-by: dongyun.xzh <dongyun.xzh@alibaba-inc.com>
    
    * Add empty line to end of script
    
    Signed-off-by: dongyun.xzh <dongyun.xzh@alibaba-inc.com>
    
    * Rename template folder to "upgrade"
    
    Signed-off-by: dongyun.xzh <dongyun.xzh@alibaba-inc.com>
    
    * Enable helm chart hook when upgrading only
    
    Signed-off-by: dongyun.xzh <dongyun.xzh@alibaba-inc.com>
    
    Signed-off-by: dongyun.xzh <dongyun.xzh@alibaba-inc.com>

---
+++ tools/crd-upgrade/upgrade-crds.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+for crdfile in $(find /fluid/crds/*.yaml);
+do
+  crdshort=${crdfile#*_}
+  if [[ $(kubectl get --ignore-not-found -f $crdfile | wc -l) -gt 0 ]]; then
+    echo "$crdshort founded, replacing its crd..."
+    kubectl replace -f $crdfile
+  else
+    echo "$crdshort not founded, applying its crd..."
+    kubectl apply -f $crdfile
+  fi
+done
