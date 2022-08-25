f163c5e4fab8db70488d4cf44bd6574e092ca214
cheyang <cheyang@163.com>
2022-08-25 11:32:50 +0800

    Support scale out runtime controller on demand, To #43759447
    Signed-off-by: cheyang <cheyang@163.com>

--- pkg/runtime/runtime_controllers_test.go
+++ pkg/runtime/runtime_controllers_test.go
@@ -21,15 +21,14 @@ import (
 	"context"
 	"testing"
 
-	appsv1 "k8s.io/api/apps/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	utilpointer "k8s.io/utils/pointer"
-
 	"github.com/fluid-cloudnative/fluid/pkg/common"
 	"github.com/fluid-cloudnative/fluid/pkg/utils/fake"
 	"github.com/go-logr/logr"
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	utilpointer "k8s.io/utils/pointer"
 )
 
 func Test_scaleoutRuntimeControllerIfNeeded(t *testing.T) {
@@ -57,6 +56,42 @@ func Test_scaleoutRuntimeControllerIfNeeded(t *testing.T) {
 			}, wantErr: true,
 			wantScale:  false,
 			wantGotErr: true,
+		}, {
+			name: "scale to 1 without annotations",
+			args: args{
+				key: types.NamespacedName{
+					Namespace: common.NamespaceFluidSystem,
+					Name:      "unknown-Controller",
+				},
+				log: fake.NullLogger(),
+			}, wantErr: false,
+			wantScale:    true,
+			wantGotErr:   false,
+			wantReplicas: 1,
+		}, {
+			name: "scale to 1 annotations 3",
+			args: args{
+				key: types.NamespacedName{
+					Namespace: common.NamespaceFluidSystem,
+					Name:      "goosefsruntime-controller",
+				},
+				log: fake.NullLogger(),
+			}, wantErr: false,
+			wantScale:    true,
+			wantGotErr:   false,
+			wantReplicas: 3,
+		}, {
+			name: "scale to 1 annotations 0",
+			args: args{
+				key: types.NamespacedName{
+					Namespace: common.NamespaceFluidSystem,
+					Name:      "juicefsruntime-controller",
+				},
+				log: fake.NullLogger(),
+			}, wantErr: false,
+			wantScale:    true,
+			wantGotErr:   false,
+			wantReplicas: 1,
 		},
 	}
 
@@ -82,6 +117,9 @@ func Test_scaleoutRuntimeControllerIfNeeded(t *testing.T) {
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      "juicefsruntime-controller",
 				Namespace: common.NamespaceFluidSystem,
+				Annotations: map[string]string{
+					common.RuntimeControllerReplicas: "0",
+				},
 			}, Spec: appsv1.DeploymentSpec{
 				Replicas: utilpointer.Int32Ptr(0),
 			},
@@ -92,6 +130,13 @@ func Test_scaleoutRuntimeControllerIfNeeded(t *testing.T) {
 				Annotations: map[string]string{
 					common.RuntimeControllerReplicas: "3",
 				},
+			}, Spec: appsv1.DeploymentSpec{
+				Replicas: utilpointer.Int32Ptr(0),
+			},
+		}, {
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "unknown-Controller",
+				Namespace: common.NamespaceFluidSystem,
 			}, Spec: appsv1.DeploymentSpec{
 				Replicas: utilpointer.Int32Ptr(0),
 			},
@@ -129,6 +174,137 @@ func Test_scaleoutRuntimeControllerIfNeeded(t *testing.T) {
 				}
 			}
 
+		})
+	}
+}
+
+func TestScaleoutRuntimeContollerOnDemand(t *testing.T) {
+	type args struct {
+		key types.NamespacedName
+		log logr.Logger
+	}
+	tests := []struct {
+		name               string
+		args               args
+		wantControllerName string
+		wantScaleout       bool
+		wantErr            bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "notFound",
+			args: args{
+				key: types.NamespacedName{
+					Namespace: "default",
+					Name:      "notFoundController",
+				},
+				log: fake.NullLogger(),
+			}, wantErr: true,
+			wantControllerName: "",
+			wantScaleout:       false,
+		}, {
+			name: "unknown",
+			args: args{
+				key: types.NamespacedName{
+					Namespace: common.NamespaceFluidSystem,
+					Name:      "unknown-Controller",
+				},
+				log: fake.NullLogger(),
+			},
+			wantErr:            true,
+			wantControllerName: "",
+			wantScaleout:       false,
+		}, {
+			name: "scale to 1 annotations 3",
+			args: args{
+				key: types.NamespacedName{
+					Namespace: common.NamespaceFluidSystem,
+					Name:      "goosefsruntime-controller",
+				},
+				log: fake.NullLogger(),
+			}, wantErr: true,
+			wantControllerName: "",
+			wantScaleout:       false,
+		}, {
+			name: "scale to 1 annotations 0",
+			args: args{
+				key: types.NamespacedName{
+					Namespace: common.NamespaceFluidSystem,
+					Name:      "juicefsruntime-controller",
+				},
+				log: fake.NullLogger(),
+			}, wantErr: true,
+			wantControllerName: "",
+			wantScaleout:       false,
+		},
+	}
+
+	objs := []runtime.Object{}
+	s := runtime.NewScheme()
+	_ = appsv1.AddToScheme(s)
+	deployments := []*appsv1.Deployment{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "alluxioruntime-controller",
+				Namespace: common.NamespaceFluidSystem,
+			}, Spec: appsv1.DeploymentSpec{
+				Replicas: utilpointer.Int32Ptr(0),
+			},
+		}, {
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "jindoruntime-controller",
+				Namespace: common.NamespaceFluidSystem,
+			}, Spec: appsv1.DeploymentSpec{
+				Replicas: utilpointer.Int32Ptr(1),
+			},
+		}, {
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "juicefsruntime-controller",
+				Namespace: common.NamespaceFluidSystem,
+				Annotations: map[string]string{
+					common.RuntimeControllerReplicas: "0",
+				},
+			}, Spec: appsv1.DeploymentSpec{
+				Replicas: utilpointer.Int32Ptr(0),
+			},
+		}, {
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "goosefsruntime-controller",
+				Namespace: common.NamespaceFluidSystem,
+				Annotations: map[string]string{
+					common.RuntimeControllerReplicas: "3",
+				},
+			}, Spec: appsv1.DeploymentSpec{
+				Replicas: utilpointer.Int32Ptr(0),
+			},
+		}, {
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "unknown-Controller",
+				Namespace: common.NamespaceFluidSystem,
+			}, Spec: appsv1.DeploymentSpec{
+				Replicas: utilpointer.Int32Ptr(0),
+			},
+		},
+	}
+
+	for _, deployment := range deployments {
+		objs = append(objs, deployment)
+	}
+
+	fakeClient := fake.NewFakeClientWithScheme(s, objs...)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotControllerName, gotScaleout, err := ScaleoutRuntimeContollerOnDemand(fakeClient, tt.args.key, tt.args.log)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ScaleoutRuntimeContollerOnDemand() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotControllerName != tt.wantControllerName {
+				t.Errorf("ScaleoutRuntimeContollerOnDemand() gotControllerName = %v, want %v", gotControllerName, tt.wantControllerName)
+			}
+			if gotScaleout != tt.wantScaleout {
+				t.Errorf("ScaleoutRuntimeContollerOnDemand() gotScaleout = %v, want %v", gotScaleout, tt.wantScaleout)
+			}
 		})
 	}
 }
