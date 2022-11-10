916539d41263f724783a7f6bc5a594b6129fc5fe
sichen.zhao <sichen.zhao@outlook.com>
2022-11-10 22:53:13 +0800

    fix the alluxio logic of transformResourcesForWorker Signed-off-by: sichen.zhao <sichen.zhao@outlook.com>

--- pkg/ddc/alluxio/transform_resources_test.go
+++ pkg/ddc/alluxio/transform_resources_test.go
@@ -25,6 +25,8 @@ import (
 	"github.com/fluid-cloudnative/fluid/pkg/utils/fake"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 func TestTransformResourcesForMaster(t *testing.T) {
@@ -162,6 +164,10 @@ func TestTransformResourcesForWorkerNoValue(t *testing.T) {
 		alluxioValue *Alluxio
 	}{
 		{&datav1alpha1.AlluxioRuntime{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test",
+				Namespace: "default",
+			},
 			Spec: datav1alpha1.AlluxioRuntimeSpec{
 				TieredStore: datav1alpha1.TieredStore{},
 			},
@@ -170,9 +176,23 @@ func TestTransformResourcesForWorkerNoValue(t *testing.T) {
 		}},
 	}
 	for _, test := range tests {
-		engine := &AlluxioEngine{Log: fake.NullLogger()}
+		engine := &AlluxioEngine{
+			Log: fake.NullLogger(),
+			name: "test",
+			namespace: "test",
+		}
 		engine.runtimeInfo, _ = base.BuildRuntimeInfo("test", "test", "alluxio", test.runtime.Spec.TieredStore)
-		engine.transformResourcesForWorker(test.runtime, test.alluxioValue)
+		runtimeObjs := []runtime.Object{}
+		runtimeObjs = append(runtimeObjs, test.runtime.DeepCopy())
+		s := runtime.NewScheme()
+		s.AddKnownTypes(datav1alpha1.GroupVersion, test.runtime)
+		_ = corev1.AddToScheme(s)
+		client := fake.NewFakeClientWithScheme(s, runtimeObjs...)
+		engine.Client = client
+		err := engine.transformResourcesForWorker(test.runtime, test.alluxioValue)
+		if err != nil {
+			t.Errorf("got err %v", err)
+		}
 		if result, found := test.alluxioValue.Worker.Resources.Limits[corev1.ResourceMemory]; found {
 			t.Errorf("expected nil, got %v", result)
 		}
@@ -187,6 +207,10 @@ func TestTransformResourcesForWorkerWithTieredStore(t *testing.T) {
 		alluxioValue *Alluxio
 	}{
 		{&datav1alpha1.AlluxioRuntime{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test",
+				Namespace: "test",
+			},
 			Spec: datav1alpha1.AlluxioRuntimeSpec{
 				TieredStore: datav1alpha1.TieredStore{
 					Levels: []datav1alpha1.Level{{
@@ -200,14 +224,29 @@ func TestTransformResourcesForWorkerWithTieredStore(t *testing.T) {
 		}},
 	}
 	for _, test := range tests {
-		engine := &AlluxioEngine{Log: fake.NullLogger()}
+		engine := &AlluxioEngine{
+			Log: fake.NullLogger(),
+			name: "test",
+			namespace: "test",
+		}
 		engine.runtimeInfo, _ = base.BuildRuntimeInfo("test", "test", "alluxio", test.runtime.Spec.TieredStore)
-		engine.transformResourcesForWorker(test.runtime, test.alluxioValue)
-		if result, found := test.alluxioValue.Worker.Resources.Limits[corev1.ResourceMemory]; found {
-			t.Errorf("expected nil, got %v", result)
+		runtimeObjs := []runtime.Object{}
+		runtimeObjs = append(runtimeObjs, test.runtime.DeepCopy())
+		s := runtime.NewScheme()
+		s.AddKnownTypes(datav1alpha1.GroupVersion, test.runtime)
+		_ = corev1.AddToScheme(s)
+		client := fake.NewFakeClientWithScheme(s, runtimeObjs...)
+		engine.Client = client
+		err := engine.transformResourcesForWorker(test.runtime, test.alluxioValue)
+		t.Log(err)
+		if err != nil {
+			t.Errorf("expected no err, got err %v", err)
 		}
 		if test.alluxioValue.Worker.Resources.Requests[corev1.ResourceMemory] != "20Gi" {
 			t.Errorf("expected 20Gi, got %v", test.alluxioValue.Worker.Resources.Requests[corev1.ResourceMemory])
+		}
+		if result, found := test.alluxioValue.Worker.Resources.Limits[corev1.ResourceMemory]; found {
+			t.Errorf("expected nil, got %v", result)
 		}
 	}
 }
@@ -222,13 +261,26 @@ func TestTransformResourcesForWorkerWithValue(t *testing.T) {
 	resources.Requests[corev1.ResourceMemory] = resource.MustParse("1Gi")
 	resources.Requests[corev1.ResourceCPU] = resource.MustParse("500m")
 
+	resources1 := corev1.ResourceRequirements{}
+	resources1.Limits = make(corev1.ResourceList)
+	resources1.Limits[corev1.ResourceMemory] = resource.MustParse("20Gi")
+	resources1.Limits[corev1.ResourceCPU] = resource.MustParse("500m")
+	resources1.Requests = make(corev1.ResourceList)
+	resources1.Requests[corev1.ResourceMemory] = resource.MustParse("1Gi")
+	resources1.Requests[corev1.ResourceCPU] = resource.MustParse("500m")
+
 	result := resource.MustParse("20Gi")
 
 	var tests = []struct {
 		runtime      *datav1alpha1.AlluxioRuntime
 		alluxioValue *Alluxio
+		wantRes []string
 	}{
 		{&datav1alpha1.AlluxioRuntime{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test",
+				Namespace: "test",
+			},
 			Spec: datav1alpha1.AlluxioRuntimeSpec{
 				Worker: datav1alpha1.AlluxioCompTemplateSpec{
 					Resources: resources,
@@ -247,18 +299,60 @@ func TestTransformResourcesForWorkerWithValue(t *testing.T) {
 			Properties: map[string]string{},
 			Master:     Master{},
 			JobMaster:  JobMaster{},
+		},[]string{
+			"err", "2Gi", "1Gi",
+		}},
+		{&datav1alpha1.AlluxioRuntime{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test",
+				Namespace: "test",
+			},
+			Spec: datav1alpha1.AlluxioRuntimeSpec{
+				Worker: datav1alpha1.AlluxioCompTemplateSpec{
+					Resources: resources1,
+				},
+				JobWorker: datav1alpha1.AlluxioCompTemplateSpec{
+					Resources: resources1,
+				},
+				TieredStore: datav1alpha1.TieredStore{
+					Levels: []datav1alpha1.Level{{
+						MediumType: common.Memory,
+						Quota:      &result,
+					}},
+				},
+			},
+		}, &Alluxio{
+			Properties: map[string]string{},
+			Master:     Master{},
+			JobMaster:  JobMaster{},
+		},[]string{
+			"nil", "20Gi", "20Gi",
 		}},
 	}
 	for _, test := range tests {
-		engine := &AlluxioEngine{Log: fake.NullLogger()}
-		engine.runtimeInfo, _ = base.BuildRuntimeInfo("test", "test", "alluxio", test.runtime.Spec.TieredStore)
-		engine.UnitTest = true
-		engine.transformResourcesForWorker(test.runtime, test.alluxioValue)
-		if test.alluxioValue.Worker.Resources.Limits[corev1.ResourceMemory] != "22Gi" {
-			t.Errorf("expected 22Gi, got %v", test.alluxioValue.Worker.Resources.Limits[corev1.ResourceMemory])
+		engine := &AlluxioEngine{
+			Log: fake.NullLogger(),
+			name: "test",
+			namespace: "test",
 		}
-		if test.alluxioValue.Worker.Resources.Requests[corev1.ResourceMemory] != "21Gi" {
-			t.Errorf("expected 21Gi, got %v", test.alluxioValue.Worker.Resources.Requests[corev1.ResourceMemory])
+		engine.runtimeInfo, _ = base.BuildRuntimeInfo("test", "test", "alluxio", test.runtime.Spec.TieredStore)
+		runtimeObjs := []runtime.Object{}
+		runtimeObjs = append(runtimeObjs, test.runtime.DeepCopy())
+		s := runtime.NewScheme()
+		s.AddKnownTypes(datav1alpha1.GroupVersion, test.runtime)
+		_ = corev1.AddToScheme(s)
+		client := fake.NewFakeClientWithScheme(s, runtimeObjs...)
+		engine.Client = client
+		engine.UnitTest = true
+		err := engine.transformResourcesForWorker(test.runtime, test.alluxioValue)
+		if (err != nil && test.wantRes[0] != "err") || (err == nil && test.wantRes[0] != "nil") {
+			t.Errorf("expected %v, got %v", test.wantRes[0], err)
+		}
+		if test.alluxioValue.Worker.Resources.Limits[corev1.ResourceMemory] != test.wantRes[1] {
+			t.Errorf("expected %v, got %v", test.wantRes[1], test.alluxioValue.Worker.Resources.Limits[corev1.ResourceMemory])
+		}
+		if test.alluxioValue.Worker.Resources.Requests[corev1.ResourceMemory] != test.wantRes[2] {
+			t.Errorf("expected %v, got %v", test.wantRes[2], test.alluxioValue.Worker.Resources.Requests[corev1.ResourceMemory])
 		}
 	}
 }
@@ -278,6 +372,10 @@ func TestTransformResourcesForWorkerWithOnlyRequest(t *testing.T) {
 		wantRes string
 	}{
 		{&datav1alpha1.AlluxioRuntime{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test",
+				Namespace: "test",
+			},
 			Spec: datav1alpha1.AlluxioRuntimeSpec{
 				Worker: datav1alpha1.AlluxioCompTemplateSpec{
 					Resources: resources,
@@ -296,8 +394,12 @@ func TestTransformResourcesForWorkerWithOnlyRequest(t *testing.T) {
 			Properties: map[string]string{},
 			Master:     Master{},
 			JobMaster:  JobMaster{},
-		}, "21Gi"},
+		}, "20Gi"},
 		{&datav1alpha1.AlluxioRuntime{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test",
+				Namespace: "test",
+			},
 			Spec: datav1alpha1.AlluxioRuntimeSpec{
 				Worker: datav1alpha1.AlluxioCompTemplateSpec{
 					Resources: resources,
@@ -314,10 +416,24 @@ func TestTransformResourcesForWorkerWithOnlyRequest(t *testing.T) {
 		}, "1Gi"},
 	}
 	for _, test := range tests {
-		engine := &AlluxioEngine{Log: fake.NullLogger()}
+		engine := &AlluxioEngine{
+			Log: fake.NullLogger(),
+			name: "test",
+			namespace: "test",
+		}
 		engine.runtimeInfo, _ = base.BuildRuntimeInfo("test", "test", "alluxio", test.runtime.Spec.TieredStore)
 		engine.UnitTest = true
-		engine.transformResourcesForWorker(test.runtime, test.alluxioValue)
+		runtimeObjs := []runtime.Object{}
+		runtimeObjs = append(runtimeObjs, test.runtime.DeepCopy())
+		s := runtime.NewScheme()
+		s.AddKnownTypes(datav1alpha1.GroupVersion, test.runtime)
+		_ = corev1.AddToScheme(s)
+		client := fake.NewFakeClientWithScheme(s, runtimeObjs...)
+		engine.Client = client
+		err := engine.transformResourcesForWorker(test.runtime, test.alluxioValue)
+		if err != nil {
+			t.Errorf("expected nil, got err %v", err)
+		}
 		if result, found := test.alluxioValue.Worker.Resources.Limits[corev1.ResourceMemory]; found {
 			t.Errorf("expected nil, got %v", result)
 		}
@@ -331,7 +447,7 @@ func TestTransformResourcesForWorkerWithOnlyLimit(t *testing.T) {
 
 	resources := corev1.ResourceRequirements{}
 	resources.Limits = make(corev1.ResourceList)
-	resources.Limits[corev1.ResourceMemory] = resource.MustParse("2Gi")
+	resources.Limits[corev1.ResourceMemory] = resource.MustParse("20Gi")
 	resources.Limits[corev1.ResourceCPU] = resource.MustParse("500m")
 
 	result := resource.MustParse("20Gi")
@@ -342,6 +458,10 @@ func TestTransformResourcesForWorkerWithOnlyLimit(t *testing.T) {
 		wantRes []string
 	}{
 		{&datav1alpha1.AlluxioRuntime{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test",
+				Namespace: "test",
+			},
 			Spec: datav1alpha1.AlluxioRuntimeSpec{
 				Worker: datav1alpha1.AlluxioCompTemplateSpec{
 					Resources: resources,
@@ -360,8 +480,12 @@ func TestTransformResourcesForWorkerWithOnlyLimit(t *testing.T) {
 			Properties: map[string]string{},
 			Master:     Master{},
 			JobMaster:  JobMaster{},
-		}, []string{"22Gi", "20Gi"}},
+		}, []string{"20Gi", "20Gi"}},
 		{&datav1alpha1.AlluxioRuntime{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test",
+				Namespace: "test",
+			},
 			Spec: datav1alpha1.AlluxioRuntimeSpec{
 				Worker: datav1alpha1.AlluxioCompTemplateSpec{
 					Resources: resources,
@@ -375,13 +499,27 @@ func TestTransformResourcesForWorkerWithOnlyLimit(t *testing.T) {
 			Properties: map[string]string{},
 			Master:     Master{},
 			JobMaster:  JobMaster{},
-		}, []string{"2Gi", "nil"}},
+		}, []string{"20Gi", "nil"}},
 	}
 	for _, test := range tests {
-		engine := &AlluxioEngine{Log: fake.NullLogger()}
+		engine := &AlluxioEngine{
+			Log: fake.NullLogger(),
+			name: "test",
+			namespace: "test",
+		}
 		engine.runtimeInfo, _ = base.BuildRuntimeInfo("test", "test", "alluxio", test.runtime.Spec.TieredStore)
 		engine.UnitTest = true
-		engine.transformResourcesForWorker(test.runtime, test.alluxioValue)
+		runtimeObjs := []runtime.Object{}
+		runtimeObjs = append(runtimeObjs, test.runtime.DeepCopy())
+		s := runtime.NewScheme()
+		s.AddKnownTypes(datav1alpha1.GroupVersion, test.runtime)
+		_ = corev1.AddToScheme(s)
+		client := fake.NewFakeClientWithScheme(s, runtimeObjs...)
+		engine.Client = client
+		err := engine.transformResourcesForWorker(test.runtime, test.alluxioValue)
+		if err != nil {
+			t.Errorf("expected nil, got err %v", err)
+		}
 		if test.alluxioValue.Worker.Resources.Limits[corev1.ResourceMemory] != test.wantRes[0] {
 			t.Errorf("expected %s, got %v", test.wantRes[0], test.alluxioValue.Worker.Resources.Limits[corev1.ResourceMemory])
 		}
@@ -390,6 +528,7 @@ func TestTransformResourcesForWorkerWithOnlyLimit(t *testing.T) {
 		}
 	}
 }
+
 func TestTransformResourcesForFuseNoValue(t *testing.T) {
 	var tests = []struct {
 		runtime      *datav1alpha1.AlluxioRuntime
