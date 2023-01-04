3ea749a7414d4586d47c019c2d00fc51938297f4
TzZtzt <trafalgarz@outlook.com>
2023-01-04 10:49:44 +0800

    Fix NPE error when log dataset info (#2489)
    Signed-off-by: dongyun.xzh <dongyun.xzh@alibaba-inc.com>
    
    Signed-off-by: dongyun.xzh <dongyun.xzh@alibaba-inc.com>

--- pkg/ddc/alluxio/ufs_internal.go
+++ pkg/ddc/alluxio/ufs_internal.go
@@ -68,10 +68,10 @@ func (e *AlluxioEngine) totalFileNumsInternal() (fileCount int64, err error) {
 // shouldMountUFS checks if there's any UFS that need to be mounted
 func (e *AlluxioEngine) shouldMountUFS() (should bool, err error) {
 	dataset, err := utils.GetDataset(e.Client, e.name, e.namespace)
-	e.Log.Info("get dataset info", "dataset", dataset)
 	if err != nil {
 		return should, err
 	}
+	e.Log.Info("get dataset info", "dataset", dataset)
 
 	podName, containerName := e.getMasterPodInfo()
 	fileUtils := operations.NewAlluxioFileUtils(podName, containerName, e.namespace, e.Log)
