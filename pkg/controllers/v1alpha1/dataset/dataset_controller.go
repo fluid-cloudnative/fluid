5534cc7958fe1896eb42e696fc161c7bfbdd0e32
TzZtzt <trafalgarz@outlook.com>
2023-02-10 17:30:24 +0800

    Fix reference dataset & Fix NPE bug for eacruntime  (#2576)
    * Fix rbac for eacruntime
    
    Signed-off-by: dongyun.xzh <dongyun.xzh@alibaba-inc.com>
    
    * Fix GetRuntimeStatus for eacruntime
    
    Signed-off-by: dongyun.xzh <dongyun.xzh@alibaba-inc.com>
    
    * Skip creating configmaps for EACRuntime
    
    Signed-off-by: dongyun.xzh <dongyun.xzh@alibaba-inc.com>
    
    * Add unit tests
    
    Signed-off-by: dongyun.xzh <dongyun.xzh@alibaba-inc.com>
    
    * Fix NPE when shutting down EACEngine
    
    Signed-off-by: dongyun.xzh <dongyun.xzh@alibaba-inc.com>
    
    * Add log for case common.EACRuntime
    
    Signed-off-by: dongyun.xzh <dongyun.xzh@alibaba-inc.com>
    
    ---------
    
    Signed-off-by: dongyun.xzh <dongyun.xzh@alibaba-inc.com>

--- pkg/controllers/v1alpha1/dataset/dataset_controller.go
+++ pkg/controllers/v1alpha1/dataset/dataset_controller.go
@@ -25,8 +25,8 @@ import (
 	"github.com/fluid-cloudnative/fluid/pkg/common"
 	"github.com/fluid-cloudnative/fluid/pkg/controllers/deploy"
 	"github.com/fluid-cloudnative/fluid/pkg/ddc/base"
-	"sigs.k8s.io/controller-runtime/pkg/controller"
 	v1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 
 	"github.com/fluid-cloudnative/fluid/pkg/utils/kubeclient"
 
