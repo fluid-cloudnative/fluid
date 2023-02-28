f9f7fb7a773aaf4cadca48414c0059dd7a1bf43c
wang-mask <2018091609006@std.uestc.edu.cn>
2023-02-28 16:00:49 +0800

    init: init the DataTable preoject
    Signed-off-by: wang-mask <2018091609006@std.uestc.edu.cn>

---
+++ pkg/controllers/v1alpha1/datatable/datatable_controller.go
@@ -0,0 +1,61 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package datatable
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	datafluidiov1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
+)
+
+// DataTableReconciler reconciles a DataTable object
+type DataTableReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=data.fluid.io.fluid.io,resources=datatables,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=data.fluid.io.fluid.io,resources=datatables/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=data.fluid.io.fluid.io,resources=datatables/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the DataTable object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.1/pkg/reconcile
+func (r *DataTableReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = log.FromContext(ctx)
+
+	// TODO(user): your logic here
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *DataTableReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&datafluidiov1alpha1.DataTable{}).
+		Complete(r)
+}
