706bea77bcb8d51df008b428c5b0d22f5750b8dd
Weiwei <zww@hdls.me>
2023-03-14 21:05:02 +0800

    update mount to check mountinfo (#2739)
    Signed-off-by: zwwhdls <zww@hdls.me>
    Signed-off-by: “A-Mavericks” <363136637@qq.com>

--- pkg/scripts/poststart/check_fuse.go
+++ pkg/scripts/poststart/check_fuse.go
@@ -47,7 +47,7 @@ SubPath="$3"
 
 count=0
 # while ! mount | grep alluxio | grep  $ConditionPathIsMountPoint | grep -v grep
-while ! mount | grep $ConditionPathIsMountPoint | grep $MountType
+while ! cat /proc/self/mountinfo | grep $ConditionPathIsMountPoint | grep $MountType
 do
     sleep 3
     count=¬expr $count + 1¬
