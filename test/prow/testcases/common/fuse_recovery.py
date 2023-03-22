a19e59342d410cc166606b48fcea4cda6025ec59
dongyun.xzh <dongyun.xzh@alibaba-inc.com>
2023-03-22 16:47:45 +0800

    Remove redundant code in FUSE Recover testcase
    Signed-off-by: dongyun.xzh <dongyun.xzh@alibaba-inc.com>

--- test/prow/testcases/common/fuse_recovery.py
+++ test/prow/testcases/common/fuse_recovery.py
@@ -61,73 +61,16 @@ def checkCsiRecoverEnabled() -> bool:
     return False
 
 
-def createDatasetAndRuntime():
-    api = client.CustomObjectsApi()
-    my_dataset = {
-        "apiVersion": "data.fluid.io/v1alpha1",
-        "kind": "Dataset",
-        "metadata": {"name": "hbase"},
-        "spec": {
-            "mounts": [{"mountPoint": "https://mirrors.bit.edu.cn/apache/hbase/stable/", "name": "hbase"}]
-        }
-    }
-
-    my_alluxioruntime = {
-        "apiVersion": "data.fluid.io/v1alpha1",
-        "kind": "AlluxioRuntime",
-        "metadata": {"name": "hbase"},
-        "spec": {
-            "replicas": 1,
-            "tieredstore": {
-                "levels": [{
-                    "mediumtype": "MEM",
-                    "path": "/dev/shm",
-                    "quota": "2Gi",
-                    "high": "0.95",
-                    "low": "0.7"
-                }]
-            }
-        }
-    }
-
-    api.create_namespaced_custom_object(
-        group="data.fluid.io",
-        version="v1alpha1",
-        namespace=namespace,
-        plural="datasets",
-        body=my_dataset,
-    )
-    print("Created dataset.")
-    api.create_namespaced_custom_object(
-        group="data.fluid.io",
-        version="v1alpha1",
-        namespace=namespace,
-        plural="alluxioruntimes",
-        body=my_alluxioruntime
-    )
-    print("Created alluxioruntime.")
-
-
-def checkDatasetBound():
-    api = client.CustomObjectsApi()
-    while True:
-        resource = api.get_namespaced_custom_object(
-            group="data.fluid.io",
-            version="v1alpha1",
-            name="hbase",
-            namespace=namespace,
-            plural="datasets"
-        )
-        print(resource)
-        if "status" in resource:
-            if "phase" in resource["status"]:
-                if resource["status"]["phase"] == "Bound":
-                    time.sleep(5)
-                    return True
-        time.sleep(1)
-
-
 def checkFuseRecovered(dataset_name, namespace="default"):
+    def getFuseRecoveredUids(namespace="default"):
+        api = client.CoreV1Api()
+        items = api.list_namespaced_event(namespace=namespace).items
+        fuseRecoveryUids = set()
+        for item in items:
+            if item.message.__contains__("Fuse recover"):
+                fuseRecoveryUids.add(item.involved_object.uid)
+        return fuseRecoveryUids
+
     ### get dataset hbase uid
     api = client.CustomObjectsApi()
     dataset = api.get_namespaced_custom_object(
@@ -147,52 +90,6 @@ def checkFuseRecovered(dataset_name, namespace="default"):
     return False
 
 
-def checkVolumeResourcesReady():
-    while True:
-        try:
-            client.CoreV1Api().read_persistent_volume(name=namespace + "-hbase")
-        except client.exceptions.ApiException as e:
-            if e.status == 404:
-                time.sleep(1)
-                continue
-        try:
-            client.CoreV1Api().read_namespaced_persistent_volume_claim(name="hbase", namespace=namespace)
-        except client.exceptions.ApiException as e:
-            if e.status == 404:
-                time.sleep(1)
-                continue
-        print("PersistentVolume & PersistentVolumeClaim Ready.")
-        break
-    time.sleep(5)
-
-
-def createDataListPod(name):
-    api = client.CoreV1Api()
-    containers = [client.V1Container(
-        name="nginx",
-        image="nginx",
-        # mount_propagation="HostToContainer"
-        volume_mounts=[client.V1VolumeMount(mount_path="/data", name="hbase-vol")]
-    )]
-    volumes = [client.V1Volume(
-        name="hbase-vol",
-        persistent_volume_claim=client.V1PersistentVolumeClaimVolumeSource(claim_name="hbase")
-    )]
-    spec = client.V1PodSpec(
-        containers=containers,
-        volumes=volumes
-    )
-    pod = client.V1Pod(
-        api_version="v1",
-        kind="Pod",
-        metadata=client.V1ObjectMeta(name=name, labels={"fuse.serverful.fluid.io/inject": "true"}),
-        spec=spec
-    )
-    api.create_namespaced_pod(namespace=namespace, body=pod)
-    print("Pod created.")
-    time.sleep(5)
-
-
 def checkDataListSuccess(pod_name, namespace="default") -> bool:
     cmd = "kubectl -n {} exec -it  {} ls /data/zookeeper".format(namespace, pod_name)
     success = os.system(cmd)
@@ -204,81 +101,16 @@ def checkDataListSuccess(pod_name, namespace="default") -> bool:
         return False
 
 
-def deletePod(prefix, pod_namespace):
-    pod_name = getPodNameByPrefix(prefix, pod_namespace)
-    api = client.CoreV1Api()
-    api.delete_namespaced_pod(pod_name, pod_namespace)
-    time.sleep(5)
-    print("Delete pod: {}".format(pod_name))
-
-
-def cleanUp(pod_name):
-    api = client.CoreV1Api()
-    # Delete Data Read Pod
-    body = client.V1DeleteOptions(propagation_policy='Background')
-    if getPodNameByPrefix(pod_name, namespace) is not None:
-        api.delete_namespaced_pod(name=pod_name, namespace=namespace, body=body)
-    print("Delete pod:{}".format(pod_name))
-    time.sleep(5)
-
-    # Delete Dataset & Alluxioruntime
-    custom_api = client.CustomObjectsApi()
-    custom_api.delete_namespaced_custom_object(
-        group="data.fluid.io",
-        version="v1alpha1",
-        name="hbase",
-        namespace=namespace,
-        plural="datasets"
-    )
-    time.sleep(5)
-    runtimeDelete = False
-    while not runtimeDelete:
-        print("runtime still exists...")
-        try:
-            runtime = custom_api.get_namespaced_custom_object(
-                group="data.fluid.io",
-                version="v1alpha1",
-                name="hbase",
-                namespace=namespace,
-                plural="alluxioruntimes"
-            )
-        except client.exceptions.ApiException as e:
-            if e.status == 404:
-                runtimeDelete = True
-                continue
-
-        time.sleep(1)
-
-
-def checkPodReady(name, pod_namespace) -> bool:
-    api = client.CoreV1Api()
-    while True:
-        name = getPodNameByPrefix(name, pod_namespace)
-        if name is None:
-            return False
-        pod = api.read_namespaced_pod(name, pod_namespace).status
-        if pod.phase == "Running":
-            print("Pod-{} is {}.".format(name, pod.phase))
-            return True
-        else:
-            print("Pod-{} is {}.".format(name, pod.phase))
-            time.sleep(1)
-
-
 def deleteAlluxioFusePod(dataset_name, namespace="default"):
-    deletePod("{}-fuse".format(dataset_name), namespace)
-    print("Delete Fuse Pod:{}-fuse-xxxxx".format(dataset_name))
+    def deletePodWithPrefix(prefix, pod_namespace):
+        pod_name = getPodNameByPrefix(prefix, pod_namespace)
+        api = client.CoreV1Api()
+        api.delete_namespaced_pod(pod_name, pod_namespace)
+        time.sleep(5)
+        print("Delete pod: {}".format(pod_name))
+
+    deletePodWithPrefix("{}-fuse".format(dataset_name), namespace)
     time.sleep(30)
-
-
-def getFuseRecoveredUids(namespace="default"):
-    api = client.CoreV1Api()
-    items = api.list_namespaced_event(namespace=namespace).items
-    fuseRecoveryUids = set()
-    for item in items:
-        if item.message.__contains__("Fuse recover"):
-            fuseRecoveryUids.add(item.involved_object.uid)
-    return fuseRecoveryUids
 
 
 def main():
@@ -288,7 +120,6 @@ def main():
         config.load_incluster_config()
 
 
-    exit_code = 0
     if checkCsiRecoverEnabled() is False:
         print("FAIL at checkCsiRecoverEnabled(): FUSE Recover feature gate is not enabled")
         return 1
@@ -385,30 +216,6 @@ def main():
     except Exception as e:
         print(e)
         exit(1)
-
-
-
-    ### Create dataset & alluxioruntime
-    # createDatasetAndRuntime()
-    # checkDatasetBound()
-    # checkVolumeResourcesReady()
-
-    # ### Create Pod with Injection Label
-    # createDataListPod("nginx")
-    # if checkPodReady("nginx", namespace):
-    #     time.sleep(5)
-    #     checkDataListSuccess("nginx")
-
-    # ### Delete fuse
-    # deleteAlluxioFusePod()
-    # if checkPodReady("hbase-fuse", namespace) and checkFuseRecovered():
-    #     time.sleep(5)
-    #     if checkDataListSuccess("nginx"):
-    #         exit_code = 0
-    #     else:
-    #         exit_code = 1
-    # cleanUp("nginx")
-    # return exit_code
 
 
 if __name__ == '__main__':
