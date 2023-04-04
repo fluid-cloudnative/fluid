9e621f3d2567d77e9586d839252429c6b8c728c8
antshuang19 <anthonyhuang1909@gmail.com>
2023-04-04 16:46:42 +0800

    Copyright 2023 The Fluid  Author

--- pkg/ddc/goosefs/worker.go
+++ pkg/ddc/goosefs/worker.go
@@ -1,4 +1,8 @@
 /*
+<<<<<<< Updated upstream
+=======
+Copyright 2023 The Fluid  Author
+>>>>>>> Stashed changes
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
