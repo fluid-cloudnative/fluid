a9d3c0a66a5596de706e072dacd91f4ac0624fef
cheyang <cheyang@163.com>
2023-04-05 10:46:17 +0800

    Update fluid version to 0.8.6 (#2843)
    * Update fluid version to 0.8.6, To #46827170
    
    Signed-off-by: cheyang <cheyang@163.com>
    
    * Update CI configuration to support released version, To #46827170
    
    Signed-off-by: cheyang <cheyang@163.com>
    
    * Update CI configuration to support released version, To #46827170
    
    Signed-off-by: cheyang <cheyang@163.com>
    
    * Fix go version, To #48332556
    
    Signed-off-by: cheyang <cheyang@163.com>
    
    * Fix golint, To #48327952
    
    Signed-off-by: cheyang <cheyang@163.com>
    
    * Fix golint, To #48327952
    
    Signed-off-by: cheyang <cheyang@163.com>
    
    ---------
    
    Signed-off-by: cheyang <cheyang@163.com>

--- pkg/webhook/plugins/nodeaffinitywithcache/node_affinity_with_cache.go
+++ pkg/webhook/plugins/nodeaffinitywithcache/node_affinity_with_cache.go
@@ -18,6 +18,7 @@ package nodeaffinitywithcache
 
 import (
 	"fmt"
+
 	"github.com/fluid-cloudnative/fluid/pkg/common"
 	"github.com/fluid-cloudnative/fluid/pkg/ddc/base"
 	"github.com/fluid-cloudnative/fluid/pkg/utils"
@@ -82,9 +83,6 @@ func (p *NodeAffinityWithCache) Mutate(pod *corev1.Pod, runtimeInfos map[string]
 
 func getRequiredAndPreferredSchedulingRuntimes(pod *corev1.Pod, runtimeInfos map[string]base.RuntimeInfoInterface) (
 	requiredRuntimes map[string]base.RuntimeInfoInterface, preferredRuntimes map[string]base.RuntimeInfoInterface) {
-
-	requiredRuntimes = nil
-	preferredRuntimes = nil
 
 	// get the pod specified bound dataset
 	boundDataSetNames := map[string]bool{}
