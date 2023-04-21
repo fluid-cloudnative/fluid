b78e5fe505d35b6007b71b1427bfacc77185b7d8
trafalgarzzz <trafalgarz@outlook.com>
2023-04-21 11:49:07 +0800

    Fix lint check
    Signed-off-by: trafalgarzzz <trafalgarz@outlook.com>

--- pkg/utils/dataset/volume/create.go
+++ pkg/utils/dataset/volume/create.go
@@ -21,7 +21,6 @@ import (
 	"time"
 
 	"github.com/go-logr/logr"
-	"k8s.io/api/core/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -53,7 +52,7 @@ func CreatePersistentVolumeForRuntime(client client.Client,
 	}
 
 	if !found {
-		pv := &v1.PersistentVolume{
+		pv := &corev1.PersistentVolume{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      pvName,
 				Namespace: runtime.GetNamespace(),
@@ -62,14 +61,14 @@ func CreatePersistentVolumeForRuntime(client client.Client,
 				},
 				Annotations: common.ExpectedFluidAnnotations,
 			},
-			Spec: v1.PersistentVolumeSpec{
+			Spec: corev1.PersistentVolumeSpec{
 				AccessModes: accessModes,
-				Capacity: v1.ResourceList{
-					v1.ResourceName(v1.ResourceStorage): resource.MustParse("100Pi"),
+				Capacity: corev1.ResourceList{
+					corev1.ResourceName(corev1.ResourceStorage): resource.MustParse("100Pi"),
 				},
 				StorageClassName: common.FluidStorageClass,
-				PersistentVolumeSource: v1.PersistentVolumeSource{
-					CSI: &v1.CSIPersistentVolumeSource{
+				PersistentVolumeSource: corev1.PersistentVolumeSource{
+					CSI: &corev1.CSIPersistentVolumeSource{
 						Driver:       common.CSIDriver,
 						VolumeHandle: pvName,
 						VolumeAttributes: map[string]string{
@@ -102,17 +101,17 @@ func CreatePersistentVolumeForRuntime(client client.Client,
 		if global {
 			log.Info("Enable global mode for fuse in volume")
 			if len(nodeSelector) > 0 {
-				nodeSelectorRequirements := []v1.NodeSelectorRequirement{}
+				nodeSelectorRequirements := []corev1.NodeSelectorRequirement{}
 				for key, value := range nodeSelector {
-					nodeSelectorRequirements = append(nodeSelectorRequirements, v1.NodeSelectorRequirement{
+					nodeSelectorRequirements = append(nodeSelectorRequirements, corev1.NodeSelectorRequirement{
 						Key:      key,
-						Operator: v1.NodeSelectorOpIn,
+						Operator: corev1.NodeSelectorOpIn,
 						Values:   []string{value},
 					})
 				}
-				pv.Spec.NodeAffinity = &v1.VolumeNodeAffinity{
-					Required: &v1.NodeSelector{
-						NodeSelectorTerms: []v1.NodeSelectorTerm{
+				pv.Spec.NodeAffinity = &corev1.VolumeNodeAffinity{
+					Required: &corev1.NodeSelector{
+						NodeSelectorTerms: []corev1.NodeSelectorTerm{
 							{
 								MatchExpressions: nodeSelectorRequirements,
 							},
@@ -122,14 +121,14 @@ func CreatePersistentVolumeForRuntime(client client.Client,
 			}
 		} else {
 			log.Info("Disable global mode for fuse in volume")
-			pv.Spec.NodeAffinity = &v1.VolumeNodeAffinity{
-				Required: &v1.NodeSelector{
-					NodeSelectorTerms: []v1.NodeSelectorTerm{
+			pv.Spec.NodeAffinity = &corev1.VolumeNodeAffinity{
+				Required: &corev1.NodeSelector{
+					NodeSelectorTerms: []corev1.NodeSelectorTerm{
 						{
-							MatchExpressions: []v1.NodeSelectorRequirement{
+							MatchExpressions: []corev1.NodeSelectorRequirement{
 								{
 									Key:      runtime.GetCommonLabelName(),
-									Operator: v1.NodeSelectorOpIn,
+									Operator: corev1.NodeSelectorOpIn,
 									Values:   []string{"true"},
 								},
 							},
@@ -140,7 +139,7 @@ func CreatePersistentVolumeForRuntime(client client.Client,
 		}
 		metadataList := runtime.GetMetadataList()
 		for i := range metadataList {
-			if selector := metadataList[i].Selector; selector.Group != v1.GroupName || selector.Kind != "PersistentVolume" {
+			if selector := metadataList[i].Selector; selector.Group != corev1.GroupName || selector.Kind != "PersistentVolume" {
 				continue
 			}
 			pv.Labels = utils.UnionMapsWithOverride(pv.Labels, metadataList[i].Labels)
@@ -154,7 +153,7 @@ func CreatePersistentVolumeForRuntime(client client.Client,
 
 		timeoutCtx, cancelFn := context.WithTimeout(context.Background(), 1*time.Second)
 		defer cancelFn()
-		wait.PollImmediateUntilWithContext(timeoutCtx, 200*time.Millisecond, func(ctx context.Context) (done bool, err error) {
+		pollErr := wait.PollImmediateUntilWithContext(timeoutCtx, 200*time.Millisecond, func(ctx context.Context) (done bool, err error) {
 			pvCreated, pvErr := kubeclient.GetPersistentVolume(client, pvName)
 			if pvErr != nil {
 				if utils.IgnoreNotFound(pvErr) == nil {
@@ -173,6 +172,9 @@ func CreatePersistentVolumeForRuntime(client client.Client,
 
 			return false, nil
 		})
+		if pollErr != nil {
+			log.Error(pollErr, "Got error when polling PV's status", "pv", pvName)
+		}
 	} else {
 		log.Info("The persistent volume is created", "name", pvName)
 	}
@@ -195,7 +197,7 @@ func CreatePersistentVolumeClaimForRuntime(client client.Client,
 	}
 
 	if !found {
-		pvc := &v1.PersistentVolumeClaim{
+		pvc := &corev1.PersistentVolumeClaim{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      runtime.GetName(),
 				Namespace: runtime.GetNamespace(),
@@ -204,7 +206,7 @@ func CreatePersistentVolumeClaimForRuntime(client client.Client,
 				},
 				Annotations: common.ExpectedFluidAnnotations,
 			},
-			Spec: v1.PersistentVolumeClaimSpec{
+			Spec: corev1.PersistentVolumeClaimSpec{
 				Selector: &metav1.LabelSelector{
 					MatchLabels: map[string]string{
 						runtime.GetCommonLabelName(): "true",
@@ -212,16 +214,16 @@ func CreatePersistentVolumeClaimForRuntime(client client.Client,
 				},
 				StorageClassName: &common.FluidStorageClass,
 				AccessModes:      accessModes,
-				Resources: v1.ResourceRequirements{
-					Requests: v1.ResourceList{
-						v1.ResourceName(v1.ResourceStorage): resource.MustParse("100Pi"),
+				Resources: corev1.ResourceRequirements{
+					Requests: corev1.ResourceList{
+						corev1.ResourceName(corev1.ResourceStorage): resource.MustParse("100Pi"),
 					},
 				},
 			},
 		}
 		metadataList := runtime.GetMetadataList()
 		for i := range metadataList {
-			if selector := metadataList[i].Selector; selector.Group != v1.GroupName || selector.Kind != "PersistentVolumeClaim" {
+			if selector := metadataList[i].Selector; selector.Group != corev1.GroupName || selector.Kind != "PersistentVolumeClaim" {
 				continue
 			}
 			pvc.Labels = utils.UnionMapsWithOverride(pvc.Labels, metadataList[i].Labels)
