04437b863a5c0eac14f0613ac851d31a8d6feb4c
wuweinvyou <116442132+wuweinvyou@users.noreply.github.com>
2023-04-25 19:51:40 +0800

    Lab1 (#3172)
    * 规范化node_test.go通告声明，增加Copyright
    
    Signed-off-by: wuweinvyou <1303112670@qq.com>
    
    * -sadd Copyright to the note_test.go
    
    Signed-off-by: wuweinvyou <1303112670@qq.com>
    
    * add Copyright to the note_test.go
    
    Signed-off-by: wuweinvyou <1303112670@qq.com>
    
    * add
    
    Signed-off-by: wuweinvyou <1303112670@qq.com>
    
    ---------
    
    Signed-off-by: wuweinvyou <1303112670@qq.com>
    Signed-off-by: njucs <cqy191220018@github.com>

--- pkg/utils/kubeclient/node_test.go
+++ pkg/utils/kubeclient/node_test.go
@@ -1,4 +1,5 @@
 /*
+Copyright 2023 The Fluid Author.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -329,3 +330,4 @@ func TestGetIpAddressesOfNodes(t *testing.T) {
 		})
 	}
 }
+
