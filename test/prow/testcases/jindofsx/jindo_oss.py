9723a3b4c4bef5dd7a54c121fbb546e0c62b2800
trafalgarzzz <trafalgarz@outlook.com>
2023-05-22 14:32:56 +0800

    Fix e2e testcases
    Signed-off-by: trafalgarzzz <trafalgarz@outlook.com>

--- test/prow/testcases/jindofsx/jindo_oss.py
+++ test/prow/testcases/jindofsx/jindo_oss.py
@@ -99,7 +99,7 @@ def main():
     flow.append_step(
         SimpleStep(
             step_name="create data read job",
-            forth_fn=funcs.create_job_fn(script="time cp -r /data/ /tmp-data && [[ ! -z $(ls -l /tmp-data) ]]", dataset_name=name, namespace=namespace),
+            forth_fn=funcs.create_job_fn(script="time cp -r /data/ /tmp-data && [[ ! -z \"$(ls -l /tmp-data)\" ]]", dataset_name=name, namespace=namespace),
             back_fn=funcs.delete_job_fn()
         )
     )
