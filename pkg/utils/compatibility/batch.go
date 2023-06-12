8b914f9afee46362385deb349f094801c3caaa78
TzZtzt <trafalgarz@outlook.com>
2023-06-12 20:21:26 +0800

    bugfix: Support batch/v1beta1 cronjobs for compatibility before Kubernetes v1.21 (#3280)
    * Support batch/v1beta1 cronjobs for compatibility before Kubernetes v1.21
    
    Signed-off-by: trafalgarzzz <trafalgarz@outlook.com>
    
    * Refactor code to improve readability
    
    Signed-off-by: trafalgarzzz <trafalgarz@outlook.com>
    
    * Add license
    
    Signed-off-by: trafalgarzzz <trafalgarz@outlook.com>
    
    ---------
    
    Signed-off-by: trafalgarzzz <trafalgarz@outlook.com>
    Signed-off-by: njucs <cqy191220018@github.com>

---
+++ pkg/utils/compatibility/batch.go
@@ -0,0 +1,51 @@
+/*
+Copyright 2023 The Fluid Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package compatibility
+
+import (
+	"log"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/client-go/discovery"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+var batchV1CronJobCompatible = false
+
+func init() {
+	restConfig := ctrl.GetConfigOrDie()
+
+	discoveryClient := discovery.NewDiscoveryClientForConfigOrDie(restConfig)
+
+	resources, err := discoveryClient.ServerResourcesForGroupVersion("batch/v1")
+	if err != nil && !errors.IsNotFound(err) {
+		log.Fatalf("failed to discover batch/v1 group version: %v", err)
+	}
+
+	if len(resources.APIResources) > 0 {
+		for _, res := range resources.APIResources {
+			if res.Name == "cronjobs" {
+				batchV1CronJobCompatible = true
+				break
+			}
+		}
+	}
+}
+
+func IsBatchV1CronJobSupported() bool {
+	return batchV1CronJobCompatible
+}
