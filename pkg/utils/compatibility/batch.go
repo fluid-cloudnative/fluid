5e9a1ad09492a097fef18ab5deee1e263e773196
trafalgarzzz <trafalgarz@outlook.com>
2023-06-12 17:35:02 +0800

    Refactor code to improve readability
    Signed-off-by: trafalgarzzz <trafalgarz@outlook.com>

--- pkg/utils/compatibility/batch.go
+++ pkg/utils/compatibility/batch.go
@@ -14,21 +14,19 @@ func init() {
 	restConfig := ctrl.GetConfigOrDie()
 
 	discoveryClient := discovery.NewDiscoveryClientForConfigOrDie(restConfig)
-	if resources, err := discoveryClient.ServerResourcesForGroupVersion("batch/v1"); err != nil {
-		if !errors.IsNotFound(err) {
-			log.Fatalf("failed to discover batch/v1 group version: %v", err)
-		}
-	} else {
+
+	resources, err := discoveryClient.ServerResourcesForGroupVersion("batch/v1")
+	if err != nil && !errors.IsNotFound(err) {
+		log.Fatalf("failed to discover batch/v1 group version: %v", err)
+	}
+
+	if len(resources.APIResources) > 0 {
 		for _, res := range resources.APIResources {
 			if res.Name == "cronjobs" {
 				batchV1CronJobCompatible = true
 				break
 			}
 		}
-	}
-
-	if !batchV1CronJobCompatible {
-		log.Print("batch/v1 cronjobs not found in the cluster, fall back to use batch/v1beta1 cronjobs")
 	}
 }
 
