806387f1c4903b646d06b6b6d0da8b10088cf56b
trafalgarzzz <trafalgarz@outlook.com>
2023-06-29 15:44:00 +0800

    Initialize client.Client according to HELM_DRIVER value
    Signed-off-by: trafalgarzzz <trafalgarz@outlook.com>

--- pkg/controllers/manager.go
+++ pkg/controllers/manager.go
@@ -1,12 +1,25 @@
 package controllers
 
 import (
+	"os"
+
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/cluster"
 )
+
+// NewFluidControllerClient creates client.Client according to the HELM_DRIVER env variable. It returns the default client when setting HELM_DRIVER=true,
+// meaning users explicitly grant secret permissions to Fluid controllers. Otherwise, it returns a specific client.Client that utilizes informers as cache
+// except for Secrets.
+func NewFluidControllerClient(cache cache.Cache, config *rest.Config, options client.Options, uncachedObjects ...client.Object) (client.Client, error) {
+	if driver, exist := os.LookupEnv("HELM_DRIVER"); exist && driver == "secret" {
+		return cluster.DefaultNewClient(cache, config, options)
+	}
+
+	return NewCacheClientBypassSecrets(cache, config, options, uncachedObjects...)
+}
 
 // NewCacheClientBypassSecrets creates a client querying kubernetes resources with cache(informers) except for Secrets.
 // Secret is an exception for that we aim to trade performance for higher security(e.g. less rbac verbs on Secrets).
