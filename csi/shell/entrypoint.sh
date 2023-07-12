0a9ec0c36f226438471ecde2a840fc66a2fcb983
wangshulin <wangshulin@smail.nju.edu.cn>
2023-07-12 15:15:45 +0800

    add more info
    Signed-off-by: wangshulin <wangshulin@smail.nju.edu.cn>

--- csi/shell/entrypoint.sh
+++ csi/shell/entrypoint.sh
@@ -2,20 +2,19 @@
 set -xe
 
 # Function to check if a directory exists
-check_directory() {
+check_kubelet_rootdir_subfolder() {
   local dir="$1"
   
   if [ ! -d "$dir" ]; then
-    echo "Error: $dir does not exist."
-    echo "See https://github.com/fluid-cloudnative/fluid/blob/master/docs/zh/troubleshooting/debug-fuse.md#%E6%AD%A5%E9%AA%A41-1 for more information!"
+    echo "Error: subfolder $dir does not exist, please check whether KUBELET_ROOTDIR $KUBELET_ROOTDIR is configured correctly." 
     exit 1
   fi
 }
 
-check_directory "$KUBELET_ROOTDIR/pods"
-check_directory "$KUBELET_ROOTDIR/plugins"
+check_kubelet_rootdir_subfolder "$KUBELET_ROOTDIR/pods"
+check_kubelet_rootdir_subfolder "$KUBELET_ROOTDIR/plugins"
 
-rm -f $KUBELET_ROOTDIR/csi-plugins/fuse.csi.fluid.io/csi.sock
-mkdir -p $KUBELET_ROOTDIR/csi-plugins/fuse.csi.fluid.io
+rm -f "$KUBELET_ROOTDIR/csi-plugins/fuse.csi.fluid.io/csi.sock"
+mkdir -p "$KUBELET_ROOTDIR/csi-plugins/fuse.csi.fluid.io"
 
 fluid-csi start $@
