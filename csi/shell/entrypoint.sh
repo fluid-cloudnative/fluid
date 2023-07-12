5eb42d39bdef12b03aaaaaa8504f88ad0ad1534c
wangshulin <wangshulin@smail.nju.edu.cn>
2023-07-12 11:40:59 +0800

    function check_directory
    Signed-off-by: wangshulin <wangshulin@smail.nju.edu.cn>

--- csi/shell/entrypoint.sh
+++ csi/shell/entrypoint.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 set -xe
 
-# Check if required subfolders exist in $KUBELET_ROOTDIR folder
-if [ ! -d "$KUBELET_ROOTDIR/pods" ]; then
-    echo "Error: $KUBELET_ROOTDIR does not contain /pods folder, it is not a kubelet rootdir."
+# Function to check if a directory exists
+check_directory() {
+  local dir="$1"
+  
+  if [ ! -d "$dir" ]; then
+    echo "Error: $dir does not exist."
     echo "See https://github.com/fluid-cloudnative/fluid/blob/master/docs/zh/troubleshooting/debug-fuse.md#%E6%AD%A5%E9%AA%A41-1 for more information!"
     exit 1
-fi
+  fi
+}
 
-if [ ! -d "$KUBELET_ROOTDIR/plugins" ]; then
-    echo "Error: $KUBELET_ROOTDIR does not contain /plugins folder, it is not a kubelet rootdir."
-    echo "See https://github.com/fluid-cloudnative/fluid/blob/master/docs/zh/troubleshooting/debug-fuse.md#%E6%AD%A5%E9%AA%A41-1 for more information!"
-    exit 1
-fi
+check_directory "$KUBELET_ROOTDIR/pods"
+check_directory "$KUBELET_ROOTDIR/plugins"
 
 rm -f $KUBELET_ROOTDIR/csi-plugins/fuse.csi.fluid.io/csi.sock
 mkdir -p $KUBELET_ROOTDIR/csi-plugins/fuse.csi.fluid.io
