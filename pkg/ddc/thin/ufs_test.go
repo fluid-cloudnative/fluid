788207ed5384c81610600d4404b3809431293570
wangshulin <wangshulin@smail.nju.edu.cn>
2023-09-04 16:47:00 +0800

    fix ut error
    Signed-off-by: wangshulin <wangshulin@smail.nju.edu.cn>

--- pkg/ddc/thin/ufs_test.go
+++ pkg/ddc/thin/ufs_test.go
@@ -21,6 +21,7 @@ import (
 	"testing"
 
 	. "github.com/agiledragon/gomonkey/v2"
+	"github.com/brahma-adshonor/gohook"
 	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
 	"github.com/fluid-cloudnative/fluid/pkg/utils"
 	"github.com/fluid-cloudnative/fluid/pkg/utils/fake"
@@ -29,6 +30,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func mockExecCommandInContainerForTotalFileNums() (stdout string, stderr string, err error) {
@@ -311,7 +313,7 @@ func TestThinEngine_ShouldUpdateUFS(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			j := ThinEngine{}
+			j := ThinEngine{Client: fake.NewFakeClient()}
 			if gotUfsToUpdate := j.ShouldUpdateUFS(); !reflect.DeepEqual(gotUfsToUpdate, tt.wantUfsToUpdate) {
 				t.Errorf("ShouldUpdateUFS() = %v, want %v", gotUfsToUpdate, tt.wantUfsToUpdate)
 			}
@@ -441,5 +443,121 @@ func TestThinEngine_UsedStorageBytes(t *testing.T) {
 				t.Errorf("ThinEngine.UsedStorageBytes() = %v, want %v", gotValue, tt.wantValue)
 			}
 		})
+	}
+}
+
+func TestThinEngine_updateFuseConfigOnChange(t *testing.T) {
+	mockUpdate := func(client client.Client, cm *corev1.ConfigMap) error {
+		return nil
+	}
+	err := gohook.Hook(kubeclient.UpdateConfigMap, mockUpdate, nil)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	wrappedUnhook := func() {
+		err := gohook.UnHook(kubeclient.UpdateConfigMap)
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	}
+	defer wrappedUnhook()
+
+	dataset := datav1alpha1.Dataset{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test",
+			Namespace: "default",
+		},
+		Spec: datav1alpha1.DatasetSpec{
+			Mounts: []datav1alpha1.Mount{
+				{
+					MountPoint: "https://mirrors.bit.edu.cn/apache/zookeeper/stable/",
+					Name:       "zookeeper",
+				},
+			},
+		},
+	}
+	cm := corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-fuse-conf",
+			Namespace: "default",
+		},
+		Data: map[string]string{
+			"config.json": "{\"mounts\":[{\"mountPoint\":\"https://mirrors.bit.edu.cn/apache/zookeeper/stable/\",\"name\":\"zookeeper\"}],\"targetPath\":\"/thin/default/test/thin-fuse\"}",
+		},
+	}
+	newDataset := datav1alpha1.Dataset{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test",
+			Namespace: "default",
+		},
+		Spec: datav1alpha1.DatasetSpec{
+			Mounts: []datav1alpha1.Mount{
+				{
+					MountPoint: "https://mirrors.bit.edu.cn/apache/zookeeper/stable/",
+					Name:       "zookeeper",
+				},
+				{
+					MountPoint: "https://mirrors.bit.edu.cn/apache/hbase/stable/",
+					Name:       "hbase",
+				},
+			},
+		},
+	}
+	thinruntime := datav1alpha1.ThinRuntime{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test",
+			Namespace: "default",
+		},
+	}
+	testcase := []struct {
+		name       string
+		dataset    *datav1alpha1.Dataset
+		cm         *corev1.ConfigMap
+		wantUpdate bool
+		wantErr    bool
+	}{
+		{
+			name:       "not update configmap",
+			dataset:    &dataset,
+			cm:         &cm,
+			wantUpdate: false,
+			wantErr:    false,
+		},
+		{
+			name:       "update configmap",
+			dataset:    &newDataset,
+			cm:         &cm,
+			wantUpdate: true,
+			wantErr:    false,
+		},
+		{
+			name:       "no configmap",
+			dataset:    &datav1alpha1.Dataset{},
+			cm:         &corev1.ConfigMap{},
+			wantUpdate: false,
+			wantErr:    false,
+		},
+	}
+
+	scheme := runtime.NewScheme()
+	_ = corev1.AddToScheme(scheme)
+	_ = datav1alpha1.AddToScheme(scheme)
+
+	for _, tt := range testcase {
+		client := fake.NewFakeClientWithScheme(scheme, tt.dataset, tt.cm)
+		thinEngine := ThinEngine{
+			Client:    client,
+			name:      "test",
+			namespace: "default",
+			runtime:   &thinruntime,
+			Log:       fake.NullLogger(),
+		}
+		update, err := thinEngine.updateFuseConfigOnChange(thinEngine.runtime, tt.dataset)
+		if (err != nil) != tt.wantErr {
+			t.Errorf("testcase %s failed due to error %v", tt.name, err)
+		}
+		if update != tt.wantUpdate {
+			t.Errorf("testcase %s failed, want update %v, get update %v", tt.name, tt.wantUpdate, update)
+		}
 	}
 }
