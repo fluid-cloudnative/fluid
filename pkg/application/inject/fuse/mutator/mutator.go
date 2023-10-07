dc60ab4d7c70a9fa68cbfac341e39249e1d3a478
trafalgarzzz <trafalgarz@outlook.com>
2023-10-07 21:29:32 +0800

    Implement default mutator
    Signed-off-by: trafalgarzzz <trafalgarz@outlook.com>

--- pkg/application/inject/fuse/mutator/mutator.go
+++ pkg/application/inject/fuse/mutator/mutator.go
@@ -1,7 +1,34 @@
 package mutator
 
-import "github.com/fluid-cloudnative/fluid/pkg/common"
+import (
+	"github.com/fluid-cloudnative/fluid/pkg/common"
+	"github.com/fluid-cloudnative/fluid/pkg/ddc/base"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
 type FluidObjectMutator interface {
-	Mutate(pod *common.FluidObject, template common.FuseInjectionTemplate, options common.FuseSidecarInjectOption) error
+	Mutate() error
+}
+
+type MutatingContext struct {
+	pvcName     string
+	runtimeInfo base.RuntimeInfoInterface
+	template    *common.FuseInjectionTemplate
+	options     common.FuseSidecarInjectOption
+	nameSuffix  string
+
+	appendedVolumeNames         map[string]string
+	datasetUsedInContainers     bool
+	datasetUsedInInitContainers bool
+
+	specs *MutatingPodSpecs
+}
+
+type MutatingPodSpecs struct {
+	Volumes        []corev1.Volume
+	VolumeMounts   []corev1.VolumeMount
+	Containers     []corev1.Container
+	InitContainers []corev1.Container
+	MetaObj        metav1.ObjectMeta
 }
--- pkg/application/inject/fuse/mutator/mutator_default.go
+++ pkg/application/inject/fuse/mutator/mutator_default.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"github.com/fluid-cloudnative/fluid/pkg/common"
-	"github.com/fluid-cloudnative/fluid/pkg/ddc/base"
 	"github.com/fluid-cloudnative/fluid/pkg/scripts/poststart"
 	"github.com/fluid-cloudnative/fluid/pkg/utils"
 	"github.com/fluid-cloudnative/fluid/pkg/utils/kubeclient"
@@ -17,26 +16,41 @@ import (
 )
 
 type DefaultMutator struct {
-	pvcName     string
-	runtimeInfo base.RuntimeInfoInterface
-	template    *common.FuseInjectionTemplate
-	options     common.FuseSidecarInjectOption
-
+	MutatingContext
 	client client.Client
 	log    logr.Logger
 }
 
 var _ FluidObjectMutator = &DefaultMutator{}
 
-func (mutator *DefaultMutator) Mutate(pod common.FluidObject, template *common.FuseInjectionTemplate, options common.FuseSidecarInjectOption) error {
+func (mutator *DefaultMutator) Mutate() error {
 	// 1. prepare platform-specific resources
 	if err := mutator.prepareResources(); err != nil {
 		return err
 	}
 
 	// 2. mutate & append volumes
+	if err := mutator.mutateDatasetVolume(); err != nil {
+		return err
+	}
 
-	// 3. mutate volume mounts
+	// 3. append volumes required by fuse sidecar container
+	if err := mutator.appendFuseSidecarVolumes(); err != nil {
+		return err
+	}
+
+	if mutator.datasetUsedInContainers {
+		if err := mutator.prependFuseContainer(false /* asInit */); err != nil {
+			return err
+		}
+	}
+
+	if mutator.datasetUsedInInitContainers {
+		if err := mutator.prependFuseContainer(true /* asInit */); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
@@ -101,11 +115,119 @@ func (mutator *DefaultMutator) prepareFuseContainerPostStartScript() error {
 	return nil
 }
 
-func (mutator *DefaultMutator) mutateVolumes(pod common.FluidObject) error {
-	volumes, err := pod.GetVolumes()
-	if err != nil {
-		return err
+func (mutator *DefaultMutator) mutateDatasetVolume() error {
+	volumes := mutator.specs.Volumes
+
+	mountPath := mutator.template.FuseMountInfo.MountPath
+	if mutator.template.FuseMountInfo.SubPath != "" {
+		mountPath = mountPath + "/" + mutator.template.FuseMountInfo.SubPath
 	}
 
-	
+	mutatedDatasetVolume := corev1.Volume{
+		Name: "",
+		VolumeSource: corev1.VolumeSource{
+			HostPath: &corev1.HostPathVolumeSource{
+				Path: mountPath,
+			},
+		},
+	}
+
+	var overriddenVolumeNames []string
+	for i, volume := range volumes {
+		if volume.PersistentVolumeClaim != nil && volume.PersistentVolumeClaim.ClaimName == mutator.pvcName {
+			name := volume.Name
+			volumes[i] = mutatedDatasetVolume
+			volumes[i].Name = name
+			overriddenVolumeNames = append(overriddenVolumeNames, name)
+		}
+	}
+
+	mountPropagationHostToContainer := corev1.MountPropagationHostToContainer
+
+	for _, container := range mutator.specs.Containers {
+		for i, volumeMount := range container.VolumeMounts {
+			if utils.ContainsString(overriddenVolumeNames, volumeMount.Name) {
+				container.VolumeMounts[i].MountPropagation = &mountPropagationHostToContainer
+			}
+		}
+	}
+
+	for _, container := range mutator.specs.InitContainers {
+		for i, volumeMount := range container.VolumeMounts {
+			if utils.ContainsString(overriddenVolumeNames, volumeMount.Name) {
+				container.VolumeMounts[i].MountPropagation = &mountPropagationHostToContainer
+			}
+		}
+	}
+
+	return nil
+}
+
+func (mutator *DefaultMutator) appendFuseSidecarVolumes() error {
+	// collect all volumes' names
+	var (
+		// volumeNames  = []string{}
+		volumesToAdd = mutator.template.VolumesToAdd
+		nameSuffix   = mutator.nameSuffix
+	)
+	// for _, volume := range mutator.specs.Volumes {
+	// 	volumeNames = append(volumeNames, volume.Name)
+	// }
+
+	// Append volumes
+	if len(volumesToAdd) > 0 {
+		mutator.log.V(1).Info("Before append volume", "original", mutator.specs.Volumes)
+		// volumes = append(volumes, template.VolumesToAdd...)
+		for _, volumeToAdd := range volumesToAdd {
+			// nameSuffix would be like: "-0", "-1", "-2", "-3", ...
+			oldVolumeName := volumeToAdd.Name
+			newVolumeName := volumeToAdd.Name + nameSuffix
+			// if utils.ContainsString(volumeNames, newVolumeName) {
+			// 	newVolumeName, err = s.randomizeNewVolumeName(newVolumeName, volumeNames)
+			// 	if err != nil {
+			// 		return volumeNamesConflict, volumes, err
+			// 	}
+			// }
+			volumeToAdd.Name = newVolumeName
+			// volumeNames = append(volumeNames, newVolumeName)
+			mutator.specs.Volumes = append(mutator.specs.Volumes, volumeToAdd)
+			if oldVolumeName != newVolumeName {
+				mutator.appendedVolumeNames[oldVolumeName] = newVolumeName
+			}
+		}
+
+		mutator.log.V(1).Info("After append volume", "original", mutator.specs.Volumes)
+	}
+
+	return nil
+}
+
+func (mutator *DefaultMutator) prependFuseContainer(asInit bool) error {
+	fuseContainer := mutator.template.FuseContainer
+	if !asInit {
+		fuseContainer.Name = common.FuseContainerName + mutator.nameSuffix
+	} else {
+		fuseContainer.Name = common.InitFuseContainerName + mutator.nameSuffix
+	}
+
+	if asInit {
+		fuseContainer.Lifecycle = nil
+		fuseContainer.Command = []string{"sleep"}
+		fuseContainer.Args = []string{"2s"}
+	}
+
+	for oldName, newName := range mutator.appendedVolumeNames {
+		for i, volumeMount := range fuseContainer.VolumeMounts {
+			if volumeMount.Name == oldName {
+				fuseContainer.VolumeMounts[i].Name = newName
+			}
+		}
+	}
+
+	if !asInit {
+		mutator.specs.Containers = append([]corev1.Container{fuseContainer}, mutator.specs.Containers...)
+	} else {
+		mutator.specs.InitContainers = append([]corev1.Container{fuseContainer}, mutator.specs.InitContainers...)
+	}
+	return nil
 }
