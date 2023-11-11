50870324db7f67161fe0bd661bd5f6375a279da8
shijie <1405151809@qq.com>
2023-11-11 17:18:51 +0800

    “修改了所有源码的license信息”
    Signed-off-by: shijie <1405151809@qq.com>

--- pkg/ddc/jindocache/node.go
+++ pkg/ddc/jindocache/node.go
@@ -1,17 +1,28 @@
-/*
-Copyright 2023 The Fluid Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+/* ==================================================================
+* Copyright (c) 2023,11.5.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+* 1. Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimer in the
+* documentation and/or other materials provided with the
+* distribution.
+* 3. All advertising materials mentioning features or use of this software
+* must display the following acknowledgement:
+* This product includes software developed by the xxx Group. and
+* its contributors.
+* 4. Neither the name of the Group nor the names of its contributors may
+* be used to endorse or promote products derived from this software
+* without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY xxx,GROUP AND CONTRIBUTORS
+* ===================================================================
+* Author: xiao shi jie.
 */
 
 package jindocache
--- pkg/utils/symlink.go
+++ pkg/utils/symlink.go
@@ -1,3 +1,29 @@
+/* ==================================================================
+* Copyright (c) 2023,11.5.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+* 1. Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimer in the
+* documentation and/or other materials provided with the
+* distribution.
+* 3. All advertising materials mentioning features or use of this software
+* must display the following acknowledgement:
+* This product includes software developed by the xxx Group. and
+* its contributors.
+* 4. Neither the name of the Group nor the names of its contributors may
+* be used to endorse or promote products derived from this software
+* without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY xxx,GROUP AND CONTRIBUTORS
+* ===================================================================
+* Author: xiao shi jie.
+*/
 package utils
 
 import (
