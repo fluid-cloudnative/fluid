0b0a48927bd8abbbf7ebab01501e620a2e07e6c6
frankleaf <frank.wt@alibaba-inc.com>
2023-11-13 15:29:01 +0800

    update
    Signed-off-by: frankleaf <frank.wt@alibaba-inc.com>

--- pkg/ddc/jindocache/transform_test.go
+++ pkg/ddc/jindocache/transform_test.go
@@ -1161,3 +1161,62 @@ func TestJindoCacheEngine_transformPolicy(t *testing.T) {
 		})
 	}
 }
+
+func TestTransformMasterVolume(t *testing.T) {
+	var tests = []struct {
+		runtime    *datav1alpha1.JindoRuntime
+		dataset    *datav1alpha1.Dataset
+		jindoValue *Jindo
+		expect     int
+	}{
+		{&datav1alpha1.JindoRuntime{
+			Spec: datav1alpha1.JindoRuntimeSpec{
+				Master: datav1alpha1.JindoCompTemplateSpec{
+					VolumeMounts: []corev1.VolumeMount{{
+						Name:      "nas",
+						MountPath: "test",
+						SubPath:   "/test",
+					}},
+				},
+				Volumes: []corev1.Volume{{
+					Name: "nas",
+				}},
+			},
+		}, &datav1alpha1.Dataset{
+			Spec: datav1alpha1.DatasetSpec{
+				Mounts: []datav1alpha1.Mount{{
+					MountPoint: "local:///mnt/test",
+					Name:       "test",
+					Path:       "/",
+				}},
+			}}, &Jindo{}, 1},
+		{&datav1alpha1.JindoRuntime{
+			Spec: datav1alpha1.JindoRuntimeSpec{
+				Master: datav1alpha1.JindoCompTemplateSpec{
+					VolumeMounts: []corev1.VolumeMount{{
+						Name:      "nas",
+						MountPath: "test",
+						SubPath:   "/test",
+					}},
+				},
+				Volumes: []corev1.Volume{{
+					Name: "nas-test",
+				}},
+			},
+		}, &datav1alpha1.Dataset{
+			Spec: datav1alpha1.DatasetSpec{
+				Mounts: []datav1alpha1.Mount{{
+					MountPoint: "local:///mnt/test",
+					Name:       "test",
+					Path:       "/",
+				}},
+			}}, &Jindo{}, 0},
+	}
+	for _, test := range tests {
+		engine := &JindoCacheEngine{Log: fake.NullLogger()}
+		engine.transformMasterVolumes(test.runtime, test.jindoValue)
+		if len(test.jindoValue.Master.VolumeMounts) != test.expect {
+			t.Errorf("expected value %v, but got %v", test.expect, len(test.jindoValue.Master.VolumeMounts))
+		}
+	}
+}
