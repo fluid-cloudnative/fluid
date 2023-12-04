3d4027bc79e5a6b4a1ad25ddf0563d1f20a89381
cheyang <cheyang@163.com>
2023-12-04 19:29:01 +0800

    Extend juicefs encript options support, To #51793350
    Signed-off-by: cheyang <cheyang@163.com>

--- pkg/ddc/juicefs/filter_test.go
+++ pkg/ddc/juicefs/filter_test.go
@@ -140,3 +140,48 @@ func TestBuildFormatCmdFilterForEnterpriseEditionAndFilterEncryptEnvOptions(t *t
 		})
 	}
 }
+
+func Test_buildFormatCmdFilterForCommunityEdition(t *testing.T) {
+	var mockOptions = map[string]string{
+		"JuiceBucket2":     "value",
+		"DisallowedOption": "value",
+	}
+
+	// Define test cases
+	testCases := []struct {
+		name           string
+		input          map[string]string
+		expectedOutput map[string]string
+	}{
+		{
+			name:           "Test with empty options",
+			input:          map[string]string{},
+			expectedOutput: map[string]string{},
+		},
+		{
+			name:  "Test with all allowed options",
+			input: mockOptions,
+			expectedOutput: map[string]string{"JuiceBucket2": "value",
+				"DisallowedOption": "value"},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// Create filter object
+			filter := buildFormatCmdFilterForCommunityEdition()
+
+			// Use filter to filter options
+			output := filter.filterOption(tc.input)
+
+			if len(output) == 0 && len(tc.expectedOutput) == 0 {
+				return
+			}
+
+			// Check if the filter result is as expected
+			if !reflect.DeepEqual(output, tc.expectedOutput) {
+				t.Errorf("filterOption() = %v, want %v", output, tc.expectedOutput)
+			}
+		})
+	}
+}
