3414cb6a36fae5abe267b4ce88ebf078e1d68d66
xliuqq <xlzq1992@gmail.com>
2024-01-09 17:20:46 +0800

    remove useless sshconfig
    Signed-off-by: xliuqq <xlzq1992@gmail.com>

--- pkg/common/sshconfig.go
+++
@@ -1,8 +0,0 @@
-package common
-
-type SSHConfig struct {
-	PrivateKey     string `json:"privateKey"`
-	PublicKey      string `json:"publicKey"`
-	AuthorizedKeys string `json:"authorizedKeys"`
-	Config         string `json:"config"`
-}
