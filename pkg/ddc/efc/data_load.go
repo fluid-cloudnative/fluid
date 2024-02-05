64818c3f14eef3a1ab498828a8417259d3ed0e32
高灏 Hao Gao <71384960+UYTFVBNJ@users.noreply.github.com>
2024-02-05 11:04:49 +0800

    update license of pkg/ddc/efc/data_load.go (#3554)
    * update license of pkg/ddc/efc/data_load.go
    
    * Update data_load.go
    
    ---------
    
    Co-authored-by: cheyang <cheyang@163.com>
    
    Signed-off-by: njucs <cqy191220018@github.com>

--- pkg/ddc/efc/data_load.go
+++ pkg/ddc/efc/data_load.go
@@ -1,5 +1,5 @@
 /*
-  Copyright 2022 The Fluid Authors.
+  Copyright 2023 The Fluid Authors.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
@@ -64,6 +64,7 @@ func (e *EFCEngine) CreateDataLoadJob(ctx cruntime.ReconcileRequestContext, targ
 	return err
 }
 
+// CheckRuntimeReady checks if runtime is ready or not
 func (e *EFCEngine) CheckRuntimeReady() (ready bool) {
 	// 1. check master ready
 	podName, containerName := e.getMasterPodInfo()
