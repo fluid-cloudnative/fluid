4d44dcf7e4ea2f2eaaa1b7a06bd19a9b7a12d28b
frankleaf <62129564+frankleaf@users.noreply.github.com>
2024-03-05 09:57:02 +0800

    fix transformToken (#3748)
    * fix transformToken
    
    Signed-off-by: frankleaf <frank.wt@alibaba-inc.com>
    
    * fix transformToken
    
    Signed-off-by: frankleaf <frank.wt@alibaba-inc.com>
    
    ---------
    
    Signed-off-by: frankleaf <frank.wt@alibaba-inc.com>

--- pkg/ddc/jindofsx/transform.go
+++ pkg/ddc/jindofsx/transform.go
@@ -169,7 +169,7 @@ func (e *JindoFSxEngine) transform(runtime *datav1alpha1.JindoRuntime) (value *J
 	if err != nil {
 		return
 	}
-	e.transformToken(runtime, value)
+	e.transformToken(value)
 	e.transformWorker(runtime, dataPath, userQuotas, value)
 	e.transformFuse(runtime, value)
 	e.transformInitPortCheck(value)
@@ -960,7 +960,7 @@ func (e *JindoFSxEngine) transformSecret(runtime *datav1alpha1.JindoRuntime, val
 	}
 }
 
-func (e *JindoFSxEngine) transformToken(runtime *datav1alpha1.JindoRuntime, value *Jindo) {
+func (e *JindoFSxEngine) transformToken(value *Jindo) {
 	properties := map[string]string{}
 	if len(value.Secret) != 0 {
 		properties["default.credential.provider"] = "secrets:///token/"
