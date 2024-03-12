05bbc3ce788c87cb0944d7c4dfbaaef9c6345160
trafalgarzzz <trafalgarz@outlook.com>
2024-03-12 14:59:49 +0800

    ThinRuntime controller supports setting rate limiter configuration
    Signed-off-by: trafalgarzzz <trafalgarz@outlook.com>

--- cmd/thin/app/thin.go
+++ cmd/thin/app/thin.go
@@ -18,6 +18,7 @@ package app
 
 import (
 	"os"
+	"time"
 
 	"github.com/fluid-cloudnative/fluid/pkg/controllers"
 	"github.com/fluid-cloudnative/fluid/pkg/ddc/thin"
@@ -56,6 +57,14 @@ var (
 	kubeClientBurst int
 )
 
+// configuration for controllers' rate limiter
+var (
+	controllerWorkqueueDefaultSyncBackoffStr string
+	controllerWorkqueueMaxSyncBackoffStr     string
+	controllerWorkqueueQPS                   int
+	controllerWorkqueueBurst                 int
+)
+
 var startCmd = &cobra.Command{
 	Use:   "start",
 	Short: "start thinruntime-controller in Kubernetes",
@@ -76,6 +85,10 @@ func init() {
 	startCmd.Flags().BoolVar(&eventDriven, "event-driven", true, "The reconciler's loop strategy. if it's false, it indicates period driven.")
 	startCmd.Flags().Float32VarP(&kubeClientQPS, "kube-api-qps", "", 20, "QPS to use while talking with kubernetes apiserver.")   // 20 is the default qps in controller-runtime
 	startCmd.Flags().IntVarP(&kubeClientBurst, "kube-api-burst", "", 30, "Burst to use while talking with kubernetes apiserver.") // 30 is the default burst in controller-runtime
+	startCmd.Flags().StringVar(&controllerWorkqueueDefaultSyncBackoffStr, "workqueue-default-sync-backoff", "5ms", "base backoff period for failed reconcilation in controller's workqueue")
+	startCmd.Flags().StringVar(&controllerWorkqueueMaxSyncBackoffStr, "workqueue-max-sync-backoff", "1000s", "max backoff period for failed reconcilation in controller's workqueue")
+	startCmd.Flags().IntVar(&controllerWorkqueueQPS, "workqueue-qps", 10, "qps limit value for controller's workqueue")
+	startCmd.Flags().IntVar(&controllerWorkqueueBurst, "workqueue-burst", 100, "burst limit value for controller's workqueue")
 }
 
 func handle() {
@@ -111,8 +124,21 @@ func handle() {
 		os.Exit(1)
 	}
 
+	defaultSyncBackoff, err := time.ParseDuration(controllerWorkqueueDefaultSyncBackoffStr)
+	if err != nil {
+		setupLog.Error(err, "workqueue-default-sync-backoff is not a valid duration, please use string like \"100ms\", \"5s\", \"3m\", ...")
+		os.Exit(1)
+	}
+
+	maxSyncBackoff, err := time.ParseDuration(controllerWorkqueueMaxSyncBackoffStr)
+	if err != nil {
+		setupLog.Error(err, "workqueue-max-sync-backoff is not a valid duration, please use string like \"100ms\", \"5s\", \"3m\", ...)")
+		os.Exit(1)
+	}
+
 	controllerOptions := controller.Options{
 		MaxConcurrentReconciles: maxConcurrentReconciles,
+		RateLimiter:             controllers.NewFluidControllerRateLimiter(defaultSyncBackoff, maxSyncBackoff, controllerWorkqueueQPS, controllerWorkqueueBurst),
 	}
 
 	if err = (thinctl.NewRuntimeReconciler(mgr.GetClient(),
