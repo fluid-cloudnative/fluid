a2664be2a4b03ca1527e91c0de904d6b2aa16368
Ye Cao <caoye.cao@alibaba-inc.com>
2024-04-09 10:23:13 +0800

    Add the tutotial about how to use vineyard runtime to accelerate the kubeflow pipelines. (#3779)
    * Add the tutotial about how to use vineyard runtime to accelerate the kubeflow pipelines.
    
    Signed-off-by: Ye Cao <caoye.cao@alibaba-inc.com>
    
    * Move the common source code and yamls to samples/vineyard.
    
    Signed-off-by: Ye Cao <caoye.cao@alibaba-inc.com>
    
    ---------
    
    Signed-off-by: Ye Cao <caoye.cao@alibaba-inc.com>

---
+++ samples/vineyard/pipeline.py
@@ -0,0 +1,57 @@
+from kfp import dsl
+
+def PreProcess(data_multiplier: int, registry: str):
+    op =  dsl.ContainerOp(
+        name='Preprocess Data',
+        image = f'{registry}/preprocess-data',
+        container_kwargs={
+            'image_pull_policy': "IfNotPresent",
+        },
+        pvolumes={
+            "/data": dsl.PipelineVolume(pvc="benchmark-data"),
+        },
+        command = ['python3', 'preprocess-data.py'],
+        arguments = [f'--data_multiplier={data_multiplier}'],
+    )
+    return op
+
+def Train(comp1, registry: str):
+    op = dsl.ContainerOp(
+        name='Train Data',
+        image=f'{registry}/train-data',
+        container_kwargs={
+            'image_pull_policy': "IfNotPresent",
+        },
+        pvolumes={
+            "/data": comp1.pvolumes['/data'],
+        },
+        command = ['python3', 'train-data.py'],
+    )
+    return op
+
+def Test(comp2, registry: str):
+    op = dsl.ContainerOp(
+        name='Test Data',
+        image=f'{registry}/test-data',
+        container_kwargs={
+            'image_pull_policy': "IfNotPresent",
+        },
+        pvolumes={
+            "/data": comp2.pvolumes['/data'],
+        },
+        command = ['python3', 'test-data.py'],
+    )
+    return op
+
+@dsl.pipeline(
+   name='Machine Learning Pipeline',
+   description='An example pipeline that trains and logs a regression model.'
+)
+def pipeline(data_multiplier: int, registry: str):
+    comp1 = PreProcess(data_multiplier=data_multiplier, registry=registry)
+    comp2 = Train(comp1, registry=registry)
+    comp3 = Test(comp2, registry=registry)
+
+if __name__ == '__main__':
+    from kfp import compiler
+    compiler.Compiler().compile(pipeline, __file__[:-3]+ '.yaml')
