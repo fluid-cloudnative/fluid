010a4b85a6bd83e232d40100ac5f4b67199a2010
tzwo <819853066@qq.com>
2024-05-07 13:27:20 +0800

    1

--- pkg/ddc/alluxio/transform_init_users_test.go
+++ pkg/ddc/alluxio/transform_init_users_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 The Fluid Author.
+Copyright 2021 The Fluid Author.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
