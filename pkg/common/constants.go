9f38aa406c6db61147f00a426053223089f390bf
melldy <miyue816@163.com>
2024-05-08 10:38:40 +0800

    This pr is to add license to ./pkg/common/constants.go
    Signed-off-by: melldy <miyue816@163.com>

--- pkg/common/constants.go
+++ pkg/common/constants.go
@@ -1,5 +1,4 @@
-/*
-Copyright <year> The <project-name> Authors.
+/*Copyright <year> The <project-name> Authors.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
