b095702c6b85441830b923893b3ea7d22a581c42
21098179-Huang Dongcheng <211098179@smail.nju.edu.cn>
2024-05-21 03:44:18 +0800

    fix transformers
    Signed-off-by: 21098179-Huang Dongcheng <211098179@smail.nju.edu.cn>

--- pkg/ddc/juicefs/data_load.go
+++ pkg/ddc/juicefs/data_load.go
@@ -24,7 +24,7 @@ import (
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	"github.com/fluid-cloudnative/fluid/pkg/utils/transfromers"
+	"github.com/fluid-cloudnative/fluid/pkg/utils/transformers"
 
 	v1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/yaml"
