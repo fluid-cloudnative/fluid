ecae987f5be3bd10274ec072d78193a226bce3ee
KlarkeZ <1719977655@qq.com>
2024-05-20 01:58:47 -0700

    cm1

--- pkg/ddc/alluxio/ufs.go
+++ pkg/ddc/alluxio/ufs.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 The Fluid Author.
+Copyright 2020 The Fluid Author.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
