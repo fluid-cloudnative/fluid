b8704c90f58685175727c4d7d8418293b6792c40
cheyang <cheyang@163.com>
2024-06-28 10:25:13 +0800

    Update data_load.go

--- pkg/ddc/juicefs/data_load.go
+++ pkg/ddc/juicefs/data_load.go
@@ -24,7 +24,7 @@ import (
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	"github.com/fluid-cloudnative/fluid/pkg/utils/transformers"
+	"github.com/fluid-cloudnative/fluid/pkg/utils/transformer"
 
 	v1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/yaml"
