03e387b9751f63b8601ac8f1dd34d09c6ff5d409
SouthWest7 <1403572259@qq.com>
2024-07-24 16:02:21 +0800

    Change Constant Variable Naming Convention for DEFAULT_MASTER_RPC_PORT in camelCase format defined in pkg/ddc/jindo/const.go and its reference in /pkg/ddc/jindo/transform.go
    Signed-off-by: SouthWest7 <1403572259@qq.com>

--- pkg/ddc/jindo/const.go
+++ pkg/ddc/jindo/const.go
@@ -38,7 +38,7 @@ const (
 	JINDO_MASTERNUM_DEFAULT = 1
 	JINDO_HA_MASTERNUM      = 3
 
-	defaultMasterRpcRort = 8101
+	defaultMasterRpcPort    = 8101
 	DEFAULT_WORKER_RPC_PORT = 6101
 	DEFAULT_RAFT_RPC_PORT   = 8103
 
