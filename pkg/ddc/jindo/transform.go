74d730cbd38c4a58d8b7d008acf7d5f5ab7e3827
SouthWest7 <1403572259@qq.com>
2024-07-24 16:03:06 +0800

    Change Constant Variable Naming Convention for DEFAULT_MASTER_RPC_PORT in camelCase format defined in pkg/ddc/jindo/const.go and its reference in /pkg/ddc/jindo/transform.go
    Signed-off-by: SouthWest7 <1403572259@qq.com>

--- pkg/ddc/jindo/transform.go
+++ pkg/ddc/jindo/transform.go
@@ -597,7 +597,7 @@ func (e *JindoEngine) allocatePorts(value *Jindo) error {
 	// usehostnetwork to choose port from port allocator
 	expectedPortNum := 2
 	if !value.UseHostNetwork {
-		value.Master.Port.Rpc = defaultMasterRpcRort
+		value.Master.Port.Rpc = defaultMasterRpcPort
 		value.Worker.Port.Rpc = DEFAULT_WORKER_RPC_PORT
 		if value.Master.ReplicaCount == JINDO_HA_MASTERNUM {
 			value.Master.Port.Raft = DEFAULT_RAFT_RPC_PORT
