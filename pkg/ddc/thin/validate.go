fb0538bfe4f4b2025688d4e0b9df7c6aa017e38b
trafalgarzzz <trafalgarz@outlook.com>
2024-08-02 17:24:40 +0800

    validate duplicate dataset mount fields
    Signed-off-by: trafalgarzzz <trafalgarz@outlook.com>

--- pkg/ddc/thin/validate.go
+++ pkg/ddc/thin/validate.go
@@ -17,10 +17,59 @@ limitations under the License.
 package thin
 
 import (
+	"fmt"
+
 	cruntime "github.com/fluid-cloudnative/fluid/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
+type validateFn func(ctx cruntime.ReconcileRequestContext) error
+
+var checks []validateFn = []validateFn{
+	validateDuplicateDatasetMounts,
+}
+
+func validateDuplicateDatasetMounts(ctx cruntime.ReconcileRequestContext) error {
+	if ctx.Dataset == nil {
+		return nil
+	}
+
+	if len(ctx.Dataset.Spec.Mounts) == 0 {
+		return nil
+	}
+
+	fieldErrorList := field.ErrorList{}
+
+	existedMountNames := map[string]int{}
+	existedMountPath := map[string]int{}
+	for idx, mount := range ctx.Dataset.Spec.Mounts {
+		path := mount.Path
+		if len(path) == 0 {
+			path = fmt.Sprintf("/%s", mount.Name)
+		}
+
+		if _, exists := existedMountNames[mount.Name]; exists {
+			fieldErrorList = append(fieldErrorList, field.Duplicate(field.NewPath("Dataset").Child("spec", "mounts").Index(idx).Child("name"), mount.Name))
+			continue // Skip to next iteration because collided mount names imply collided mount paths.
+		}
+
+		if _, exists := existedMountPath[path]; exists {
+			fieldErrorList = append(fieldErrorList, field.Duplicate(field.NewPath("Dataset").Child("spec", "mounts").Index(idx).Child("path"), path))
+			continue
+		}
+
+		existedMountNames[mount.Name] = idx
+		existedMountPath[path] = idx
+	}
+
+	return fieldErrorList.ToAggregate()
+}
+
 func (t *ThinEngine) Validate(ctx cruntime.ReconcileRequestContext) (err error) {
-	// TODO: impl validation logic for ThinEngine
+	for _, checkFn := range checks {
+		if err := checkFn(ctx); err != nil {
+			return err
+		}
+	}
 	return nil
 }
