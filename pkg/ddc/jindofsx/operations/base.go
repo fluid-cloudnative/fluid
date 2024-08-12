dd59abfa6e72ddcdf2c2592a68bee0aea3cc313d
trafalgarzzz <trafalgarz@outlook.com>
2024-08-12 13:29:20 +0800

    Sync ufs total size in a longer timeout
    Signed-off-by: trafalgarzzz <trafalgarz@outlook.com>

--- pkg/ddc/jindofsx/operations/base.go
+++ pkg/ddc/jindofsx/operations/base.go
@@ -65,27 +65,6 @@ func (a JindoFileUtils) exec(command []string, verbose bool) (stdout string, std
 	return
 }
 
-// exec with timeout
-func (a JindoFileUtils) execWithTimeOut(command []string, verbose bool, second int64) (stdout string, stderr string, err error) {
-	ctx, cancel := context.WithTimeout(context.TODO(), time.Second*time.Duration(second))
-	ch := make(chan string, 1)
-	defer cancel()
-
-	go func() {
-		stdout, stderr, err = a.execWithoutTimeout(command, verbose)
-		ch <- "done"
-	}()
-
-	select {
-	case <-ch:
-		a.log.V(1).Info("execute in time", "command", command)
-	case <-ctx.Done():
-		err = fmt.Errorf("timeout when executing %v", command)
-	}
-
-	return
-}
-
 // execWithoutTimeout
 func (a JindoFileUtils) execWithoutTimeout(command []string, verbose bool) (stdout string, stderr string, err error) {
 	err = cmdguard.ValidateCommandSlice(command)
@@ -174,8 +153,7 @@ func (a JindoFileUtils) GetUfsTotalSize(url string) (summary string, err error) 
 		stderr  string
 	)
 
-	// default 2min
-	stdout, stderr, err = a.execWithTimeOut(command, false, 120)
+	stdout, stderr, err = a.exec(command, false)
 
 	str := strings.Fields(stdout)
 
