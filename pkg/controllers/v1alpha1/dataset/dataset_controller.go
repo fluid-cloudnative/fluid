d623f314ed3f220b067114e00845041f9a5c3428
cheyang <cheyang52@gmail.com>
2024-10-12 18:51:41 +0800

    Docker build for fixing the skipping check mount ready (#4351)
    * Add logging for Finalizer is removed successfully, To #57240825
    
    Signed-off-by: cheyang <cheyang@163.com>
    
    * Add logging for Finalizer is removed successfully, To #57240825
    
    Signed-off-by: cheyang <cheyang@163.com>
    
    * Add logging for Finalizer is removed successfully, To #57240825
    
    Signed-off-by: cheyang <cheyang@163.com>
    
    * Build docker images for skipping check mount ready, To #57240825
    
    Signed-off-by: cheyang <cheyang@163.com>
    
    ---------
    
    Signed-off-by: cheyang <cheyang@163.com>

--- pkg/controllers/v1alpha1/dataset/dataset_controller.go
+++ pkg/controllers/v1alpha1/dataset/dataset_controller.go
@@ -87,9 +87,9 @@ func (r *DatasetReconciler) Reconcile(context context.Context, req ctrl.Request)
 		// return utils.RequeueIfError(err)
 	} else {
 		if scaleout {
-			ctx.Log.V(1).Info("scale out the runtime controller on demand successfully", "controller", controller)
+			ctx.Log.Info("scale out the runtime controller on demand successfully", "controller", controller)
 		} else {
-			ctx.Log.Info("no need to scale out the runtime controller because it's already scaled", "controller", controller)
+			ctx.Log.V(1).Info("no need to scale out the runtime controller because it's already scaled", "controller", controller)
 		}
 	}
 
@@ -220,7 +220,7 @@ func (r *DatasetReconciler) reconcileDatasetDeletion(ctx reconcileRequestContext
 				ctx.Log.Error(err, "DatasetRef has changed but update failed", "DatasetDeleteError", datasetToUpdate)
 				return utils.RequeueAfterInterval(time.Duration(10 * time.Second))
 			}
-			ctx.Log.V(1).Info("Update dataset datasetRef successfully", "Before", ctx.Dataset.Status.DatasetRef, "After", datasetRefToUpdate)
+			ctx.Log.Info("Update dataset datasetRef successfully", "Before", ctx.Dataset.Status.DatasetRef, "After", datasetRefToUpdate)
 			// if dataset has been updated, return to continue next round reconcile
 			return utils.RequeueAfterInterval(1 * time.Second)
 		}
@@ -242,7 +242,7 @@ func (r *DatasetReconciler) reconcileDatasetDeletion(ctx reconcileRequestContext
 			log.Error(err, "Failed to remove finalizer")
 			return ctrl.Result{}, err
 		}
-		ctx.Log.V(1).Info("Finalizer is removed", "dataset", ctx.Dataset)
+		ctx.Log.Info("Finalizer is removed", "dataset", ctx.Dataset)
 	}
 
 	log.Info("delete the dataset successfully", "dataset", ctx.Dataset)
