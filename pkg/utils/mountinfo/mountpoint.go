c416e574d6f48bff2d09a950d07417747ba17ade
cheyang <cheyang52@gmail.com>
2024-12-03 13:58:36 +0800

    Revert "fix: sort broken mountpoint and put subpaths at the end. (#4416)" (#4421)
    This reverts commit 1f99e3c8a152a8a5a9addbbf8b8314314c4acbfe.
    
    Signed-off-by: cheyang <cheyang@163.com>

--- pkg/utils/mountinfo/mountpoint.go
+++ pkg/utils/mountinfo/mountpoint.go
@@ -21,9 +21,8 @@ import (
 	"path"
 	"strings"
 
-	"github.com/golang/glog"
-
 	"github.com/fluid-cloudnative/fluid/pkg/utils"
+	"github.com/golang/glog"
 )
 
 type MountPoint struct {
@@ -35,7 +34,7 @@ type MountPoint struct {
 	NamespacedDatasetName string // <namespace>-<dataset>
 }
 
-func GetBrokenMountPoints() (MountPoints, error) {
+func GetBrokenMountPoints() ([]MountPoint, error) {
 	// get mountinfo from proc
 	mountByPath, err := loadMountInfo()
 	if err != nil {
@@ -82,7 +81,6 @@ func getBindMounts(mountByPath map[string]*Mount) (bindMountByName map[string][]
 	bindMountByName = make(map[string][]*Mount)
 	for k, m := range mountByPath {
 		var datasetNamespacedName string
-		mCopy := m
 		if strings.Contains(k, "kubernetes.io~csi") && strings.Contains(k, "mount") {
 			// fluid bind mount target path is: /{kubeletRootDir}(default: /var/lib/kubelet)/pods/{podUID}/volumes/kubernetes.io~csi/{namespace}-{datasetName}/mount
 			fields := strings.Split(k, "/")
@@ -90,7 +88,7 @@ func getBindMounts(mountByPath map[string]*Mount) (bindMountByName map[string][]
 				continue
 			}
 			datasetNamespacedName = fields[len(fields)-2]
-			bindMountByName[datasetNamespacedName] = append(bindMountByName[datasetNamespacedName], mCopy)
+			bindMountByName[datasetNamespacedName] = append(bindMountByName[datasetNamespacedName], m)
 		}
 		if strings.Contains(k, "volume-subpaths") {
 			// pod using subPath, bind mount path is: /{kubeletRootDir}(default: /var/lib/kubelet)/pods/{podUID}/volume-subpaths/{namespace}-{datasetName}/{containerName}/{volumeIndex}
@@ -99,7 +97,7 @@ func getBindMounts(mountByPath map[string]*Mount) (bindMountByName map[string][]
 				continue
 			}
 			datasetNamespacedName = fields[len(fields)-3]
-			bindMountByName[datasetNamespacedName] = append(bindMountByName[datasetNamespacedName], mCopy)
+			bindMountByName[datasetNamespacedName] = append(bindMountByName[datasetNamespacedName], m)
 		}
 	}
 	return
@@ -113,14 +111,7 @@ func getBrokenBindMounts(globalMountByName map[string]*Mount, bindMountByName ma
 			glog.V(6).Infof("ignoring mountpoint %s because of not finding its global mount point", name)
 			continue
 		}
-		for _, bm := range bindMounts {
-			bindMount := bm
-
-			if strings.HasSuffix(bindMount.Subtree, "//deleted") {
-				glog.V(5).Infof("bindMount subtree has been deleted, trim /deleted suffix, bindmount: %v", bindMount)
-				bindMount.Subtree = strings.TrimSuffix(bindMount.Subtree, "//deleted")
-			}
-
+		for _, bindMount := range bindMounts {
 			// In case of not sharing same peer group in mount info, meaning it a broken mount point
 			if len(utils.IntersectIntegerSets(bindMount.PeerGroups, globalMount.PeerGroups)) == 0 {
 				brokenMounts = append(brokenMounts, MountPoint{
@@ -135,26 +126,4 @@ func getBrokenBindMounts(globalMountByName map[string]*Mount, bindMountByName ma
 		}
 	}
 	return
-}
-
-type MountPoints []MountPoint
-
-func (mp MountPoints) Len() int { return len(mp) }
-func (mp MountPoints) Less(i, j int) bool {
-	if strings.Contains(mp[i].MountPath, "main") && !strings.Contains(mp[j].MountPath, "main") {
-		return false
-	}
-	if !strings.Contains(mp[i].MountPath, "main") && strings.Contains(mp[j].MountPath, "main") {
-		return true
-	}
-	if strings.Contains(mp[i].MountPath, "subpath") && !strings.Contains(mp[j].MountPath, "subpath") {
-		return false
-	}
-	if !strings.Contains(mp[i].MountPath, "subpath") && strings.Contains(mp[j].MountPath, "subpath") {
-		return true
-	}
-	return mp[i].MountPath < mp[j].MountPath
-}
-func (mp MountPoints) Swap(i, j int) {
-	mp[i].MountPath, mp[j].MountPath = mp[j].MountPath, mp[i].MountPath
 }
