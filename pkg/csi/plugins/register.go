3407f4031655b1d657791bd9dbbb632c05c1b93a
trafalgarzzz <trafalgarz@outlook.com>
2025-02-11 14:15:24 +0800

    Support `useNodeAuthorization` option to specify whether or not to use node authorized client for CSI
    Signed-off-by: trafalgarzzz <trafalgarz@outlook.com>

--- pkg/csi/plugins/register.go
+++ pkg/csi/plugins/register.go
@@ -17,14 +17,35 @@ limitations under the License.
 package plugins
 
 import (
+	"os"
+
 	"github.com/fluid-cloudnative/fluid/pkg/csi/config"
 	"github.com/fluid-cloudnative/fluid/pkg/utils/kubelet"
+	"github.com/golang/glog"
+	"github.com/pkg/errors"
+	"k8s.io/client-go/kubernetes"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
+// getNodeAuthorizedClientFromKubeletConfig retrieves a node-authorized Kubernetes client from the Kubelet configuration file.
+// This function checks if the specified Kubelet configuration file exists. If the file does not exist, it returns an empty client without an error .
+// If the file exists, it attempts to initialize and return a node-authorized Kubernetes client.
+func getNodeAuthorizedClientFromKubeletConfig(kubeletConfigPath string) (*kubernetes.Clientset, error) {
+	_, err := os.Stat(kubeletConfigPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			glog.Warningf("kubelet config file %s not exists, continue without node authorization...", kubeletConfigPath)
+			return nil, nil
+		}
+		return nil, errors.Wrapf(err, "fail to stat kubelet config file %s", kubeletConfigPath)
+	}
+
+	return kubelet.InitNodeAuthorizedClient(kubeletConfigPath)
+}
+
 // Register initializes the csi driver and registers it to the controller manager.
 func Register(mgr manager.Manager, ctx config.RunningContext) error {
-	client, err := kubelet.InitNodeAuthorizedClient(ctx.KubeletConfigPath)
+	client, err := getNodeAuthorizedClientFromKubeletConfig(ctx.KubeletConfigPath)
 	if err != nil {
 		return err
 	}
