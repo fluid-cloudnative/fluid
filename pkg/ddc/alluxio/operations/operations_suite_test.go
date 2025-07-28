f6362c806cf12186b8f60372cf3a2f1c1542106e
trafalgarzzz <trafalgarz@outlook.com>
2025-07-28 11:54:13 +0800

    make pkg/ddc/alluxio/operations unit tests support arm arch powered by lingma
    Signed-off-by: trafalgarzzz <trafalgarz@outlook.com>

--- pkg/ddc/alluxio/operations/operations_suite_test.go
+++ pkg/ddc/alluxio/operations/operations_suite_test.go
@@ -7,7 +7,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func TestAlluxioFileUtilsSuite(t *testing.T) {
+func TestOperations(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "AlluxioFileUtils Suite")
+	RunSpecs(t, "Operations Suite")
 }
