40c8ab62d2a35fdf952b245c0076d0c4ebfec1fd
trafalgarzzz <trafalgarz@outlook.com>
2025-09-10 20:32:49 +0800

    fix unit tests for ctrl functions
    Signed-off-by: trafalgarzzz <trafalgarz@outlook.com>

--- pkg/ctrl/replicas_test.go
+++ pkg/ctrl/replicas_test.go
@@ -18,7 +18,6 @@ package ctrl
 
 import (
 	"context"
-	"testing"
 
 	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
 	"github.com/fluid-cloudnative/fluid/pkg/common"
@@ -26,294 +25,195 @@ import (
 	cruntime "github.com/fluid-cloudnative/fluid/pkg/runtime"
 	"github.com/fluid-cloudnative/fluid/pkg/utils/fake"
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
 	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
-// func newAlluxioEngineREP(client client.Client, name string, namespace string) *alluxio.AlluxioEngine {
+var _ = Describe("Ctrl Replicas Tests", func() {
+	// Shared variables for all tests
+	var (
+		helper       *Helper
+		resources    []runtime.Object
+		k8sClient    client.Client
+		runtimeInfo  base.RuntimeInfoInterface
+		fluidRuntime *datav1alpha1.JuiceFSRuntime
+		dataset      *datav1alpha1.Dataset
+		workerSts    *appsv1.StatefulSet
+	)
 
-// 	runTimeInfo, _ := base.BuildRuntimeInfo(name, namespace, "alluxio", datav1alpha1.TieredStore{})
-// 	engine := &alluxio.AlluxioEngine{
-// 		runtime:     &datav1alpha1.AlluxioRuntime{},
-// 		name:        name,
-// 		namespace:   namespace,
-// 		Client:      client,
-// 		runtimeInfo: runTimeInfo,
-// 		Log:         fake.NullLogger(),
-// 	}
-// 	return engine
-// }
-
-func TestSyncReplicas(t *testing.T) {
-
-	runtimeInputs := []*datav1alpha1.JindoRuntime{
-		{
+	BeforeEach(func() {
+		// Initialize shared resources
+		fluidRuntime = &datav1alpha1.JuiceFSRuntime{
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hbase",
+				Name:      "test",
 				Namespace: "fluid",
 			},
-			Spec: datav1alpha1.JindoRuntimeSpec{
-				Replicas: 3, // 2
+			Spec: datav1alpha1.JuiceFSRuntimeSpec{
+				Replicas: 3,
 			},
-			Status: datav1alpha1.RuntimeStatus{
-				CurrentWorkerNumberScheduled: 2,
-				CurrentMasterNumberScheduled: 2, // 0
-				CurrentFuseNumberScheduled:   2,
-				DesiredMasterNumberScheduled: 3,
-				DesiredWorkerNumberScheduled: 2,
-				DesiredFuseNumberScheduled:   3,
-				WorkerPhase:                  "NotReady",
-				FusePhase:                    "NotReady",
-			},
-		},
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hadoop",
-				Namespace: "fluid",
-			},
-			Spec: datav1alpha1.JindoRuntimeSpec{
-				Replicas: 2,
-			},
-			Status: datav1alpha1.RuntimeStatus{
-				CurrentWorkerNumberScheduled: 3,
-				CurrentMasterNumberScheduled: 3,
-				CurrentFuseNumberScheduled:   3,
-				DesiredMasterNumberScheduled: 2,
-				DesiredWorkerNumberScheduled: 3,
-				DesiredFuseNumberScheduled:   2,
-				WorkerPhase:                  "NotReady",
-				FusePhase:                    "NotReady",
-			},
-		},
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "obj",
-				Namespace: "fluid",
-			},
-			Spec: datav1alpha1.JindoRuntimeSpec{
-				Replicas: 2,
-			},
-			Status: datav1alpha1.RuntimeStatus{
-				CurrentWorkerNumberScheduled: 2,
-				CurrentMasterNumberScheduled: 2,
-				CurrentFuseNumberScheduled:   2,
-				DesiredMasterNumberScheduled: 2,
-				DesiredWorkerNumberScheduled: 2,
-				DesiredFuseNumberScheduled:   2,
+			Status: datav1alpha1.RuntimeStatus{},
+		}
 
-				WorkerPhase: "NotReady",
-				FusePhase:   "NotReady",
-			},
-		},
-	}
-
-	dataSetInputs := []*datav1alpha1.Dataset{
-		{
+		dataset = &datav1alpha1.Dataset{
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hbase",
+				Name:      "test",
 				Namespace: "fluid",
 			},
-		},
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hadoop",
-				Namespace: "fluid",
-			},
-		},
-	}
+		}
 
-	statefulsetInputs := []*appsv1.StatefulSet{
-		{
+		workerSts = &appsv1.StatefulSet{
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hbase-jindofs-worker",
-				Namespace: "big-data",
-			},
-			Spec: appsv1.StatefulSetSpec{
-				Replicas: ptr.To[int32](2),
-			},
-			Status: appsv1.StatefulSetStatus{
-				ReadyReplicas: 1,
-			},
-		}, {
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hadoop-jindofs-worker",
+				Name:      "test-worker",
 				Namespace: "fluid",
 			},
 			Spec: appsv1.StatefulSetSpec{
-				Replicas: ptr.To[int32](3),
+				Replicas: nil, // should be set in each test case
 			},
-			Status: appsv1.StatefulSetStatus{
-				ReadyReplicas: 1,
-			},
-		}, {
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "obj-jindofs-worker",
-				Namespace: "fluid",
-			},
-			Spec: appsv1.StatefulSetSpec{
-				Replicas: ptr.To[int32](3),
-			},
-			Status: appsv1.StatefulSetStatus{
-				ReadyReplicas: 1,
-			},
-		},
-	}
-
-	objs := []runtime.Object{}
-
-	for _, runtimeInput := range runtimeInputs {
-		objs = append(objs, runtimeInput.DeepCopy())
-	}
-	for _, dataSetInput := range dataSetInputs {
-		objs = append(objs, dataSetInput.DeepCopy())
-	}
-	for _, statefulsetInput := range statefulsetInputs {
-		objs = append(objs, statefulsetInput.DeepCopy())
-	}
-
-	s := runtime.NewScheme()
-	_ = datav1alpha1.AddToScheme(s)
-	_ = appsv1.AddToScheme(s)
-	fakeClient := fake.NewFakeClientWithScheme(s, objs...)
-	testCases := []struct {
-		caseName         string
-		name             string
-		namespace        string
-		Type             datav1alpha1.RuntimeConditionType
-		worker           *appsv1.StatefulSet
-		isErr            bool
-		expectConditions int
-	}{
-		{
-			caseName:  "scaleOut",
-			name:      "hbase",
-			namespace: "fluid",
-			worker: &appsv1.StatefulSet{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "hbase-jindofs-worker",
-					Namespace: "big-data",
-				},
-				Spec: appsv1.StatefulSetSpec{
-					Replicas: ptr.To[int32](2),
-				},
-				Status: appsv1.StatefulSetStatus{
-					ReadyReplicas: 1,
-				},
-			},
-			Type:             datav1alpha1.RuntimeWorkerScaledOut,
-			isErr:            false,
-			expectConditions: 2,
-		},
-		{
-			caseName:  "scaleIn",
-			name:      "hadoop",
-			namespace: "fluid",
-			worker: &appsv1.StatefulSet{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "hadoop-jindofs-worker",
-					Namespace: "fluid",
-				},
-				Spec: appsv1.StatefulSetSpec{
-					Replicas: ptr.To[int32](3),
-				},
-				Status: appsv1.StatefulSetStatus{
-					ReadyReplicas: 1,
-				},
-			},
-			Type:             datav1alpha1.RuntimeWorkerScaledIn,
-			isErr:            false,
-			expectConditions: 2,
-		},
-		{
-
-			caseName:  "noAction",
-			name:      "obj",
-			namespace: "fluid",
-			worker: &appsv1.StatefulSet{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "obj-jindofs-worker",
-					Namespace: "fluid",
-				},
-				Spec: appsv1.StatefulSetSpec{
-					Replicas: ptr.To[int32](3),
-				},
-				Status: appsv1.StatefulSetStatus{
-					ReadyReplicas: 1,
-				},
-			},
-			Type:             "",
-			isErr:            false,
-			expectConditions: 0,
-		},
-	}
-	for _, testCase := range testCases {
-
-		runtimeInfo, err := base.BuildRuntimeInfo(testCase.name, testCase.namespace, common.JindoRuntime)
-		if err != nil {
-			t.Errorf("testcase %s failed due to %v", testCase.name, err)
 		}
+	})
 
-		var runtime *datav1alpha1.JindoRuntime = &datav1alpha1.JindoRuntime{}
+	JustBeforeEach(func() {
+		k8sClient = fake.NewFakeClientWithScheme(datav1alpha1.UnitTestScheme, resources...)
+		runtimeInfo, _ = base.BuildRuntimeInfo(fluidRuntime.Name, fluidRuntime.Namespace, common.JuiceFSRuntime)
+		helper = BuildHelper(runtimeInfo, k8sClient, fake.NullLogger())
+	})
 
-		err = fakeClient.Get(context.TODO(), types.NamespacedName{
-			Namespace: testCase.namespace,
-			Name:      testCase.name,
-		}, runtime)
+	Describe("Test Helper.SyncReplicas()", func() {
+		When("Runtime has a spec.replicas larger than worker sts's spec.replicas", func() {
+			BeforeEach(func() {
+				// Override specific settings for scale out test
+				fluidRuntime.Spec.Replicas = 3
+				workerSts.Spec.Replicas = ptr.To[int32](2)
+				fluidRuntime.Status.DesiredWorkerNumberScheduled = 2
 
-		if err != nil {
-			t.Errorf("testCase %s sync replicas failed,err:%v", testCase.caseName, err)
-		}
+				resources = []runtime.Object{
+					fluidRuntime,
+					dataset,
+					workerSts,
+				}
+			})
 
-		statefulset := &appsv1.StatefulSet{}
-		err = fakeClient.Get(context.TODO(), types.NamespacedName{
-			Namespace: testCase.worker.Namespace,
-			Name:      testCase.worker.Name,
-		}, statefulset)
-		if err != nil {
-			t.Errorf("sync replicas failed,err:%s", err.Error())
-		}
+			It("should scale out worker statefulset and update runtime conditions", func() {
+				ctx := cruntime.ReconcileRequestContext{
+					Log:      fake.NullLogger(),
+					Recorder: record.NewFakeRecorder(300),
+				}
 
-		h := BuildHelper(runtimeInfo, fakeClient, fake.NullLogger())
-		err = h.SyncReplicas(cruntime.ReconcileRequestContext{
-			Log:      fake.NullLogger(),
-			Recorder: record.NewFakeRecorder(300),
-		}, runtime, runtime.Status, statefulset)
+				// Perform sync replicas operation
+				err := helper.SyncReplicas(ctx, fluidRuntime, fluidRuntime.Status, workerSts)
+				Expect(err).NotTo(HaveOccurred())
 
-		if err != nil {
-			t.Errorf("sync replicas failed,err:%s", err.Error())
-		}
+				// Verify the updated runtime
+				updatedRuntime := &datav1alpha1.JuiceFSRuntime{}
+				err = k8sClient.Get(context.TODO(), types.NamespacedName{
+					Namespace: fluidRuntime.Namespace,
+					Name:      fluidRuntime.Name,
+				}, updatedRuntime)
+				Expect(err).NotTo(HaveOccurred())
 
-		err = fakeClient.Get(context.TODO(), types.NamespacedName{
-			Namespace: testCase.namespace,
-			Name:      testCase.name,
-		}, runtime)
+				// Check that conditions are updated
+				Expect(updatedRuntime.Status.Conditions).To(HaveLen(2))
+				Expect(updatedRuntime.Status.Conditions[0].Type).To(Equal(datav1alpha1.RuntimeWorkerScaledOut))
+				Expect(updatedRuntime.Status.Conditions[0].Status).To(Equal(corev1.ConditionTrue))
+				Expect(updatedRuntime.Status.Conditions[1].Type).To(Equal(datav1alpha1.RuntimeWorkersInitialized))
+				Expect(updatedRuntime.Status.Conditions[1].Status).To(Equal(corev1.ConditionTrue))
 
-		if err != nil {
-			t.Errorf("sync replicas failed,err:%v", err)
-		}
+				updatedSts := &appsv1.StatefulSet{}
+				err = k8sClient.Get(ctx, types.NamespacedName{Name: "test-worker", Namespace: "fluid"}, updatedSts)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(*updatedSts.Spec.Replicas).To(Equal(int32(3)))
+			})
+		})
 
-		// rt, _ := engine.getRuntime()
-		if len(runtime.Status.Conditions) != testCase.expectConditions {
+		When("Runtime has a spec.replicas smaller than worker sts's spec.replicas", func() {
+			BeforeEach(func() {
+				// Override specific settings for scale in test
+				fluidRuntime.Spec.Replicas = 2
+				workerSts.Spec.Replicas = ptr.To[int32](3)
+				fluidRuntime.Status.DesiredWorkerNumberScheduled = 3
 
-			t.Errorf("runtime condition want count %v, got  condtions %v", testCase.expectConditions, runtime.Status.Conditions)
+				resources = []runtime.Object{
+					fluidRuntime,
+					dataset,
+					workerSts,
+				}
+			})
 
-		}
+			It("should scale in worker statefulset and update runtime conditions", func() {
+				ctx := cruntime.ReconcileRequestContext{
+					Log:      fake.NullLogger(),
+					Recorder: record.NewFakeRecorder(300),
+				}
 
-		found := false
-		for _, cond := range runtime.Status.Conditions {
+				// Perform sync replicas operation
+				err := helper.SyncReplicas(ctx, fluidRuntime, fluidRuntime.Status, workerSts)
+				Expect(err).NotTo(HaveOccurred())
 
-			if cond.Type == testCase.Type {
-				found = true
-				break
-			}
-		}
+				// Verify the updated runtime
+				updatedRuntime := &datav1alpha1.JuiceFSRuntime{}
+				err = k8sClient.Get(context.TODO(), types.NamespacedName{
+					Namespace: fluidRuntime.Namespace,
+					Name:      fluidRuntime.Name,
+				}, updatedRuntime)
+				Expect(err).NotTo(HaveOccurred())
 
-		if !found && testCase.expectConditions > 0 {
-			t.Errorf("runtime condition want conditionType %v, got  conditions %v", testCase.Type, runtime.Status.Conditions)
+				// Check that conditions are updated
+				Expect(updatedRuntime.Status.Conditions).To(HaveLen(2))
+				Expect(updatedRuntime.Status.Conditions[0].Type).To(Equal(datav1alpha1.RuntimeWorkerScaledIn))
+				Expect(updatedRuntime.Status.Conditions[0].Status).To(Equal(corev1.ConditionTrue))
+				Expect(updatedRuntime.Status.Conditions[1].Type).To(Equal(datav1alpha1.RuntimeWorkersInitialized))
+				Expect(updatedRuntime.Status.Conditions[1].Status).To(Equal(corev1.ConditionTrue))
 
-		}
-	}
-}
+				updatedSts := &appsv1.StatefulSet{}
+				err = k8sClient.Get(ctx, types.NamespacedName{Name: "test-worker", Namespace: "fluid"}, updatedSts)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(*updatedSts.Spec.Replicas).To(Equal(int32(2)))
+			})
+		})
+
+		Context("When no runtime has a spec.replicas equal to worker sts's spec.replicas", func() {
+			BeforeEach(func() {
+				// Override specific settings for no action test
+				fluidRuntime.Spec.Replicas = 2
+				workerSts.Spec.Replicas = ptr.To[int32](2)
+				fluidRuntime.Status.DesiredWorkerNumberScheduled = 2
+
+				resources = []runtime.Object{
+					fluidRuntime,
+					dataset,
+					workerSts,
+				}
+			})
+
+			It("should not update runtime conditions when no scaling is needed", func() {
+				ctx := cruntime.ReconcileRequestContext{
+					Log:      fake.NullLogger(),
+					Recorder: record.NewFakeRecorder(300),
+				}
+
+				// Perform sync replicas operation
+				err := helper.SyncReplicas(ctx, fluidRuntime, fluidRuntime.Status, workerSts)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Verify the updated runtime
+				updatedRuntime := &datav1alpha1.JuiceFSRuntime{}
+				err = k8sClient.Get(context.TODO(), types.NamespacedName{
+					Namespace: fluidRuntime.Namespace,
+					Name:      fluidRuntime.Name,
+				}, updatedRuntime)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Check that conditions are not updated
+				Expect(updatedRuntime.Status.Conditions).To(HaveLen(0))
+			})
+		})
+	})
+})
