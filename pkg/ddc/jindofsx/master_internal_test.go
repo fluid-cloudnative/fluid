f1deebbd0a2718572844acba67bb1bb577eb127a
xliuqq <xlzq1992@gmail.com>
2025-11-16 20:10:17 +0800

    use gomonkey instead of gohook for pkg/ddc/jindofsx
    Signed-off-by: xliuqq <xlzq1992@gmail.com>

--- pkg/ddc/jindofsx/master_internal_test.go
+++ pkg/ddc/jindofsx/master_internal_test.go
@@ -19,7 +19,7 @@ package jindofsx
 import (
 	"testing"
 
-	"github.com/brahma-adshonor/gohook"
+	"github.com/agiledragon/gomonkey/v2"
 	"github.com/fluid-cloudnative/fluid/pkg/common"
 	"github.com/pkg/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -49,19 +49,6 @@ func TestSetupMasterInternal(t *testing.T) {
 	}
 	mockExecInstallReleaseErr := func(name string, namespace string, valueFile string, chartName string) error {
 		return errors.New("fail to install dataload chart")
-	}
-
-	wrappedUnhookCheckRelease := func() {
-		err := gohook.UnHook(helm.CheckRelease)
-		if err != nil {
-			t.Fatal(err.Error())
-		}
-	}
-	wrappedUnhookInstallRelease := func() {
-		err := gohook.UnHook(helm.InstallRelease)
-		if err != nil {
-			t.Fatal(err.Error())
-		}
 	}
 
 	allixioruntime := &datav1alpha1.JindoRuntime{
@@ -115,48 +102,30 @@ func TestSetupMasterInternal(t *testing.T) {
 	}
 
 	// check release found
-	err = gohook.Hook(helm.CheckRelease, mockExecCheckReleaseCommonFound, nil)
-	if err != nil {
-		t.Fatal(err.Error())
-	}
+	patches := gomonkey.ApplyFunc(helm.CheckRelease, mockExecCheckReleaseCommonFound)
+	defer patches.Reset()
 	_ = engine.setupMasterInernal()
-	wrappedUnhookCheckRelease()
 
 	// check release error
-	err = gohook.Hook(helm.CheckRelease, mockExecCheckReleaseErr, nil)
-	if err != nil {
-		t.Fatal(err.Error())
-	}
+	patches.ApplyFunc(helm.CheckRelease, mockExecCheckReleaseErr)
 	err = engine.setupMasterInernal()
 	if err == nil {
 		t.Errorf("fail to catch the error")
 	}
-	wrappedUnhookCheckRelease()
 
 	// check release not found
-	err = gohook.Hook(helm.CheckRelease, mockExecCheckReleaseCommonNotFound, nil)
-	if err != nil {
-		t.Fatal(err.Error())
-	}
+	patches.ApplyFunc(helm.CheckRelease, mockExecCheckReleaseCommonNotFound)
 
 	// install release with error
-	err = gohook.Hook(helm.InstallRelease, mockExecInstallReleaseErr, nil)
-	if err != nil {
-		t.Fatal(err.Error())
-	}
+	patches.ApplyFunc(helm.InstallRelease, mockExecInstallReleaseErr)
 	err = engine.setupMasterInernal()
 	if err == nil {
 		t.Errorf("fail to catch the error")
 	}
-	wrappedUnhookInstallRelease()
 
 	// install release successfully
-	err = gohook.Hook(helm.InstallRelease, mockExecInstallReleaseCommon, nil)
-	if err != nil {
-		t.Fatal(err.Error())
-	}
+	patches.ApplyFunc(helm.InstallRelease, mockExecInstallReleaseCommon)
 	_ = engine.setupMasterInernal()
-	wrappedUnhookInstallRelease()
 }
 
 func TestGenerateJindoValueFile(t *testing.T) {
