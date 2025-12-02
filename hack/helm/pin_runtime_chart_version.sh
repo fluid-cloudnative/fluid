3e4730b13eb1c39ad114dcdc906860863e2a3873
TzZtzt <trafalgarz@outlook.com>
2025-12-02 15:17:36 +0800

    build: pin runtime chart version to ensure consistency
    - Add a script to inject the specified Fluid version into all charts
    - Ensure all charts (except Fluid control-plane and library charts) have the correct appVersion
    - This change improves consistency and traceability of Fluid versions across charts
    
    Signed-off-by: TzZtzt <trafalgarz@outlook.com>

---
+++ hack/helm/pin_runtime_chart_version.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# USAGE:
+#   FLUID_HOME=/path/to/fluid
+#   cd $FLUID_HOME
+#   bash hack/helm/inject_library_chart.sh
+
+set -e
+
+fluid_version=$1
+if [ -z "$fluid_version" ]; then
+  echo "$0 got an error: please specify a non-empty Fluid version to continue"
+  exit 1
+fi
+
+# All charts should be injected except for Fluid control-plane chart and library chart itself.
+charts=$(find charts | grep Chart.yaml | grep -v "charts/fluid/\|charts/library" | xargs -I _ dirname _)
+
+for chart in $charts; do
+    if [[ -f "$chart/Chart.yaml" ]]; then
+        echo "Processing $chart/Chart.yaml."
+        if [[ $(grep -c "appVersion:" $chart/Chart.yaml) == 0 ]]; then
+            echo "appVersion: $fluid_version" >> $chart/Chart.yaml
+        else
+            sed -i "" 's/appVersion:.*/appVersion: '"$fluid_version"'/' $chart/Chart.yaml
+        fi
+    fi
+done
