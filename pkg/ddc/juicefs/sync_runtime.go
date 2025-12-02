3a90d4c512c1b0aefed86c75e12080f8620f5e33
TzZtzt <trafalgarz@outlook.com>
2025-12-02 10:56:10 +0800

    feat(controller): add option to skip runtime syncing
    - Add environment variable CONTROLLER_SKIP_SYNCING_RUNTIME to control runtime syncing
    - Implement logic to skip runtime syncing if the environment variable is set to true
    - Update JindoCacheEngine, JindoFSxEngine, and JuiceFSEngine to use the new option
    
    Signed-off-by: TzZtzt <trafalgarz@outlook.com>

--- pkg/ddc/juicefs/sync_runtime.go
+++ pkg/ddc/juicefs/sync_runtime.go
@@ -35,10 +35,16 @@ import (
 	cruntime "github.com/fluid-cloudnative/fluid/pkg/runtime"
 	"github.com/fluid-cloudnative/fluid/pkg/utils"
 	"github.com/fluid-cloudnative/fluid/pkg/utils/kubeclient"
+	runtimeOpts "github.com/fluid-cloudnative/fluid/pkg/utils/runtimes/options"
 )
 
 // SyncRuntime syncs the runtime spec
 func (j *JuiceFSEngine) SyncRuntime(ctx cruntime.ReconcileRequestContext) (changed bool, err error) {
+	if runtimeOpts.ControllerSkipSyncingRuntime() {
+		j.Log.V(1).Info("ControllerSkipSyncRuntime is enabled, skip syncing runtime")
+		return
+	}
+
 	runtime, err := j.getRuntime()
 	if err != nil {
 		return
--- pkg/utils/runtimes/options/init.go
+++ pkg/utils/runtimes/options/init.go
@@ -29,4 +29,5 @@ var (
 func init() {
 	setPortCheckOption()
 	setCriticalFusePodOption()
+	setControllerSkipSyncingRuntime()
 }
