2357cd62abfa50bb197ce7c47d8d3b4ade215fac
TzZtzt <trafalgarz@outlook.com>
2025-12-04 20:37:27 +0800

    fix unit tests
    Signed-off-by: TzZtzt <trafalgarz@outlook.com>

--- pkg/utils/resources_test.go
+++ pkg/utils/resources_test.go
@@ -35,60 +35,60 @@ func TestTransformRequirementsToResources(t *testing.T) {
 				corev1.ResourceList{"cpu": resource.MustParse("100m")},
 				corev1.ResourceList{"cpu": resource.MustParse("200m")},
 			),
-			wantRes: mockCommonResource(
-				common.ResourceList{"cpu": "100m"},
-				common.ResourceList{"cpu": "200m"},
-			),
+			wantRes: common.Resources{
+				Requests: common.ResourceList{"cpu": "100m"},
+				Limits:   common.ResourceList{"cpu": "200m"},
+			},
 		},
 		"test resource transform case 2": {
 			required: mockRequiredResource(
 				corev1.ResourceList{"cpu": resource.MustParse("100m")},
 				corev1.ResourceList{},
 			),
-			wantRes: mockCommonResource(
-				common.ResourceList{"cpu": "100m"},
-				common.ResourceList{},
-			),
+			wantRes: common.Resources{
+				Requests: common.ResourceList{"cpu": "100m"},
+				Limits:   common.ResourceList{},
+			},
 		},
 		"test resource transform case 3": {
 			required: mockRequiredResource(
 				corev1.ResourceList{"memory": resource.MustParse("100Gi"), "cpu": resource.MustParse("100m")},
 				corev1.ResourceList{"memory": resource.MustParse("600Gi"), "cpu": resource.MustParse("600m")},
 			),
-			wantRes: mockCommonResource(
-				common.ResourceList{"memory": "100Gi", "cpu": "100m"},
-				common.ResourceList{"memory": "600Gi", "cpu": "600m"},
-			),
+			wantRes: common.Resources{
+				Requests: common.ResourceList{"memory": "100Gi", "cpu": "100m"},
+				Limits:   common.ResourceList{"memory": "600Gi", "cpu": "600m"},
+			},
 		},
 		"test resource transform case 4": {
 			required: mockRequiredResource(
 				corev1.ResourceList{},
 				corev1.ResourceList{"nvidia.com/gpu": resource.MustParse("1")},
 			),
-			wantRes: mockCommonResource(
-				common.ResourceList{},
-				common.ResourceList{"nvidia.com/gpu": "1"},
-			),
+			wantRes: common.Resources{
+				Requests: common.ResourceList{},
+				Limits:   common.ResourceList{"nvidia.com/gpu": "1"},
+			},
 		},
 		"test resource transform case 5": {
 			required: mockRequiredResource(
 				corev1.ResourceList{"cpu": resource.MustParse("100m")},
 				corev1.ResourceList{"cpu": resource.MustParse("200m"), "nvidia.com/gpu": resource.MustParse("1")},
 			),
-			wantRes: mockCommonResource(
-				common.ResourceList{"cpu": "100m"},
-				common.ResourceList{"cpu": "200m", "nvidia.com/gpu": "1"},
-			),
+			wantRes: common.Resources{
+				Requests: common.ResourceList{"cpu": "100m"},
+				Limits:   common.ResourceList{"cpu": "200m", "nvidia.com/gpu": "1"},
+			},
 		},
 		"test resource transform case 6": {
 			required: mockRequiredResource(
 				corev1.ResourceList{},
 				corev1.ResourceList{"cpu": resource.MustParse("100m")},
 			),
-			wantRes: mockCommonResource(
-				common.ResourceList{},
-				common.ResourceList{"cpu": "100m"},
-			),
+			wantRes: common.Resources{
+				Requests: common.ResourceList{},
+				Limits:   common.ResourceList{"cpu": "100m"},
+			},
 		},
 	}
 
@@ -102,17 +102,6 @@ func TestTransformRequirementsToResources(t *testing.T) {
 
 func mockRequiredResource(req, limit corev1.ResourceList) corev1.ResourceRequirements {
 	res := corev1.ResourceRequirements{}
-	if len(req) > 0 {
-		res.Requests = req
-	}
-	if len(limit) > 0 {
-		res.Limits = limit
-	}
-	return res
-}
-
-func mockCommonResource(req, limit common.ResourceList) common.Resources {
-	res := common.Resources{}
 	if len(req) > 0 {
 		res.Requests = req
 	}
