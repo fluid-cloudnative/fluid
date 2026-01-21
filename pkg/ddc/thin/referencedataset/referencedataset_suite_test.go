668b65bf9eab4fd6cd8659103793f6c8f48aff54
Aditya Raut <159172287+adity1raut@users.noreply.github.com>
2026-01-21 12:31:38 +0530

    test(referencedataset): add unit tests for ConfigMap and DaemonSet operations (#5419)
    * test(referencedataset): add unit tests for ConfigMap and DaemonSet operations
    
    Signed-off-by: adity1raut <araut7798@gmail.com>
    
    * REsolve
    
    Signed-off-by: adity1raut <araut7798@gmail.com>
    
    ---------
    
    Signed-off-by: adity1raut <araut7798@gmail.com>

---
+++ pkg/ddc/thin/referencedataset/referencedataset_suite_test.go
@@ -0,0 +1,13 @@
+package referencedataset
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestReferencedataset(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Referencedataset Suite")
+}
