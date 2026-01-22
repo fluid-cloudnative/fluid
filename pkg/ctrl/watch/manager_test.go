61bb985b5bc98ef9c6733781271f5e7edb42e1b2
adity1raut <araut7798@gmail.com>
2026-01-23 00:21:04 +0530

    test(watch): add Ginkgo test suite for watch package
    Signed-off-by: adity1raut <araut7798@gmail.com>

--- pkg/ctrl/watch/manager_test.go
+++ pkg/ctrl/watch/manager_test.go
@@ -17,65 +17,239 @@ limitations under the License.
 package watch
 
 import (
-	"testing"
+	"context"
 
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/ptr"
+	ctrl "sigs.k8s.io/controller-runtime"
 
 	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
 )
 
-func TestIsObjectInManaged(t *testing.T) {
-	reconciler := &FakeRuntimeReconciler{}
+var _ = Describe("IsObjectInManaged", func() {
+	var reconciler *FakeRuntimeReconciler
 
-	_ = reconciler.ControllerName()
-	_ = reconciler.ManagedResource()
+	BeforeEach(func() {
+		reconciler = &FakeRuntimeReconciler{}
+		// Test methods are called
+		_ = reconciler.ControllerName()
+		_ = reconciler.ManagedResource()
+	})
 
-	matchedPod := corev1.Pod{ObjectMeta: metav1.ObjectMeta{
-		OwnerReferences: []metav1.OwnerReference{
-			{
+	Context("when object has controller reference", func() {
+		It("should be managed", func() {
+			pod := &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "managed-pod",
+					Namespace: "default",
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							Kind:       datav1alpha1.JindoRuntimeKind,
+							APIVersion: datav1alpha1.GroupVersion.Group + "/" + datav1alpha1.GroupVersion.Version,
+							Controller: ptr.To(true),
+						},
+					},
+				},
+			}
+			isManaged := isObjectInManaged(pod, reconciler)
+			Expect(isManaged).To(BeTrue())
+		})
+	})
+
+	Context("when object has no controller flag", func() {
+		It("should not be managed", func() {
+			pod := &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "not-managed-pod",
+					Namespace: "default",
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							Kind:       datav1alpha1.JindoRuntimeKind,
+							APIVersion: datav1alpha1.GroupVersion.Group + "/" + datav1alpha1.GroupVersion.Version,
+						},
+					},
+				},
+			}
+			isManaged := isObjectInManaged(pod, reconciler)
+			Expect(isManaged).To(BeFalse())
+		})
+	})
+
+	Context("when object has wrong kind", func() {
+		It("should not be managed", func() {
+			pod := &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "wrong-kind-pod",
+					Namespace: "default",
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							Kind:       "Test",
+							APIVersion: datav1alpha1.GroupVersion.Group + "/" + datav1alpha1.GroupVersion.Version,
+							Controller: ptr.To(true),
+						},
+					},
+				},
+			}
+			isManaged := isObjectInManaged(pod, reconciler)
+			Expect(isManaged).To(BeFalse())
+		})
+	})
+
+	Context("when object has wrong api version", func() {
+		It("should not be managed", func() {
+			pod := &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "wrong-api-pod",
+					Namespace: "default",
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							Kind:       datav1alpha1.JindoRuntimeKind,
+							APIVersion: "v1",
+							Controller: ptr.To(true),
+						},
+					},
+				},
+			}
+			isManaged := isObjectInManaged(pod, reconciler)
+			Expect(isManaged).To(BeFalse())
+		})
+	})
+
+	Context("when object has no owner references", func() {
+		It("should not be managed", func() {
+			pod := &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "no-owner-pod",
+					Namespace: "default",
+				},
+			}
+			isManaged := isObjectInManaged(pod, reconciler)
+			Expect(isManaged).To(BeFalse())
+		})
+	})
+})
+
+var _ = Describe("IsOwnerMatched", func() {
+	var reconciler *FakeRuntimeReconciler
+
+	BeforeEach(func() {
+		reconciler = &FakeRuntimeReconciler{}
+	})
+
+	Context("when owner reference is matched", func() {
+		It("should return true", func() {
+			controllerRef := &metav1.OwnerReference{
 				Kind:       datav1alpha1.JindoRuntimeKind,
 				APIVersion: datav1alpha1.GroupVersion.Group + "/" + datav1alpha1.GroupVersion.Version,
 				Controller: ptr.To(true),
-			},
-		},
-	}}
+			}
+			matched := isOwnerMatched(controllerRef, reconciler)
+			Expect(matched).To(BeTrue())
+		})
+	})
 
-	isManaged := isObjectInManaged(&matchedPod, reconciler)
+	Context("when kind is unmatched", func() {
+		It("should return false", func() {
+			controllerRef := &metav1.OwnerReference{
+				Kind:       "WrongKind",
+				APIVersion: datav1alpha1.GroupVersion.Group + "/" + datav1alpha1.GroupVersion.Version,
+				Controller: ptr.To(true),
+			}
+			matched := isOwnerMatched(controllerRef, reconciler)
+			Expect(matched).To(BeFalse())
+		})
+	})
 
-	if !isManaged {
-		t.Errorf("The object %v is not managed by %v which is not expected.", matchedPod, reconciler)
-	}
-
-	notmatchedPod := corev1.Pod{ObjectMeta: metav1.ObjectMeta{
-		OwnerReferences: []metav1.OwnerReference{
-			{
+	Context("when api version is unmatched", func() {
+		It("should return false", func() {
+			controllerRef := &metav1.OwnerReference{
 				Kind:       datav1alpha1.JindoRuntimeKind,
-				APIVersion: datav1alpha1.GroupVersion.Group + "/" + datav1alpha1.GroupVersion.Version,
-			},
-		},
-	}}
+				APIVersion: "apps/v1",
+				Controller: ptr.To(true),
+			}
+			matched := isOwnerMatched(controllerRef, reconciler)
+			Expect(matched).To(BeFalse())
+		})
+	})
+})
 
-	isManaged = isObjectInManaged(&notmatchedPod, reconciler)
+var _ = Describe("ControllerInterface", func() {
+	var reconciler *FakeRuntimeReconciler
 
-	if isManaged {
-		t.Errorf("The object %v is  managed by %v which is not expected.", matchedPod, reconciler)
-	}
+	BeforeEach(func() {
+		reconciler = &FakeRuntimeReconciler{}
+	})
 
-	notmatchedPod = corev1.Pod{ObjectMeta: metav1.ObjectMeta{
-		OwnerReferences: []metav1.OwnerReference{
-			{
-				Kind:       "Test",
-				APIVersion: datav1alpha1.GroupVersion.Group + "/" + datav1alpha1.GroupVersion.Version,
-			},
-		},
-	}}
+	It("should call controller name method", func() {
+		name := reconciler.ControllerName()
+		_ = name
+	})
 
-	isManaged = isObjectInManaged(&notmatchedPod, reconciler)
+	It("should return non-nil managed resource", func() {
+		resource := reconciler.ManagedResource()
+		Expect(resource).NotTo(BeNil())
+	})
 
-	if isManaged {
-		t.Errorf("The object %v is  managed by %v which is not expected.", matchedPod, reconciler)
-	}
+	It("should execute reconcile without error", func() {
+		ctx := context.Background()
+		req := ctrl.Request{}
+		_, _ = reconciler.Reconcile(ctx, req)
+	})
+})
 
-}
+var _ = Describe("IsObjectInManaged Edge Cases", func() {
+	var reconciler *FakeRuntimeReconciler
+
+	BeforeEach(func() {
+		reconciler = &FakeRuntimeReconciler{}
+	})
+
+	Context("when object has multiple owner references", func() {
+		It("should not be managed if matching reference doesn't have controller=true", func() {
+			podWithMultipleOwners := &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "multi-owner-pod",
+					Namespace: "default",
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							Kind:       "ReplicaSet",
+							APIVersion: "apps/v1",
+							Controller: ptr.To(true),
+						},
+						{
+							Kind:       datav1alpha1.JindoRuntimeKind,
+							APIVersion: datav1alpha1.GroupVersion.Group + "/" + datav1alpha1.GroupVersion.Version,
+						},
+					},
+				},
+			}
+			isManaged := isObjectInManaged(podWithMultipleOwners, reconciler)
+			Expect(isManaged).To(BeFalse())
+		})
+
+		It("should be managed if matching controller reference is second item", func() {
+			podWithMatchingSecond := &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "matching-second-pod",
+					Namespace: "default",
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							Kind:       "ReplicaSet",
+							APIVersion: "apps/v1",
+						},
+						{
+							Kind:       datav1alpha1.JindoRuntimeKind,
+							APIVersion: datav1alpha1.GroupVersion.Group + "/" + datav1alpha1.GroupVersion.Version,
+							Controller: ptr.To(true),
+						},
+					},
+				},
+			}
+			isManaged := isObjectInManaged(podWithMatchingSecond, reconciler)
+			Expect(isManaged).To(BeTrue())
+		})
+	})
+})
