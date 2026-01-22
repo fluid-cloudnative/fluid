a04f783774a023fc2c4d5b9b59321807c2177f3a
adity1raut <araut7798@gmail.com>
2026-01-22 19:53:00 +0530

    test(kubelet): add Ginkgo test suite for Kubelet client functionality
    Signed-off-by: adity1raut <araut7798@gmail.com>

---
+++ pkg/utils/kubelet/kubelet_suite_test.go
@@ -0,0 +1,13 @@
+package kubelet_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestKubelet(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Kubelet Suite")
+}
