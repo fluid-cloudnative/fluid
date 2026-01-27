17439ce689becd23d1f4a46f41b00ae20ec77651
Aditya Raut <159172287+adity1raut@users.noreply.github.com>
2026-01-27 09:15:54 +0530

    test: refactor JindoFileUtils tests to use Ginkgo and Gomega for better readability and structure (#5449)
    Signed-off-by: adity1raut <araut7798@gmail.com>

--- pkg/ddc/jindocache/operations/base_test.go
+++ pkg/ddc/jindocache/operations/base_test.go
@@ -16,119 +16,423 @@ package operations
 
 import (
 	"errors"
-	"reflect"
-	"testing"
 
 	"github.com/agiledragon/gomonkey/v2"
 	"github.com/fluid-cloudnative/fluid/pkg/utils/fake"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
-func TestNewJindoFileUtils(t *testing.T) {
-	expectedResult := JindoFileUtils{
-		podName:   "hadoop",
-		namespace: "default",
-		container: "hadoop",
-		log:       fake.NullLogger(),
-	}
+var _ = Describe("JindoFileUtils", func() {
+	Describe("NewJindoFileUtils", func() {
 
-	result := NewJindoFileUtils("hadoop", "default", "hadoop", fake.NullLogger())
-	if reflect.DeepEqual(result, expectedResult) {
-		t.Errorf("check failure, expected %v, get %v", expectedResult, result)
-	}
-}
+		It("should handle parameter order correctly", func() {
+			result := NewJindoFileUtils("test-pod", "test-container", "test-namespace", fake.NullLogger())
+			Expect(result.podName).To(Equal("test-pod"))
+			Expect(result.namespace).To(Equal("test-namespace"))
+			Expect(result.container).To(Equal("test-container"))
+		})
+	})
 
-func TestJindoFileUtils_exec(t *testing.T) {
-	ExecWithoutTimeoutCommon := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
-		return "Test stdout", "", nil
-	}
-	ExecWithoutTimeoutErr := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
-		return "", "", errors.New("fail to run the command")
-	}
+	Describe("exec", func() {
+		var (
+			a       *JindoFileUtils
+			patches *gomonkey.Patches
+		)
 
-	patches := gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecWithoutTimeoutErr)
-	defer patches.Reset()
+		BeforeEach(func() {
+			a = &JindoFileUtils{log: fake.NullLogger()}
+		})
 
-	a := &JindoFileUtils{log: fake.NullLogger()}
-	_, _, err := a.exec([]string{"jindo", "fs", "-report"}, false)
-	if err == nil {
-		t.Error("check failure, want err, got nil")
-	}
+		AfterEach(func() {
+			if patches != nil {
+				patches.Reset()
+			}
+		})
 
-	patches.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecWithoutTimeoutCommon)
-	_, _, err = a.exec([]string{"jindo", "fs", "-report"}, true)
-	if err != nil {
-		t.Errorf("check failure, want nil, got err: %v", err)
-	}
-}
+		Context("when exec fails", func() {
+			It("should return an error", func() {
+				ExecWithoutTimeoutErr := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "", "", errors.New("fail to run the command")
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecWithoutTimeoutErr)
 
-func TestJindoFileUtils_ReportSummary(t *testing.T) {
-	ExecCommon := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
-		return "Test stdout", "", nil
-	}
-	ExecErr := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
-		return "", "", errors.New("fail to run the command")
-	}
+				_, _, err := a.exec([]string{"jindo", "fs", "-report"}, false)
+				Expect(err).To(HaveOccurred())
+			})
+		})
 
-	patches := gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecErr)
-	defer patches.Reset()
+		Context("when exec succeeds", func() {
+			It("should return stdout without error", func() {
+				ExecWithoutTimeoutCommon := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "Test stdout", "", nil
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecWithoutTimeoutCommon)
 
-	a := JindoFileUtils{}
-	_, err := a.ReportSummary()
-	if err == nil {
-		t.Error("check failure, want err, got nil")
-	}
+				stdout, _, err := a.exec([]string{"jindo", "fs", "-report"}, true)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(stdout).To(Equal("Test stdout"))
+			})
+		})
+	})
 
-	patches.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecCommon)
-	_, err = a.ReportSummary()
-	if err != nil {
-		t.Errorf("check failure, want nil, got err: %v", err)
-	}
-}
+	Describe("ReportSummary", func() {
+		var (
+			a       JindoFileUtils
+			patches *gomonkey.Patches
+		)
 
-func TestJindoFileUtils_GetUfsTotalSize(t *testing.T) {
-	ExecWithoutTimeoutCommon := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
-		return "2      1    108 testUrl", "", nil
-	}
-	ExecWithoutTimeoutErr := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
-		return "", "", errors.New("fail to run the command")
-	}
+		AfterEach(func() {
+			if patches != nil {
+				patches.Reset()
+			}
+		})
 
-	patches := gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecWithoutTimeoutErr)
-	defer patches.Reset()
+		Context("when exec fails", func() {
+			It("should return an error", func() {
+				ExecErr := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "", "", errors.New("fail to run the command")
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecErr)
 
-	a := &JindoFileUtils{log: fake.NullLogger()}
-	_, err := a.GetUfsTotalSize("/tmpDictionary")
-	if err == nil {
-		t.Error("check failure, want err, got nil")
-	}
+				_, err := a.ReportSummary()
+				Expect(err).To(HaveOccurred())
+			})
+		})
 
-	patches.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecWithoutTimeoutCommon)
-	_, err = a.GetUfsTotalSize("/tmpDictionary")
-	if err != nil {
-		t.Errorf("check failure, want nil, got err: %v", err)
-	}
-}
+		Context("when exec succeeds", func() {
+			It("should return result without error", func() {
+				ExecCommon := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "Test stdout", "", nil
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecCommon)
 
-func TestJindoFileUtils_Ready(t *testing.T) {
-	ExecCommon := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
-		return "Test stdout ", "", nil
-	}
-	ExecErr := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
-		return "", "", errors.New("fail to run the command")
-	}
+				_, err := a.ReportSummary()
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+	})
 
-	patches := gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecErr)
-	defer patches.Reset()
+	Describe("GetUfsTotalSize", func() {
+		var (
+			a       *JindoFileUtils
+			patches *gomonkey.Patches
+		)
 
-	a := &JindoFileUtils{log: fake.NullLogger()}
-	ready := a.Ready()
-	if ready != false {
-		t.Errorf("check failure, want false, got %t", ready)
-	}
+		BeforeEach(func() {
+			a = &JindoFileUtils{log: fake.NullLogger()}
+		})
 
-	patches.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecCommon)
-	ready = a.Ready()
-	if ready != true {
-		t.Errorf("check failure, want true, got %t", ready)
-	}
-}
+		AfterEach(func() {
+			if patches != nil {
+				patches.Reset()
+			}
+		})
+
+		Context("when exec fails", func() {
+			It("should return an error", func() {
+				ExecWithoutTimeoutErr := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "", "", errors.New("fail to run the command")
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecWithoutTimeoutErr)
+
+				_, err := a.GetUfsTotalSize("/tmpDictionary")
+				Expect(err).To(HaveOccurred())
+			})
+		})
+
+		Context("when exec succeeds", func() {
+			It("should return size without error", func() {
+				ExecWithoutTimeoutCommon := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "2      1    108 testUrl", "", nil
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecWithoutTimeoutCommon)
+
+				_, err := a.GetUfsTotalSize("/tmpDictionary")
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+
+		Context("when output is invalid", func() {
+			It("should return a parse error", func() {
+				ExecInvalidOutput := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "field1 field2", "", nil
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecInvalidOutput)
+
+				_, err := a.GetUfsTotalSize("/tmpDictionary")
+				Expect(err).To(HaveOccurred())
+			})
+		})
+	})
+
+	Describe("Ready", func() {
+		var (
+			a       *JindoFileUtils
+			patches *gomonkey.Patches
+		)
+
+		BeforeEach(func() {
+			a = &JindoFileUtils{log: fake.NullLogger()}
+		})
+
+		AfterEach(func() {
+			if patches != nil {
+				patches.Reset()
+			}
+		})
+
+		Context("when exec fails", func() {
+			It("should return false", func() {
+				ExecErr := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "", "", errors.New("fail to run the command")
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecErr)
+
+				ready := a.Ready()
+				Expect(ready).To(BeFalse())
+			})
+		})
+
+		Context("when exec succeeds", func() {
+			It("should return true", func() {
+				ExecCommon := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "Test stdout ", "", nil
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecCommon)
+
+				ready := a.Ready()
+				Expect(ready).To(BeTrue())
+			})
+		})
+	})
+
+	Describe("IsMounted", func() {
+		var (
+			a       *JindoFileUtils
+			patches *gomonkey.Patches
+		)
+
+		BeforeEach(func() {
+			a = &JindoFileUtils{log: fake.NullLogger()}
+		})
+
+		AfterEach(func() {
+			if patches != nil {
+				patches.Reset()
+			}
+		})
+
+		Context("when exec fails", func() {
+			It("should return an error", func() {
+				ExecErr := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "", "", errors.New("fail to run the command")
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecErr)
+
+				_, err := a.IsMounted("/test/mount")
+				Expect(err).To(HaveOccurred())
+			})
+		})
+
+		Context("when mount point exists", func() {
+			It("should return true", func() {
+				ExecCommon := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "field1 field2 /test/mount field3\nfield1 field2 /other/mount field3", "", nil
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecCommon)
+
+				mounted, err := a.IsMounted("/test/mount")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(mounted).To(BeTrue())
+			})
+		})
+
+		Context("when mount point does not exist", func() {
+			It("should return false", func() {
+				ExecNotMounted := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "field1 field2 /other/mount field3", "", nil
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecNotMounted)
+
+				mounted, err := a.IsMounted("/test/mount")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(mounted).To(BeFalse())
+			})
+		})
+
+		Context("when output has short fields", func() {
+			It("should return false", func() {
+				ExecShortFields := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "field1 field2\nfield1", "", nil
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecShortFields)
+
+				mounted, err := a.IsMounted("/test/mount")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(mounted).To(BeFalse())
+			})
+		})
+	})
+
+	Describe("Mount", func() {
+		var (
+			a       *JindoFileUtils
+			patches *gomonkey.Patches
+		)
+
+		BeforeEach(func() {
+			a = &JindoFileUtils{log: fake.NullLogger()}
+		})
+
+		AfterEach(func() {
+			if patches != nil {
+				patches.Reset()
+			}
+		})
+
+		Context("when exec fails", func() {
+			It("should return an error", func() {
+				ExecErr := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "", "", errors.New("fail to run the command")
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecErr)
+
+				err := a.Mount("/test/mount", "oss://bucket/path")
+				Expect(err).To(HaveOccurred())
+			})
+		})
+
+		Context("when mount point already exists", func() {
+			It("should ignore the error and return nil", func() {
+				ExecAlreadyExists := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "Mount point already exists", "", errors.New("mount point exists")
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecAlreadyExists)
+
+				err := a.Mount("/test/mount", "oss://bucket/path")
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+
+		Context("when mount succeeds", func() {
+			It("should return nil", func() {
+				ExecCommon := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "mount success", "", nil
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecCommon)
+
+				err := a.Mount("/test/mount", "oss://bucket/path")
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+
+		Context("when exec returns other error", func() {
+			It("should return an error", func() {
+				ExecOtherErr := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "Some other error", "", errors.New("different error")
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecOtherErr)
+
+				err := a.Mount("/test/mount", "oss://bucket/path")
+				Expect(err).To(HaveOccurred())
+			})
+		})
+	})
+
+	Describe("IsRefreshed", func() {
+		var (
+			a       *JindoFileUtils
+			patches *gomonkey.Patches
+		)
+
+		BeforeEach(func() {
+			a = &JindoFileUtils{log: fake.NullLogger()}
+		})
+
+		AfterEach(func() {
+			if patches != nil {
+				patches.Reset()
+			}
+		})
+
+		Context("when exec fails", func() {
+			It("should return an error", func() {
+				ExecErr := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "", "", errors.New("fail to run the command")
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecErr)
+
+				_, err := a.IsRefreshed()
+				Expect(err).To(HaveOccurred())
+			})
+		})
+
+		Context("when cache strategy is found", func() {
+			It("should return true", func() {
+				ExecCommon := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "line1\ncacheStrategy: some strategy\nline3", "", nil
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecCommon)
+
+				refreshed, err := a.IsRefreshed()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(refreshed).To(BeTrue())
+			})
+		})
+
+		Context("when cache strategy is not found", func() {
+			It("should return false", func() {
+				ExecNotRefreshed := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "line1\nline2\nline3", "", nil
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecNotRefreshed)
+
+				refreshed, err := a.IsRefreshed()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(refreshed).To(BeFalse())
+			})
+		})
+	})
+
+	Describe("RefreshCacheSet", func() {
+		var (
+			a       *JindoFileUtils
+			patches *gomonkey.Patches
+		)
+
+		BeforeEach(func() {
+			a = &JindoFileUtils{log: fake.NullLogger()}
+		})
+
+		AfterEach(func() {
+			if patches != nil {
+				patches.Reset()
+			}
+		})
+
+		Context("when exec fails", func() {
+			It("should return an error", func() {
+				ExecErr := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "", "", errors.New("fail to run the command")
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecErr)
+
+				err := a.RefreshCacheSet()
+				Expect(err).To(HaveOccurred())
+			})
+		})
+
+		Context("when exec succeeds", func() {
+			It("should return nil", func() {
+				ExecCommon := func(a JindoFileUtils, command []string, verbose bool) (stdout string, stderr string, err error) {
+					return "refresh cache set success", "", nil
+				}
+				patches = gomonkey.ApplyPrivateMethod(JindoFileUtils{}, "exec", ExecCommon)
+
+				err := a.RefreshCacheSet()
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+	})
+})
