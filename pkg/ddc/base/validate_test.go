ea1fd65ba44a389fe4b3efed7763f8bd234454b0
harsh kumar <135993950+hxrshxz@users.noreply.github.com>
2026-01-27 08:53:45 +0530

    add unit tests for validate.go (#5439)
    * test(pkg/ddc/base): add unit tests for validate.go
    
    Signed-off-by: Harsh <harshmastic@gmail.com>
    
    * test(pkg/ddc/base): add unit tests for validate.go
    
    Signed-off-by: Harsh <harshmastic@gmail.com>
    
    * chore: trigger CI
    
    Signed-off-by: Harsh <harshmastic@gmail.com>
    
    * refactor: convert ValidateRuntimeInfo tests to DescribeTable
    
    Signed-off-by: Harsh <harshmastic@gmail.com>
    
    ---------
    
    Signed-off-by: Harsh <harshmastic@gmail.com>

---
+++ pkg/ddc/base/validate_test.go
@@ -0,0 +1,132 @@
+/*
+Copyright 2024 The Fluid Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package base
+
+import (
+	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
+	"github.com/fluid-cloudnative/fluid/pkg/common"
+	fluiderrs "github.com/fluid-cloudnative/fluid/pkg/errors"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var _ = Describe("ValidateRuntimeInfo", func() {
+	var (
+		runtimeInfo *mockRuntimeInfoForValidate
+	)
+
+	BeforeEach(func() {
+		runtimeInfo = &mockRuntimeInfoForValidate{}
+	})
+
+	DescribeTable("validating runtime info",
+		func(ownerUID string, placementSet bool, expectError bool, expectTemporary bool, errorSubstring string) {
+			runtimeInfo.ownerDatasetUID = ownerUID
+			runtimeInfo.placementModeSet = placementSet
+
+			err := ValidateRuntimeInfo(runtimeInfo)
+
+			if expectError {
+				Expect(err).To(HaveOccurred())
+				if expectTemporary {
+					Expect(fluiderrs.IsTemporaryValidationFailed(err)).To(BeTrue())
+				}
+				if errorSubstring != "" {
+					Expect(err.Error()).To(ContainSubstring(errorSubstring))
+				}
+			} else {
+				Expect(err).NotTo(HaveOccurred())
+			}
+		},
+		Entry("all info is valid",
+			"uid-12345", true, false, false, "",
+		),
+		Entry("OwnerDatasetUID is empty",
+			"", true, true, true, "",
+		),
+		Entry("placement mode is not set",
+			"uid-12345", false, true, true, "",
+		),
+		Entry("both OwnerDatasetUID and placement mode invalid - reports UID first",
+			"", false, true, true, "OwnerDatasetUID is not set",
+		),
+	)
+})
+
+// mockRuntimeInfoForValidate implements RuntimeInfoInterface for testing ValidateRuntimeInfo
+type mockRuntimeInfoForValidate struct {
+	ownerDatasetUID  string
+	placementModeSet bool
+}
+
+// Methods used by ValidateRuntimeInfo
+func (m *mockRuntimeInfoForValidate) GetOwnerDatasetUID() string { return m.ownerDatasetUID }
+func (m *mockRuntimeInfoForValidate) IsPlacementModeSet() bool   { return m.placementModeSet }
+
+// Conventions interface methods (stub implementations)
+func (m *mockRuntimeInfoForValidate) GetPersistentVolumeName() string  { return "" }
+func (m *mockRuntimeInfoForValidate) GetLabelNameForMemory() string    { return "" }
+func (m *mockRuntimeInfoForValidate) GetLabelNameForDisk() string      { return "" }
+func (m *mockRuntimeInfoForValidate) GetLabelNameForTotal() string     { return "" }
+func (m *mockRuntimeInfoForValidate) GetCommonLabelName() string       { return "" }
+func (m *mockRuntimeInfoForValidate) GetFuseLabelName() string         { return "" }
+func (m *mockRuntimeInfoForValidate) GetRuntimeLabelName() string      { return "" }
+func (m *mockRuntimeInfoForValidate) GetDatasetNumLabelName() string   { return "" }
+func (m *mockRuntimeInfoForValidate) GetWorkerStatefulsetName() string { return "" }
+func (m *mockRuntimeInfoForValidate) GetExclusiveLabelValue() string   { return "" }
+
+// RuntimeInfoInterface methods (stub implementations)
+func (m *mockRuntimeInfoForValidate) GetTieredStoreInfo() TieredStoreInfo { return TieredStoreInfo{} }
+func (m *mockRuntimeInfoForValidate) GetName() string                     { return "" }
+func (m *mockRuntimeInfoForValidate) GetNamespace() string                { return "" }
+func (m *mockRuntimeInfoForValidate) GetRuntimeType() string              { return "" }
+func (m *mockRuntimeInfoForValidate) GetPlacementModeWithDefault(defaultMode datav1alpha1.PlacementMode) datav1alpha1.PlacementMode {
+	return defaultMode
+}
+func (m *mockRuntimeInfoForValidate) SetFuseNodeSelector(nodeSelector map[string]string) {
+	// No-op for test mock
+}
+func (m *mockRuntimeInfoForValidate) SetFuseName(fuseName string) {
+	// No-op for test mock
+}
+func (m *mockRuntimeInfoForValidate) SetupFuseCleanPolicy(policy datav1alpha1.FuseCleanPolicy) {
+	// No-op for test mock
+}
+func (m *mockRuntimeInfoForValidate) SetupWithDataset(dataset *datav1alpha1.Dataset) {
+	// No-op for test mock
+}
+func (m *mockRuntimeInfoForValidate) SetOwnerDatasetUID(alias types.UID) {
+	// No-op for test mock
+}
+func (m *mockRuntimeInfoForValidate) GetFuseNodeSelector() map[string]string { return nil }
+func (m *mockRuntimeInfoForValidate) GetFuseName() string                    { return "" }
+func (m *mockRuntimeInfoForValidate) GetFuseCleanPolicy() datav1alpha1.FuseCleanPolicy {
+	return datav1alpha1.OnDemandCleanPolicy
+}
+func (m *mockRuntimeInfoForValidate) GetFuseContainerTemplate() (*common.FuseInjectionTemplate, error) {
+	return nil, nil
+}
+func (m *mockRuntimeInfoForValidate) SetAPIReader(apiReader client.Reader) {
+	// No-op for test mock
+}
+func (m *mockRuntimeInfoForValidate) GetMetadataList() []datav1alpha1.Metadata { return nil }
+func (m *mockRuntimeInfoForValidate) GetAnnotations() map[string]string        { return nil }
+func (m *mockRuntimeInfoForValidate) GetFuseMetricsScrapeTarget() mountModeSelector {
+	return mountModeSelector{}
+}
