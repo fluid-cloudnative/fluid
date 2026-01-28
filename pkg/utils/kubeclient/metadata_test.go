64ed896be408ca18f1cc6ebe1976b874b1b869f0
adity1raut <araut7798@gmail.com>
2026-01-28 20:08:49 +0530

    test(utils): migrate tests to ginkgo of pkg/utils/kubeclient/metadata_test.go
    Signed-off-by: adity1raut <araut7798@gmail.com>

--- pkg/utils/kubeclient/metadata_test.go
+++ pkg/utils/kubeclient/metadata_test.go
@@ -17,131 +17,142 @@ limitations under the License.
 package kubeclient
 
 import (
-	"testing"
-
 	appsv1 "k8s.io/api/apps/v1"
 
 	"github.com/fluid-cloudnative/fluid/pkg/utils/fake"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-
 	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func TestCompareOwnerRefMatcheWithExpected(t *testing.T) {
-	type fields struct {
-		controller *appsv1.StatefulSet
-		child      runtime.Object
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   bool
-	}{
-		{name: "NoController",
-			fields: fields{
-				controller: &appsv1.StatefulSet{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "test1",
-						Namespace: "big-data",
-					},
-					Spec: appsv1.StatefulSetSpec{},
-				},
-				child: &v1.Pod{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "test1-0",
-						Namespace: "big-data",
-					},
-					Spec: v1.PodSpec{},
-				},
-			},
-		}, {name: "the_controller_uid_is_not_matched",
-			fields: fields{
-				controller: &appsv1.StatefulSet{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "test2",
-						Namespace: "big-data",
-						UID:       "uid",
-					},
-					Spec: appsv1.StatefulSetSpec{},
-				},
-				child: &v1.Pod{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "test2-0",
-						Namespace: "big-data",
-						OwnerReferences: []metav1.OwnerReference{{
-							Kind:       "StatefulSet",
-							APIVersion: "app/v1",
-							UID:        "uid1",
-							Controller: ptr.To(true),
-						}},
-					},
-					Spec: v1.PodSpec{},
-				},
-			},
-			want: false,
-		},
-		{name: "ControllerEqual",
-			fields: fields{
-				controller: &appsv1.StatefulSet{
-					TypeMeta: metav1.TypeMeta{
-						Kind:       "StatefulSet",
-						APIVersion: "app/v1",
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "test2",
-						Namespace: "big-data",
-						UID:       "uid2",
-					},
-					Spec: appsv1.StatefulSetSpec{},
-				},
-				child: &v1.Pod{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "test2-0",
-						Namespace: "big-data",
-						OwnerReferences: []metav1.OwnerReference{{
-							Kind:       "StatefulSet",
-							APIVersion: "app/v1",
-							UID:        "uid2",
-							Name:       "test2",
-							Controller: ptr.To(true),
-						}},
-					},
-					Spec: v1.PodSpec{},
-				},
-			},
-			want: true,
-		},
-	}
+var _ = Describe("CompareOwnerRefMatcheWithExpected", func() {
+	var (
+		controller    *appsv1.StatefulSet
+		child         runtime.Object
+		mockClient    client.Client
+		scheme        *runtime.Scheme
+		runtimeObjs   []runtime.Object
+		metaObj       metav1.Object
+		controllerRef *metav1.OwnerReference
+	)
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := runtime.NewScheme()
-			s.AddKnownTypes(appsv1.SchemeGroupVersion, tt.fields.controller)
-			_ = v1.AddToScheme(s)
-			runtimeObjs := []runtime.Object{}
-			runtimeObjs = append(runtimeObjs, tt.fields.controller)
-			runtimeObjs = append(runtimeObjs, tt.fields.child)
-			mockClient := fake.NewFakeClientWithScheme(s, runtimeObjs...)
+	BeforeEach(func() {
+		scheme = runtime.NewScheme()
+		runtimeObjs = []runtime.Object{}
+	})
 
-			metaObj, err := meta.Accessor(tt.fields.child)
-			if err != nil {
-				t.Errorf(" meta.Accessor = %v", err)
+	JustBeforeEach(func() {
+		scheme.AddKnownTypes(appsv1.SchemeGroupVersion, controller)
+		err := v1.AddToScheme(scheme)
+		Expect(err).NotTo(HaveOccurred())
+
+		runtimeObjs = append(runtimeObjs, controller)
+		runtimeObjs = append(runtimeObjs, child)
+		mockClient = fake.NewFakeClientWithScheme(scheme, runtimeObjs...)
+
+		var err2 error
+		metaObj, err2 = meta.Accessor(child)
+		Expect(err2).NotTo(HaveOccurred())
+
+		controllerRef = metav1.GetControllerOf(metaObj)
+	})
+
+	Context("when there is no controller reference", func() {
+		BeforeEach(func() {
+			controller = &appsv1.StatefulSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test1",
+					Namespace: "big-data",
+				},
+				Spec: appsv1.StatefulSetSpec{},
 			}
-			controllerRef := metav1.GetControllerOf(metaObj)
-			want, err := compareOwnerRefMatcheWithExpected(mockClient, controllerRef, metaObj.GetNamespace(), tt.fields.controller)
-			if err != nil {
-				t.Errorf("compareOwnerRefMatcheWithExpected = %v", err)
-			}
-
-			if want != tt.want {
-				t.Errorf("test case %s compareOwnerRefMatcheWithExpected() = %v, want %v",
-					tt.name,
-					want,
-					tt.want)
+			child = &v1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test1-0",
+					Namespace: "big-data",
+				},
+				Spec: v1.PodSpec{},
 			}
 		})
-	}
-}
+
+		It("should return false without error", func() {
+			result, err := compareOwnerRefMatcheWithExpected(mockClient, controllerRef, metaObj.GetNamespace(), controller)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(result).To(BeFalse())
+		})
+	})
+
+	Context("when the controller UID does not match", func() {
+		BeforeEach(func() {
+			controller = &appsv1.StatefulSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test2",
+					Namespace: "big-data",
+					UID:       "uid",
+				},
+				Spec: appsv1.StatefulSetSpec{},
+			}
+			child = &v1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test2-0",
+					Namespace: "big-data",
+					OwnerReferences: []metav1.OwnerReference{{
+						Kind:       "StatefulSet",
+						APIVersion: "app/v1",
+						UID:        "uid1",
+						Controller: ptr.To(true),
+					}},
+				},
+				Spec: v1.PodSpec{},
+			}
+		})
+
+		It("should return false without error", func() {
+			result, err := compareOwnerRefMatcheWithExpected(mockClient, controllerRef, metaObj.GetNamespace(), controller)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(result).To(BeFalse())
+		})
+	})
+
+	Context("when the controller matches", func() {
+		BeforeEach(func() {
+			controller = &appsv1.StatefulSet{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "StatefulSet",
+					APIVersion: "app/v1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test2",
+					Namespace: "big-data",
+					UID:       "uid2",
+				},
+				Spec: appsv1.StatefulSetSpec{},
+			}
+			child = &v1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test2-0",
+					Namespace: "big-data",
+					OwnerReferences: []metav1.OwnerReference{{
+						Kind:       "StatefulSet",
+						APIVersion: "app/v1",
+						UID:        "uid2",
+						Name:       "test2",
+						Controller: ptr.To(true),
+					}},
+				},
+				Spec: v1.PodSpec{},
+			}
+		})
+
+		It("should return true without error", func() {
+			result, err := compareOwnerRefMatcheWithExpected(mockClient, controllerRef, metaObj.GetNamespace(), controller)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(result).To(BeTrue())
+		})
+	})
+})
