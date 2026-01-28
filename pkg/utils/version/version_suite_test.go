b816ee7cc7b31e0b09abdd788dc4048c0eef1d86
adity1raut <araut7798@gmail.com>
2026-01-28 20:44:33 +0530

    test(version): Migrate The /utils/version/version_test.go
    Signed-off-by: adity1raut <araut7798@gmail.com>

---
+++ pkg/utils/version/version_suite_test.go
@@ -0,0 +1,13 @@
+package version
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestVersion(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Version Suite")
+}
