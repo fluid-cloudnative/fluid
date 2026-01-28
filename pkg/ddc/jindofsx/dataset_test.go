4f88bbcecc37049744b10594082fb143fe59f687
Harsh <harshmastic@gmail.com>
2026-01-29 01:43:26 +0530

    test(jindofsx): migrate dataset_test.go to Ginkgo/Gomega
    Signed-off-by: Harsh <harshmastic@gmail.com>

--- pkg/ddc/jindofsx/dataset_test.go
+++ pkg/ddc/jindofsx/dataset_test.go
@@ -19,235 +19,298 @@ package jindofsx
 import (
 	"context"
 	"reflect"
-	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
 	"github.com/fluid-cloudnative/fluid/pkg/common"
 	"github.com/fluid-cloudnative/fluid/pkg/utils/fake"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 )
 
-func TestUpdateCacheOfDataset(t *testing.T) {
-	testDatasetInputs := []*datav1alpha1.Dataset{
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hbase",
-				Namespace: "fluid",
-			},
-			Spec: datav1alpha1.DatasetSpec{},
-		},
-	}
-	testObjs := []runtime.Object{}
-	for _, datasetInput := range testDatasetInputs {
-		testObjs = append(testObjs, datasetInput.DeepCopy())
-	}
-
-	testRuntimeInputs := []*datav1alpha1.JindoRuntime{
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hbase",
-				Namespace: "fluid",
-			},
-			Spec: datav1alpha1.JindoRuntimeSpec{
-				Master: datav1alpha1.JindoCompTemplateSpec{
-					Replicas: 1,
+var _ = Describe("JindoFSxEngine Dataset Operations", func() {
+	Describe("UpdateCacheOfDataset", func() {
+		It("should update cache of dataset successfully", func() {
+			testDatasetInputs := []*datav1alpha1.Dataset{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "hbase",
+						Namespace: "fluid",
+					},
+					Spec: datav1alpha1.DatasetSpec{},
 				},
-			},
-			Status: datav1alpha1.RuntimeStatus{
-				CacheStates: map[common.CacheStateName]string{
+			}
+			testObjs := []runtime.Object{}
+			for _, datasetInput := range testDatasetInputs {
+				testObjs = append(testObjs, datasetInput.DeepCopy())
+			}
+
+			testRuntimeInputs := []*datav1alpha1.JindoRuntime{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "hbase",
+						Namespace: "fluid",
+					},
+					Spec: datav1alpha1.JindoRuntimeSpec{
+						Master: datav1alpha1.JindoCompTemplateSpec{
+							Replicas: 1,
+						},
+					},
+					Status: datav1alpha1.RuntimeStatus{
+						CacheStates: map[common.CacheStateName]string{
+							common.Cached: "true",
+						},
+					},
+				},
+			}
+			for _, runtimeInput := range testRuntimeInputs {
+				testObjs = append(testObjs, runtimeInput.DeepCopy())
+			}
+			client := fake.NewFakeClientWithScheme(testScheme, testObjs...)
+
+			engine := &JindoFSxEngine{
+				Client:    client,
+				Log:       fake.NullLogger(),
+				name:      "hbase",
+				namespace: "fluid",
+				runtime:   testRuntimeInputs[0],
+			}
+
+			err := engine.UpdateCacheOfDataset()
+			Expect(err).NotTo(HaveOccurred())
+
+			expectedDataset := datav1alpha1.Dataset{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "hbase",
+					Namespace: "fluid",
+				},
+				Status: datav1alpha1.DatasetStatus{
+					CacheStates: map[common.CacheStateName]string{
+						common.Cached: "true",
+					},
+					HCFSStatus: &datav1alpha1.HCFSStatus{
+						Endpoint:                    "",
+						UnderlayerFileSystemVersion: "",
+					},
+				},
+			}
+
+			var datasets datav1alpha1.DatasetList
+			err = client.List(context.TODO(), &datasets)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(reflect.DeepEqual(datasets.Items[0].Status, expectedDataset.Status)).To(BeTrue())
+		})
+	})
+
+	Describe("UpdateCacheOfDatasetWithoutMaster", func() {
+		It("should update cache of dataset without master successfully", func() {
+			testDatasetInputs := []*datav1alpha1.Dataset{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "hbase",
+						Namespace: "fluid",
+					},
+					Spec: datav1alpha1.DatasetSpec{},
+				},
+			}
+			testObjs := []runtime.Object{}
+			for _, datasetInput := range testDatasetInputs {
+				testObjs = append(testObjs, datasetInput.DeepCopy())
+			}
+
+			testRuntimeInputs := []*datav1alpha1.JindoRuntime{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "hbase",
+						Namespace: "fluid",
+					},
+					Spec: datav1alpha1.JindoRuntimeSpec{
+						Master: datav1alpha1.JindoCompTemplateSpec{
+							Disabled: true,
+							Replicas: 1,
+						},
+					},
+					Status: datav1alpha1.RuntimeStatus{
+						CacheStates: map[common.CacheStateName]string{
+							common.Cached: "true",
+						},
+					},
+				},
+			}
+			for _, runtimeInput := range testRuntimeInputs {
+				testObjs = append(testObjs, runtimeInput.DeepCopy())
+			}
+			client := fake.NewFakeClientWithScheme(testScheme, testObjs...)
+
+			engine := &JindoFSxEngine{
+				Client:    client,
+				Log:       fake.NullLogger(),
+				name:      "hbase",
+				namespace: "fluid",
+				runtime:   testRuntimeInputs[0],
+			}
+
+			err := engine.UpdateCacheOfDataset()
+			Expect(err).NotTo(HaveOccurred())
+
+			expectedDataset := datav1alpha1.Dataset{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "hbase",
+					Namespace: "fluid",
+				},
+				Status: datav1alpha1.DatasetStatus{
+					CacheStates: map[common.CacheStateName]string{
+						common.Cached: "true",
+					},
+					HCFSStatus: &datav1alpha1.HCFSStatus{
+						Endpoint:                    "N/A",
+						UnderlayerFileSystemVersion: "",
+					},
+				},
+			}
+
+			var datasets datav1alpha1.DatasetList
+			err = client.List(context.TODO(), &datasets)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(reflect.DeepEqual(datasets.Items[0].Status, expectedDataset.Status)).To(BeTrue())
+		})
+	})
+
+	Describe("UpdateDatasetStatus", func() {
+		var (
+			fakeClient client.Client
+			engine     *JindoFSxEngine
+		)
+
+		BeforeEach(func() {
+			testDatasetInputs := []*datav1alpha1.Dataset{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "hbase",
+						Namespace: "fluid",
+					},
+					Spec: datav1alpha1.DatasetSpec{},
+					Status: datav1alpha1.DatasetStatus{
+						HCFSStatus: &datav1alpha1.HCFSStatus{
+							Endpoint:                    "test Endpoint",
+							UnderlayerFileSystemVersion: "Underlayer HCFS Compatible Version",
+						},
+					},
+				},
+			}
+			testObjs := []runtime.Object{}
+			for _, datasetInput := range testDatasetInputs {
+				testObjs = append(testObjs, datasetInput.DeepCopy())
+			}
+
+			testRuntimeInputs := []*datav1alpha1.JindoRuntime{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "hbase",
+						Namespace: "fluid",
+					},
+					Spec: datav1alpha1.JindoRuntimeSpec{
+						Master: datav1alpha1.JindoCompTemplateSpec{
+							Replicas: 1,
+						},
+					},
+					Status: datav1alpha1.RuntimeStatus{
+						CacheStates: map[common.CacheStateName]string{
+							common.Cached: "true",
+						},
+					},
+				},
+			}
+			for _, runtimeInput := range testRuntimeInputs {
+				testObjs = append(testObjs, runtimeInput.DeepCopy())
+			}
+			fakeClient = fake.NewFakeClientWithScheme(testScheme, testObjs...)
+
+			engine = &JindoFSxEngine{
+				Client:    fakeClient,
+				Log:       fake.NullLogger(),
+				name:      "hbase",
+				namespace: "fluid",
+				runtime:   testRuntimeInputs[0],
+			}
+		})
+
+		DescribeTable("should update dataset status with different phases",
+			func(phase datav1alpha1.DatasetPhase, expectedPhase datav1alpha1.DatasetPhase) {
+				err := engine.UpdateDatasetStatus(phase)
+				Expect(err).NotTo(HaveOccurred())
+
+				var datasets datav1alpha1.DatasetList
+				err = fakeClient.List(context.TODO(), &datasets)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(datasets.Items[0].Status.Phase).To(Equal(expectedPhase))
+				Expect(datasets.Items[0].Status.CacheStates).To(Equal(map[common.CacheStateName]string{
 					common.Cached: "true",
-				},
-			},
-		},
-	}
-	for _, runtimeInput := range testRuntimeInputs {
-		testObjs = append(testObjs, runtimeInput.DeepCopy())
-	}
-	client := fake.NewFakeClientWithScheme(testScheme, testObjs...)
-
-	engine := &JindoFSxEngine{
-		Client:    client,
-		Log:       fake.NullLogger(),
-		name:      "hbase",
-		namespace: "fluid",
-		runtime:   testRuntimeInputs[0],
-	}
-
-	err := engine.UpdateCacheOfDataset()
-	if err != nil {
-		t.Errorf("fail to exec UpdateCacheOfDataset with error %v", err)
-		return
-	}
-
-	expectedDataset := datav1alpha1.Dataset{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "hbase",
-			Namespace: "fluid",
-		},
-		Status: datav1alpha1.DatasetStatus{
-			CacheStates: map[common.CacheStateName]string{
-				common.Cached: "true",
-			},
-			HCFSStatus: &datav1alpha1.HCFSStatus{
-				Endpoint:                    "",
-				UnderlayerFileSystemVersion: "",
-			},
-		},
-	}
-
-	var datasets datav1alpha1.DatasetList
-	err = client.List(context.TODO(), &datasets)
-	if err != nil {
-		t.Errorf("fail to list the datasets with error %v", err)
-		return
-	}
-	if !reflect.DeepEqual(datasets.Items[0].Status, expectedDataset.Status) {
-		t.Errorf("fail to exec the function with error %v", err)
-		return
-	}
-}
-
-func TestUpdateCacheOfDatasetWithoutMaster(t *testing.T) {
-	testDatasetInputs := []*datav1alpha1.Dataset{
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hbase",
-				Namespace: "fluid",
-			},
-			Spec: datav1alpha1.DatasetSpec{},
-		},
-	}
-	testObjs := []runtime.Object{}
-	for _, datasetInput := range testDatasetInputs {
-		testObjs = append(testObjs, datasetInput.DeepCopy())
-	}
-
-	testRuntimeInputs := []*datav1alpha1.JindoRuntime{
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hbase",
-				Namespace: "fluid",
-			},
-			Spec: datav1alpha1.JindoRuntimeSpec{
-				Master: datav1alpha1.JindoCompTemplateSpec{
-					Disabled: true,
-					Replicas: 1,
-				},
-			},
-			Status: datav1alpha1.RuntimeStatus{
-				CacheStates: map[common.CacheStateName]string{
-					common.Cached: "true",
-				},
-			},
-		},
-	}
-	for _, runtimeInput := range testRuntimeInputs {
-		testObjs = append(testObjs, runtimeInput.DeepCopy())
-	}
-	client := fake.NewFakeClientWithScheme(testScheme, testObjs...)
-
-	engine := &JindoFSxEngine{
-		Client:    client,
-		Log:       fake.NullLogger(),
-		name:      "hbase",
-		namespace: "fluid",
-		runtime:   testRuntimeInputs[0],
-	}
-
-	err := engine.UpdateCacheOfDataset()
-	if err != nil {
-		t.Errorf("fail to exec UpdateCacheOfDataset with error %v", err)
-		return
-	}
-
-	expectedDataset := datav1alpha1.Dataset{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "hbase",
-			Namespace: "fluid",
-		},
-		Status: datav1alpha1.DatasetStatus{
-			CacheStates: map[common.CacheStateName]string{
-				common.Cached: "true",
-			},
-			HCFSStatus: &datav1alpha1.HCFSStatus{
-				Endpoint:                    "N/A",
-				UnderlayerFileSystemVersion: "",
-			},
-		},
-	}
-
-	var datasets datav1alpha1.DatasetList
-	err = client.List(context.TODO(), &datasets)
-	if err != nil {
-		t.Errorf("fail to list the datasets with error %v", err)
-		return
-	}
-	if !reflect.DeepEqual(datasets.Items[0].Status, expectedDataset.Status) {
-		t.Errorf("fail to exec the function with error %v", err)
-		return
-	}
-}
-
-func TestUpdateDatasetStatus(t *testing.T) {
-	testDatasetInputs := []*datav1alpha1.Dataset{
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hbase",
-				Namespace: "fluid",
-			},
-			Spec: datav1alpha1.DatasetSpec{},
-			Status: datav1alpha1.DatasetStatus{
-				HCFSStatus: &datav1alpha1.HCFSStatus{
+				}))
+				Expect(datasets.Items[0].Status.HCFSStatus).To(Equal(&datav1alpha1.HCFSStatus{
 					Endpoint:                    "test Endpoint",
 					UnderlayerFileSystemVersion: "Underlayer HCFS Compatible Version",
-				},
+				}))
 			},
-		},
-	}
-	testObjs := []runtime.Object{}
-	for _, datasetInput := range testDatasetInputs {
-		testObjs = append(testObjs, datasetInput.DeepCopy())
-	}
+			Entry("BoundDatasetPhase", datav1alpha1.BoundDatasetPhase, datav1alpha1.BoundDatasetPhase),
+			Entry("FailedDatasetPhase", datav1alpha1.FailedDatasetPhase, datav1alpha1.FailedDatasetPhase),
+			Entry("NoneDatasetPhase", datav1alpha1.NoneDatasetPhase, datav1alpha1.NoneDatasetPhase),
+		)
+	})
 
-	testRuntimeInputs := []*datav1alpha1.JindoRuntime{
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hbase",
-				Namespace: "fluid",
-			},
-			Spec: datav1alpha1.JindoRuntimeSpec{
-				Master: datav1alpha1.JindoCompTemplateSpec{
-					Replicas: 1,
+	Describe("BindToDataset", func() {
+		It("should bind to dataset successfully", func() {
+			testDatasetInputs := []*datav1alpha1.Dataset{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "hbase",
+						Namespace: "fluid",
+					},
+					Spec: datav1alpha1.DatasetSpec{},
+					Status: datav1alpha1.DatasetStatus{
+						HCFSStatus: &datav1alpha1.HCFSStatus{
+							Endpoint:                    "test Endpoint",
+							UnderlayerFileSystemVersion: "Underlayer HCFS Compatible Version",
+						},
+					},
 				},
-			},
-			Status: datav1alpha1.RuntimeStatus{
-				CacheStates: map[common.CacheStateName]string{
-					common.Cached: "true",
+			}
+			testObjs := []runtime.Object{}
+			for _, datasetInput := range testDatasetInputs {
+				testObjs = append(testObjs, datasetInput.DeepCopy())
+			}
+
+			testRuntimeInputs := []*datav1alpha1.JindoRuntime{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "hbase",
+						Namespace: "fluid",
+					},
+					Spec: datav1alpha1.JindoRuntimeSpec{},
+					Status: datav1alpha1.RuntimeStatus{
+						CacheStates: map[common.CacheStateName]string{
+							common.Cached: "true",
+						},
+					},
 				},
-			},
-		},
-	}
-	for _, runtimeInput := range testRuntimeInputs {
-		testObjs = append(testObjs, runtimeInput.DeepCopy())
-	}
-	client := fake.NewFakeClientWithScheme(testScheme, testObjs...)
+			}
+			for _, runtimeInput := range testRuntimeInputs {
+				testObjs = append(testObjs, runtimeInput.DeepCopy())
+			}
+			client := fake.NewFakeClientWithScheme(testScheme, testObjs...)
 
-	engine := &JindoFSxEngine{
-		Client:    client,
-		Log:       fake.NullLogger(),
-		name:      "hbase",
-		namespace: "fluid",
-		runtime:   testRuntimeInputs[0],
-	}
+			engine := &JindoFSxEngine{
+				Client:    client,
+				Log:       fake.NullLogger(),
+				name:      "hbase",
+				namespace: "fluid",
+				runtime:   testRuntimeInputs[0],
+			}
 
-	var testCase = []struct {
-		phase          datav1alpha1.DatasetPhase
-		expectedResult datav1alpha1.Dataset
-	}{
-		{
-			phase: datav1alpha1.BoundDatasetPhase,
-			expectedResult: datav1alpha1.Dataset{
+			expectedResult := datav1alpha1.Dataset{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      "hbase",
 					Namespace: "fluid",
@@ -261,160 +324,18 @@ func TestUpdateDatasetStatus(t *testing.T) {
 						Endpoint:                    "test Endpoint",
 						UnderlayerFileSystemVersion: "Underlayer HCFS Compatible Version",
 					},
-					Runtimes: []datav1alpha1.Runtime{
-						{
-							Name:           "hbase",
-							Namespace:      "fluid",
-							Category:       common.AccelerateCategory,
-							Type:           common.JindoRuntime,
-							MasterReplicas: 1,
-						},
-					},
 				},
-			},
-		},
-		{
-			phase: datav1alpha1.FailedDatasetPhase,
-			expectedResult: datav1alpha1.Dataset{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "hbase",
-					Namespace: "fluid",
-				},
-				Status: datav1alpha1.DatasetStatus{
-					Phase: datav1alpha1.FailedDatasetPhase,
-					CacheStates: map[common.CacheStateName]string{
-						common.Cached: "true",
-					},
-					HCFSStatus: &datav1alpha1.HCFSStatus{
-						Endpoint:                    "test Endpoint",
-						UnderlayerFileSystemVersion: "Underlayer HCFS Compatible Version",
-					},
-				},
-			},
-		},
-		{
-			phase: datav1alpha1.NoneDatasetPhase,
-			expectedResult: datav1alpha1.Dataset{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "hbase",
-					Namespace: "fluid",
-				},
-				Status: datav1alpha1.DatasetStatus{
-					Phase: datav1alpha1.NoneDatasetPhase,
-					CacheStates: map[common.CacheStateName]string{
-						common.Cached: "true",
-					},
-					HCFSStatus: &datav1alpha1.HCFSStatus{
-						Endpoint:                    "test Endpoint",
-						UnderlayerFileSystemVersion: "Underlayer HCFS Compatible Version",
-					},
-				},
-			},
-		},
-	}
+			}
 
-	for _, test := range testCase {
-		err := engine.UpdateDatasetStatus(test.phase)
-		if err != nil {
-			t.Errorf("fail to exec UpdateCacheOfDataset with error %v", err)
-			return
-		}
+			err := engine.BindToDataset()
+			Expect(err).NotTo(HaveOccurred())
 
-		var datasets datav1alpha1.DatasetList
-		err = client.List(context.TODO(), &datasets)
-		if err != nil {
-			t.Errorf("fail to list the datasets with error %v", err)
-			return
-		}
-		if !reflect.DeepEqual(datasets.Items[0].Status.Phase, test.expectedResult.Status.Phase) ||
-			!reflect.DeepEqual(datasets.Items[0].Status.CacheStates, test.expectedResult.Status.CacheStates) ||
-			!reflect.DeepEqual(datasets.Items[0].Status.HCFSStatus, test.expectedResult.Status.HCFSStatus) {
-			t.Errorf("fail to exec the function with error %v", err)
-			return
-		}
-	}
-}
-
-func TestBindToDataset(t *testing.T) {
-	testDatasetInputs := []*datav1alpha1.Dataset{
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hbase",
-				Namespace: "fluid",
-			},
-			Spec: datav1alpha1.DatasetSpec{},
-			Status: datav1alpha1.DatasetStatus{
-				HCFSStatus: &datav1alpha1.HCFSStatus{
-					Endpoint:                    "test Endpoint",
-					UnderlayerFileSystemVersion: "Underlayer HCFS Compatible Version",
-				},
-			},
-		},
-	}
-	testObjs := []runtime.Object{}
-	for _, datasetInput := range testDatasetInputs {
-		testObjs = append(testObjs, datasetInput.DeepCopy())
-	}
-
-	testRuntimeInputs := []*datav1alpha1.JindoRuntime{
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hbase",
-				Namespace: "fluid",
-			},
-			Spec: datav1alpha1.JindoRuntimeSpec{},
-			Status: datav1alpha1.RuntimeStatus{
-				CacheStates: map[common.CacheStateName]string{
-					common.Cached: "true",
-				},
-			},
-		},
-	}
-	for _, runtimeInput := range testRuntimeInputs {
-		testObjs = append(testObjs, runtimeInput.DeepCopy())
-	}
-	client := fake.NewFakeClientWithScheme(testScheme, testObjs...)
-
-	engine := &JindoFSxEngine{
-		Client:    client,
-		Log:       fake.NullLogger(),
-		name:      "hbase",
-		namespace: "fluid",
-		runtime:   testRuntimeInputs[0],
-	}
-
-	var expectedResult = datav1alpha1.Dataset{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "hbase",
-			Namespace: "fluid",
-		},
-		Status: datav1alpha1.DatasetStatus{
-			Phase: datav1alpha1.BoundDatasetPhase,
-			CacheStates: map[common.CacheStateName]string{
-				common.Cached: "true",
-			},
-			HCFSStatus: &datav1alpha1.HCFSStatus{
-				Endpoint:                    "test Endpoint",
-				UnderlayerFileSystemVersion: "Underlayer HCFS Compatible Version",
-			},
-		},
-	}
-	err := engine.BindToDataset()
-	if err != nil {
-		t.Errorf("fail to exec UpdateCacheOfDataset with error %v", err)
-		return
-	}
-
-	var datasets datav1alpha1.DatasetList
-	err = client.List(context.TODO(), &datasets)
-	if err != nil {
-		t.Errorf("fail to list the datasets with error %v", err)
-		return
-	}
-	if !reflect.DeepEqual(datasets.Items[0].Status.Phase, expectedResult.Status.Phase) ||
-		!reflect.DeepEqual(datasets.Items[0].Status.CacheStates, expectedResult.Status.CacheStates) ||
-		!reflect.DeepEqual(datasets.Items[0].Status.HCFSStatus, expectedResult.Status.HCFSStatus) {
-		t.Errorf("fail to exec the function with error %v", err)
-		return
-	}
-}
+			var datasets datav1alpha1.DatasetList
+			err = client.List(context.TODO(), &datasets)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(datasets.Items[0].Status.Phase).To(Equal(expectedResult.Status.Phase))
+			Expect(datasets.Items[0].Status.CacheStates).To(Equal(expectedResult.Status.CacheStates))
+			Expect(datasets.Items[0].Status.HCFSStatus).To(Equal(expectedResult.Status.HCFSStatus))
+		})
+	})
+})
