8207a189bb60944ae5c75857d580e2bbf8e24cdf
Popie52 <popiesailor@gmail.com>
2026-02-01 19:09:12 +0530

    test(webhook): add Ginkgo/Gomega tests for MountPropagationInjector
    Signed-off-by: Popie52 <popiesailor@gmail.com>

--- pkg/webhook/plugins/mountpropagationinjector/mount_propagation_injector_test.go
+++ pkg/webhook/plugins/mountpropagationinjector/mount_propagation_injector_test.go
@@ -1,10 +1,9 @@
 /*
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,60 +11,76 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
 package mountpropagationinjector
 
 import (
-	"testing"
-
 	"github.com/fluid-cloudnative/fluid/pkg/ddc/base"
+	"github.com/fluid-cloudnative/fluid/pkg/webhook/plugins/api"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
-func TestMutate(t *testing.T) {
+var _ = Describe("MountPropagationInjector Plugin", func() {
 	var (
-		client client.Client
+		plugin api.MutatingHandler
 		pod    *corev1.Pod
 	)
 
-	plugin, err := NewPlugin(client, "")
-	if err != nil {
-		t.Error("new plugin occurs error", err)
-	}
-	if plugin.GetName() != Name {
-		t.Errorf("GetName expect %v, got %v", Name, plugin.GetName())
-	}
+	BeforeEach(func() {
+		scheme := runtime.NewScheme()
+		Expect(corev1.AddToScheme(scheme)).To(Succeed())
 
-	runtimeInfo, err := base.BuildRuntimeInfo("test", "fluid", "alluxio")
-	if err != nil {
-		t.Errorf("fail to create the runtimeInfo with error %v", err)
-	}
+		c := fake.NewClientBuilder().
+			WithScheme(scheme).
+			Build()
 
-	pod = &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "test",
-			Namespace: "test",
-		},
-	}
+		var err error
+		plugin, err = NewPlugin(c, "")
+		Expect(err).NotTo(HaveOccurred())
 
-	shouldStop, err := plugin.Mutate(pod, map[string]base.RuntimeInfoInterface{"test": runtimeInfo})
-	if err != nil {
-		t.Errorf("fail to mutate pod with error %v", err)
-	}
+		pod = &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test",
+				Namespace: "test",
+			},
+		}
+	})
 
-	if shouldStop {
-		t.Errorf("expect shouldStop as false, but got %v", shouldStop)
-	}
+	It("returns correct plugin name", func() {
+		Expect(plugin.GetName()).To(Equal(Name))
+	})
 
-	_, err = plugin.Mutate(pod, map[string]base.RuntimeInfoInterface{})
-	if err != nil {
-		t.Errorf("fail to mutate pod with error %v", err)
-	}
+	Context("when mutating a pod", func() {
+		It("does not stop when runtimeInfo exists", func() {
+			runtimeInfo, err := base.BuildRuntimeInfo("test", "fluid", "alluxio")
+			Expect(err).NotTo(HaveOccurred())
 
-	_, err = plugin.Mutate(pod, map[string]base.RuntimeInfoInterface{"test": nil})
-	if err == nil {
-		t.Errorf("expect error is not nil")
-	}
-}
+			shouldStop, err := plugin.Mutate(
+				pod,
+				map[string]base.RuntimeInfoInterface{"test": runtimeInfo},
+			)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(shouldStop).To(BeFalse())
+		})
+
+		It("does not error when runtimeInfos is empty", func() {
+			_, err := plugin.Mutate(pod, map[string]base.RuntimeInfoInterface{})
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("returns error when runtimeInfo is nil", func() {
+			_, err := plugin.Mutate(
+				pod,
+				map[string]base.RuntimeInfoInterface{"test": nil},
+			)
+			Expect(err).To(HaveOccurred())
+		})
+	})
+})
