c02016233c37c20764689e02cc2acba519019016
harsh kumar <135993950+hxrshxz@users.noreply.github.com>
2026-02-06 13:11:58 +0530

    refactor(juicefs): migrate metadata_test.go to Ginkgo/Gomega (#5494)
    * refactor(juicefs): migrate metadata_test.go to Ginkgo/Gomega
    
    Signed-off-by: Harsh <harshmastic@gmail.com>
    
    * test: add missing AutoSync disabled test cases
    
    Signed-off-by: Harsh <harshmastic@gmail.com>
    
    * fix: change suite package to juicefs
    
    Signed-off-by: Harsh <harshmastic@gmail.com>
    
    ---------
    
    Signed-off-by: Harsh <harshmastic@gmail.com>

--- pkg/ddc/juicefs/metadata_test.go
+++ pkg/ddc/juicefs/metadata_test.go
@@ -18,347 +18,403 @@ package juicefs
 
 import (
 	"context"
-	"testing"
 	"time"
 
-	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/utils/ptr"
-
 	"github.com/agiledragon/gomonkey/v2"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-
-	"github.com/fluid-cloudnative/fluid/pkg/utils/fake"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
 	"github.com/fluid-cloudnative/fluid/pkg/ddc/base"
 	"github.com/fluid-cloudnative/fluid/pkg/ddc/juicefs/operations"
+	"github.com/fluid-cloudnative/fluid/pkg/utils/fake"
 )
 
-func TestShouldSyncMetadata(t *testing.T) {
-	datasetInputs := []datav1alpha1.Dataset{
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hbase",
-				Namespace: "fluid",
+const (
+	metadataTestNamespace        = "fluid"
+	metadataTestNameHbase        = "hbase"
+	metadataTestNameSpark        = "spark"
+	metadataTestNameHadoop       = "hadoop"
+	metadataTestNameNoSync       = "no-sync"
+	metadataTestNameTest1        = "test1"
+	metadataTestNameTest2        = "test2"
+	metadataTestUfsTotalInitial  = "2Gi"
+	metadataTestUfsTotalExpected = "2GB"
+	metadataTestFileNumExpected  = "5"
+	metadataTestMetaQueryResult  = "1024"
+	metadataTestRestorePath      = "local:///host1/erf"
+	metadataTestRestoreNode      = "test-node"
+)
+
+var _ = Describe("ShouldSyncMetadata", Label("pkg.ddc.juicefs.metadata_test.go"), func() {
+	var (
+		testClient client.Client
+		testObjs   []runtime.Object
+	)
+
+	BeforeEach(func() {
+		datasetInputs := []datav1alpha1.Dataset{
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      metadataTestNameHbase,
+					Namespace: metadataTestNamespace,
+				},
+				Status: datav1alpha1.DatasetStatus{
+					UfsTotal: metadataTestUfsTotalInitial,
+				},
 			},
-			Status: datav1alpha1.DatasetStatus{
-				UfsTotal: "2Gi",
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      metadataTestNameSpark,
+					Namespace: metadataTestNamespace,
+				},
+				Status: datav1alpha1.DatasetStatus{
+					UfsTotal: "",
+				},
 			},
-		},
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "spark",
-				Namespace: "fluid",
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      metadataTestNameNoSync,
+					Namespace: metadataTestNamespace,
+				},
 			},
-			Status: datav1alpha1.DatasetStatus{
-				UfsTotal: "",
+		}
+		runtimeInputs := []datav1alpha1.JuiceFSRuntime{
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      metadataTestNameHbase,
+					Namespace: metadataTestNamespace,
+				},
 			},
-		},
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "no-sync",
-				Namespace: "fluid",
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      metadataTestNameSpark,
+					Namespace: metadataTestNamespace,
+				},
 			},
-		},
-	}
-	runtimeInputs := []datav1alpha1.JuiceFSRuntime{
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hbase",
-				Namespace: "fluid",
-			},
-		},
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "spark",
-				Namespace: "fluid",
-			},
-		},
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "no-sync",
-				Namespace: "fluid",
-			},
-			Spec: datav1alpha1.JuiceFSRuntimeSpec{
-				RuntimeManagement: datav1alpha1.RuntimeManagement{
-					MetadataSyncPolicy: datav1alpha1.MetadataSyncPolicy{
-						AutoSync: ptr.To(false),
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      metadataTestNameNoSync,
+					Namespace: metadataTestNamespace,
+				},
+				Spec: datav1alpha1.JuiceFSRuntimeSpec{
+					RuntimeManagement: datav1alpha1.RuntimeManagement{
+						MetadataSyncPolicy: datav1alpha1.MetadataSyncPolicy{
+							AutoSync: ptr.To(false),
+						},
 					},
 				},
 			},
-		},
-	}
-	testObjs := []runtime.Object{}
-	for _, datasetInput := range datasetInputs {
-		testObjs = append(testObjs, datasetInput.DeepCopy())
-	}
-	for _, runtimeInput := range runtimeInputs {
-		testObjs = append(testObjs, runtimeInput.DeepCopy())
-	}
-	client := fake.NewFakeClientWithScheme(testScheme, testObjs...)
-
-	engines := []JuiceFSEngine{
-		{
-			name:      "hbase",
-			namespace: "fluid",
-			Client:    client,
-			Log:       fake.NullLogger(),
-		},
-		{
-			name:      "spark",
-			namespace: "fluid",
-			Client:    client,
-			Log:       fake.NullLogger(),
-		},
-	}
-
-	var testCases = []struct {
-		engine         JuiceFSEngine
-		expectedShould bool
-	}{
-		{
-			engine:         engines[0],
-			expectedShould: false,
-		},
-		{
-			engine:         engines[1],
-			expectedShould: true,
-		},
-	}
-
-	for _, test := range testCases {
-		should, err := test.engine.shouldSyncMetadata()
-		if err != nil || should != test.expectedShould {
-			t.Errorf("fail to exec the function")
 		}
-	}
-}
 
-func TestSyncMetadata(t *testing.T) {
-	QueryMetaDataInfoIntoFileCommon := func(a operations.JuiceFileUtils, key operations.KeyOfMetaDataFile, filename string) (value string, err error) {
-		return "1024", nil
-	}
+		testObjs = []runtime.Object{}
+		for _, datasetInput := range datasetInputs {
+			testObjs = append(testObjs, datasetInput.DeepCopy())
+		}
+		for _, runtimeInput := range runtimeInputs {
+			testObjs = append(testObjs, runtimeInput.DeepCopy())
+		}
+		testClient = fake.NewFakeClientWithScheme(testScheme, testObjs...)
+	})
 
-	datasetInputs := []datav1alpha1.Dataset{
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hbase",
-				Namespace: "fluid",
-			},
-			Status: datav1alpha1.DatasetStatus{
-				UfsTotal: "2Gi",
-			},
-		},
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "spark",
-				Namespace: "fluid",
-			},
-			Status: datav1alpha1.DatasetStatus{
-				UfsTotal: "",
-			},
-		},
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hadoop",
-				Namespace: "fluid",
-			},
-			Spec: datav1alpha1.DatasetSpec{
-				DataRestoreLocation: &datav1alpha1.DataRestoreLocation{
-					Path:     "local:///host1/erf",
-					NodeName: "test-node",
+	Context("when dataset already has UfsTotal set", func() {
+		It("should return false", func() {
+			engine := JuiceFSEngine{
+				name:      metadataTestNameHbase,
+				namespace: metadataTestNamespace,
+				Client:    testClient,
+				Log:       fake.NullLogger(),
+			}
+
+			should, err := engine.shouldSyncMetadata()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(should).To(BeFalse())
+		})
+	})
+
+	Context("when dataset has empty UfsTotal", func() {
+		It("should return true", func() {
+			engine := JuiceFSEngine{
+				name:      metadataTestNameSpark,
+				namespace: metadataTestNamespace,
+				Client:    testClient,
+				Log:       fake.NullLogger(),
+			}
+
+			should, err := engine.shouldSyncMetadata()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(should).To(BeTrue())
+		})
+	})
+
+	Context("when AutoSync is disabled in runtime", func() {
+		It("should return false", func() {
+			engine := JuiceFSEngine{
+				name:      metadataTestNameNoSync,
+				namespace: metadataTestNamespace,
+				Client:    testClient,
+				Log:       fake.NullLogger(),
+			}
+
+			should, err := engine.shouldSyncMetadata()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(should).To(BeFalse())
+		})
+	})
+})
+
+var _ = Describe("SyncMetadata", Label("pkg.ddc.juicefs.metadata_test.go"), func() {
+	var (
+		testClient client.Client
+		testObjs   []runtime.Object
+	)
+
+	BeforeEach(func() {
+		datasetInputs := []datav1alpha1.Dataset{
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      metadataTestNameHbase,
+					Namespace: metadataTestNamespace,
+				},
+				Status: datav1alpha1.DatasetStatus{
+					UfsTotal: metadataTestUfsTotalInitial,
 				},
 			},
-			Status: datav1alpha1.DatasetStatus{
-				UfsTotal: "",
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      metadataTestNameSpark,
+					Namespace: metadataTestNamespace,
+				},
+				Status: datav1alpha1.DatasetStatus{
+					UfsTotal: "",
+				},
 			},
-		},
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "no-sync",
-				Namespace: "fluid",
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      metadataTestNameHadoop,
+					Namespace: metadataTestNamespace,
+				},
+				Spec: datav1alpha1.DatasetSpec{
+					DataRestoreLocation: &datav1alpha1.DataRestoreLocation{
+						Path:     metadataTestRestorePath,
+						NodeName: metadataTestRestoreNode,
+					},
+				},
+				Status: datav1alpha1.DatasetStatus{
+					UfsTotal: "",
+				},
 			},
-			Status: datav1alpha1.DatasetStatus{
-				UfsTotal: "",
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      metadataTestNameNoSync,
+					Namespace: metadataTestNamespace,
+				},
+				Status: datav1alpha1.DatasetStatus{
+					UfsTotal: "",
+				},
 			},
-		},
-	}
-	runtimeInputs := []datav1alpha1.JuiceFSRuntime{
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hbase",
-				Namespace: "fluid",
+		}
+		runtimeInputs := []datav1alpha1.JuiceFSRuntime{
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      metadataTestNameHbase,
+					Namespace: metadataTestNamespace,
+				},
+				Spec: datav1alpha1.JuiceFSRuntimeSpec{
+					RuntimeManagement: datav1alpha1.RuntimeManagement{},
+				},
 			},
-			Spec: datav1alpha1.JuiceFSRuntimeSpec{
-				RuntimeManagement: datav1alpha1.RuntimeManagement{},
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      metadataTestNameSpark,
+					Namespace: metadataTestNamespace,
+				},
+				Spec: datav1alpha1.JuiceFSRuntimeSpec{
+					RuntimeManagement: datav1alpha1.RuntimeManagement{},
+				},
 			},
-		},
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "spark",
-				Namespace: "fluid",
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      metadataTestNameHadoop,
+					Namespace: metadataTestNamespace,
+				},
+				Spec: datav1alpha1.JuiceFSRuntimeSpec{
+					RuntimeManagement: datav1alpha1.RuntimeManagement{},
+				},
 			},
-			Spec: datav1alpha1.JuiceFSRuntimeSpec{
-				RuntimeManagement: datav1alpha1.RuntimeManagement{},
-			},
-		},
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "hadoop",
-				Namespace: "fluid",
-			},
-			Spec: datav1alpha1.JuiceFSRuntimeSpec{
-				RuntimeManagement: datav1alpha1.RuntimeManagement{},
-			},
-		},
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "no-sync",
-				Namespace: "fluid",
-			},
-			Spec: datav1alpha1.JuiceFSRuntimeSpec{
-				RuntimeManagement: datav1alpha1.RuntimeManagement{
-					MetadataSyncPolicy: datav1alpha1.MetadataSyncPolicy{
-						AutoSync: ptr.To(false),
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      metadataTestNameNoSync,
+					Namespace: metadataTestNamespace,
+				},
+				Spec: datav1alpha1.JuiceFSRuntimeSpec{
+					RuntimeManagement: datav1alpha1.RuntimeManagement{
+						MetadataSyncPolicy: datav1alpha1.MetadataSyncPolicy{
+							AutoSync: ptr.To(false),
+						},
 					},
 				},
 			},
-		},
-	}
-
-	testObjs := []runtime.Object{}
-	for _, datasetInput := range datasetInputs {
-		testObjs = append(testObjs, datasetInput.DeepCopy())
-	}
-	for _, runtimeInput := range runtimeInputs {
-		testObjs = append(testObjs, runtimeInput.DeepCopy())
-	}
-	client := fake.NewFakeClientWithScheme(testScheme, testObjs...)
-
-	engines := []JuiceFSEngine{
-		{
-			name:      "hbase",
-			namespace: "fluid",
-			Client:    client,
-			Log:       fake.NullLogger(),
-		},
-		{
-			name:      "spark",
-			namespace: "fluid",
-			Client:    client,
-			Log:       fake.NullLogger(),
-		},
-	}
-
-	for _, engine := range engines {
-		err := engine.SyncMetadata()
-		if err != nil {
-			t.Errorf("fail to exec the function")
 		}
-	}
 
-	engine := JuiceFSEngine{
-		name:      "hadoop",
-		namespace: "fluid",
-		Client:    client,
-		Log:       fake.NullLogger(),
-	}
+		testObjs = []runtime.Object{}
+		for _, datasetInput := range datasetInputs {
+			testObjs = append(testObjs, datasetInput.DeepCopy())
+		}
+		for _, runtimeInput := range runtimeInputs {
+			testObjs = append(testObjs, runtimeInput.DeepCopy())
+		}
+		testClient = fake.NewFakeClientWithScheme(testScheme, testObjs...)
+	})
 
-	patch := gomonkey.ApplyMethodFunc(operations.JuiceFileUtils{}, "QueryMetaDataInfoIntoFile", QueryMetaDataInfoIntoFileCommon)
-	defer patch.Reset()
+	Context("when syncing metadata for hbase engine", func() {
+		It("should succeed without error", func() {
+			engine := JuiceFSEngine{
+				name:      metadataTestNameHbase,
+				namespace: metadataTestNamespace,
+				Client:    testClient,
+				Log:       fake.NullLogger(),
+			}
 
-	err := engine.SyncMetadata()
-	if err != nil {
-		t.Errorf("fail to exec function RestoreMetadataInternal: %v", err)
-	}
-}
+			err := engine.SyncMetadata()
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
 
-func TestJuiceFSEngine_syncMetadataInternal(t *testing.T) {
-	datasetInputs := []datav1alpha1.Dataset{
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "test1",
-				Namespace: "fluid",
+	Context("when syncing metadata for spark engine", func() {
+		It("should succeed without error", func() {
+			engine := JuiceFSEngine{
+				name:      metadataTestNameSpark,
+				namespace: metadataTestNamespace,
+				Client:    testClient,
+				Log:       fake.NullLogger(),
+			}
+
+			err := engine.SyncMetadata()
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+
+	Context("when syncing metadata for hadoop engine with data restore", func() {
+		It("should succeed using mocked QueryMetaDataInfoIntoFile", func() {
+			engine := JuiceFSEngine{
+				name:      metadataTestNameHadoop,
+				namespace: metadataTestNamespace,
+				Client:    testClient,
+				Log:       fake.NullLogger(),
+			}
+
+			patches := gomonkey.ApplyMethodFunc(operations.JuiceFileUtils{}, "QueryMetaDataInfoIntoFile",
+				func(key operations.KeyOfMetaDataFile, filename string) (string, error) {
+					return metadataTestMetaQueryResult, nil
+				})
+			defer patches.Reset()
+
+			err := engine.SyncMetadata()
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+
+	Context("when AutoSync is disabled in runtime", func() {
+		It("should not sync metadata and return no error", func() {
+			engine := JuiceFSEngine{
+				name:      metadataTestNameNoSync,
+				namespace: metadataTestNamespace,
+				Client:    testClient,
+				Log:       fake.NullLogger(),
+			}
+
+			err := engine.SyncMetadata()
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+})
+
+var _ = Describe("SyncMetadataInternal", Label("pkg.ddc.juicefs.metadata_test.go"), func() {
+	var (
+		testClient client.Client
+		testObjs   []runtime.Object
+	)
+
+	BeforeEach(func() {
+		datasetInputs := []datav1alpha1.Dataset{
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      metadataTestNameTest1,
+					Namespace: metadataTestNamespace,
+				},
+				Spec: datav1alpha1.DatasetSpec{},
 			},
-			Spec: datav1alpha1.DatasetSpec{},
-		},
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "test2",
-				Namespace: "fluid",
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      metadataTestNameTest2,
+					Namespace: metadataTestNamespace,
+				},
+				Spec: datav1alpha1.DatasetSpec{},
 			},
-			Spec: datav1alpha1.DatasetSpec{},
-		},
-	}
-	testObjs := []runtime.Object{}
-	for _, datasetInput := range datasetInputs {
-		testObjs = append(testObjs, datasetInput.DeepCopy())
-	}
-	client := fake.NewFakeClientWithScheme(testScheme, testObjs...)
+		}
 
-	engines := []JuiceFSEngine{
-		{
-			name:               "test1",
-			namespace:          "fluid",
-			Client:             client,
-			Log:                fake.NullLogger(),
-			MetadataSyncDoneCh: make(chan base.MetadataSyncResult),
-		},
-		{
-			name:               "test2",
-			namespace:          "fluid",
-			Client:             client,
-			Log:                fake.NullLogger(),
-			MetadataSyncDoneCh: nil,
-		},
-	}
+		testObjs = []runtime.Object{}
+		for _, datasetInput := range datasetInputs {
+			testObjs = append(testObjs, datasetInput.DeepCopy())
+		}
+		testClient = fake.NewFakeClientWithScheme(testScheme, testObjs...)
+	})
 
-	result := base.MetadataSyncResult{
-		StartTime: time.Now(),
-		UfsTotal:  "2GB",
-		Done:      true,
-		FileNum:   "5",
-	}
+	Context("when engine has MetadataSyncDoneCh", func() {
+		It("should update dataset status with sync result", func() {
+			engine := JuiceFSEngine{
+				name:               metadataTestNameTest1,
+				namespace:          metadataTestNamespace,
+				Client:             testClient,
+				Log:                fake.NullLogger(),
+				MetadataSyncDoneCh: make(chan base.MetadataSyncResult),
+			}
 
-	var testCase = []struct {
-		engine           JuiceFSEngine
-		expectedResult   bool
-		expectedUfsTotal string
-		expectedFileNum  string
-	}{
-		{
-			engine:           engines[0],
-			expectedUfsTotal: "2GB",
-			expectedFileNum:  "5",
-		},
-	}
+			result := base.MetadataSyncResult{
+				StartTime: time.Now(),
+				UfsTotal:  metadataTestUfsTotalExpected,
+				Done:      true,
+				FileNum:   metadataTestFileNumExpected,
+			}
 
-	for index, test := range testCase {
-		if index == 0 {
 			go func() {
-				test.engine.MetadataSyncDoneCh <- result
+				engine.MetadataSyncDoneCh <- result
 			}()
-		}
 
-		err := test.engine.syncMetadataInternal()
-		if err != nil {
-			t.Errorf("fail to exec the function with error %v", err)
-		}
+			err := engine.syncMetadataInternal()
+			Expect(err).NotTo(HaveOccurred())
 
-		key := types.NamespacedName{
-			Namespace: test.engine.namespace,
-			Name:      test.engine.name,
-		}
+			key := types.NamespacedName{
+				Namespace: engine.namespace,
+				Name:      engine.name,
+			}
 
-		dataset := &datav1alpha1.Dataset{}
-		err = client.Get(context.TODO(), key, dataset)
-		if err != nil {
-			t.Errorf("failt to get the dataset with error %v", err)
-		}
+			dataset := &datav1alpha1.Dataset{}
+			err = testClient.Get(context.Background(), key, dataset)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(dataset.Status.UfsTotal).To(Equal(metadataTestUfsTotalExpected))
+			Expect(dataset.Status.FileNum).To(Equal(metadataTestFileNumExpected))
+		})
+	})
 
-		if dataset.Status.UfsTotal != test.expectedUfsTotal || dataset.Status.FileNum != test.expectedFileNum {
-			t.Errorf("expected UfsTotal %s, get UfsTotal %s, expected FileNum %s, get FileNum %s", test.expectedUfsTotal, dataset.Status.UfsTotal, test.expectedFileNum, dataset.Status.FileNum)
-		}
-	}
-}
+	Context("when engine has nil MetadataSyncDoneCh", func() {
+		It("should succeed without error", func() {
+			engine := JuiceFSEngine{
+				name:               metadataTestNameTest2,
+				namespace:          metadataTestNamespace,
+				Client:             testClient,
+				Log:                fake.NullLogger(),
+				MetadataSyncDoneCh: nil,
+			}
+
+			err := engine.syncMetadataInternal()
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+})
