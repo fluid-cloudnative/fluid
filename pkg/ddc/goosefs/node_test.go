0fcf2d86bfa8f6c337f146f7837bf06579fdd71a
harsh kumar <135993950+hxrshxz@users.noreply.github.com>
2026-02-08 13:53:25 +0530

    Enhance SyncScheduleInfoToCacheNodes test coverage (#5463)
    * enhance SyncScheduleInfoToCacheNodes test coverage
    
    Signed-off-by: Harsh <harshmastic@gmail.com>
    
    * minor fixes
    
    Signed-off-by: Harsh <harshmastic@gmail.com>
    
    * test: enhance SyncScheduleInfoToCacheNodes test coverage
    
    Signed-off-by: Harsh <harshmastic@gmail.com>
    
    ---------
    
    Signed-off-by: Harsh <harshmastic@gmail.com>

--- pkg/ddc/goosefs/node_test.go
+++ pkg/ddc/goosefs/node_test.go
@@ -31,16 +31,27 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func getTestGooseFSEngineNode(client client.Client, name string, namespace string, withRunTime bool) *GooseFSEngine {
+const (
+	testNodeNamespace     = "big-data"
+	testNodeLabelApp      = "goosefs"
+	testNodeLabelRole     = "goosefs-worker"
+	testNodeAPIVersion    = "apps/v1"
+	testNodeKindSts       = "StatefulSet"
+	testNodeLabelDataset  = "fluid.io/dataset"
+	testNodeLabelSelector = "%s=true"
+)
+
+func getTestGooseFSEngineNode(c client.Client, name string, namespace string, withRunTime bool) *GooseFSEngine {
 	engine := &GooseFSEngine{
 		runtime:     nil,
 		name:        name,
 		namespace:   namespace,
-		Client:      client,
+		Client:      c,
 		runtimeInfo: nil,
 		Log:         fake.NullLogger(),
 	}
@@ -53,10 +64,8 @@ func getTestGooseFSEngineNode(client client.Client, name string, namespace strin
 
 func TestSyncScheduleInfoToCacheNodes(t *testing.T) {
 	type fields struct {
-		// runtime   *datav1alpha1.GooseFSRuntime
 		worker    *appsv1.StatefulSet
 		pods      []*v1.Pod
-		ds        *appsv1.DaemonSet
 		nodes     []*v1.Node
 		name      string
 		namespace string
@@ -65,199 +74,127 @@ func TestSyncScheduleInfoToCacheNodes(t *testing.T) {
 		name      string
 		fields    fields
 		nodeNames []string
-	}{
-		{
-			name: "create",
-			fields: fields{
-				name:      "spark",
-				namespace: "big-data",
-				worker: &appsv1.StatefulSet{
-					TypeMeta: metav1.TypeMeta{
-						Kind:       "StatefulSet",
-						APIVersion: "apps/v1",
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "spark-worker",
-						Namespace: "big-data",
-						UID:       "uid1",
-					},
-					Spec: appsv1.StatefulSetSpec{
-						Selector: &metav1.LabelSelector{
-							MatchLabels: map[string]string{
-								"app":     "goosefs",
-								"role":    "goosefs-worker",
-								"release": "spark",
-							},
-						},
-					},
+	}{}
+
+	testcaseCnt := 0
+	makeDatasetResourcesFn := func(dsName string, dsNamespace string, stsPodNodeNames []string) fields {
+		testcaseCnt++
+		ret := fields{
+			name:      dsName,
+			namespace: dsNamespace,
+			worker: &appsv1.StatefulSet{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       testNodeKindSts,
+					APIVersion: testNodeAPIVersion,
 				},
-				pods: []*v1.Pod{
-					{
-						ObjectMeta: metav1.ObjectMeta{
-							Name:      "spark-worker-0",
-							Namespace: "big-data",
-							OwnerReferences: []metav1.OwnerReference{{
-								Kind:       "StatefulSet",
-								APIVersion: "apps/v1",
-								Name:       "spark-worker",
-								UID:        "uid1",
-								Controller: ptr.To(true),
-							}},
-							Labels: map[string]string{
-								"app":              "goosefs",
-								"role":             "goosefs-worker",
-								"release":          "spark",
-								"fluid.io/dataset": "big-data-spark",
-							},
-						},
-						Spec: v1.PodSpec{
-							NodeName: "node1",
-						},
-					},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      dsName + "-worker",
+					Namespace: dsNamespace,
+					UID:       types.UID(fmt.Sprintf("uid%d", testcaseCnt)),
 				},
-				nodes: []*v1.Node{
-					{
-						ObjectMeta: metav1.ObjectMeta{
-							Name: "node1",
+				Spec: appsv1.StatefulSetSpec{
+					Selector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							"app":     testNodeLabelApp,
+							"role":    testNodeLabelRole,
+							"release": dsName,
 						},
 					},
 				},
 			},
-			nodeNames: []string{"node1"},
-		}, {
-			name: "add",
-			fields: fields{
-				name:      "hbase",
-				namespace: "big-data",
-				worker: &appsv1.StatefulSet{
-					TypeMeta: metav1.TypeMeta{
-						Kind:       "StatefulSet",
-						APIVersion: "apps/v1",
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "hbase-worker",
-						Namespace: "big-data",
-						UID:       "uid2",
-					},
-					Spec: appsv1.StatefulSetSpec{
-						Selector: &metav1.LabelSelector{
-							MatchLabels: map[string]string{
-								"app":     "goosefs",
-								"role":    "goosefs-worker",
-								"release": "hbase",
-							},
-						},
+			pods: []*v1.Pod{},
+		}
+
+		for idx, nodeName := range stsPodNodeNames {
+			ret.pods = append(ret.pods, &v1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      fmt.Sprintf("%s-worker-%d", dsName, idx),
+					Namespace: dsNamespace,
+					OwnerReferences: []metav1.OwnerReference{{
+						Kind:       testNodeKindSts,
+						APIVersion: testNodeAPIVersion,
+						Name:       dsName + "-worker",
+						UID:        types.UID(fmt.Sprintf("uid%d", testcaseCnt)),
+						Controller: ptr.To(true),
+					}},
+					Labels: map[string]string{
+						"app":                testNodeLabelApp,
+						"role":               testNodeLabelRole,
+						"release":            dsName,
+						testNodeLabelDataset: fmt.Sprintf("%s-%s", dsNamespace, dsName),
 					},
 				},
-				pods: []*v1.Pod{
-					{
-						ObjectMeta: metav1.ObjectMeta{
-							Name:      "hbase-worker-0",
-							Namespace: "big-data",
-							OwnerReferences: []metav1.OwnerReference{{
-								Kind:       "StatefulSet",
-								APIVersion: "apps/v1",
-								Name:       "hbase-worker",
-								UID:        "uid2",
-								Controller: ptr.To(true),
-							}},
-							Labels: map[string]string{
-								"app":              "goosefs",
-								"role":             "goosefs-worker",
-								"release":          "hbase",
-								"fluid.io/dataset": "big-data-hbase",
-							},
-						},
-						Spec: v1.PodSpec{
-							NodeName: "node3",
-						},
-					},
+				Spec: v1.PodSpec{
+					NodeName: nodeName,
 				},
-				nodes: []*v1.Node{
-					{
-						ObjectMeta: metav1.ObjectMeta{
-							Name: "node3",
-						},
-					}, {
-						ObjectMeta: metav1.ObjectMeta{
-							Name: "node2",
-							Labels: map[string]string{
-								"fluid.io/s-default-hbase": "true",
-							},
-						},
-					},
-				},
-			},
-			nodeNames: []string{"node3"},
-		}, {
-			name: "noController",
-			fields: fields{
-				name:      "hbase-a",
-				namespace: "big-data",
-				worker: &appsv1.StatefulSet{
-					TypeMeta: metav1.TypeMeta{
-						Kind:       "StatefulSet",
-						APIVersion: "apps/v1",
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "hbase-a-worker",
-						Namespace: "big-data",
-						UID:       "uid3",
-					},
-					Spec: appsv1.StatefulSetSpec{
-						Selector: &metav1.LabelSelector{
-							MatchLabels: map[string]string{
-								"app":     "goosefs",
-								"role":    "goosefs-worker",
-								"release": "hbase-a",
-							},
-						},
-					},
-				},
-				pods: []*v1.Pod{
-					{
-						ObjectMeta: metav1.ObjectMeta{
-							Name:      "hbase-a-worker-0",
-							Namespace: "big-data",
-							Labels: map[string]string{
-								"app":              "goosefs",
-								"role":             "goosefs-worker",
-								"release":          "hbase-a",
-								"fluid.io/dataset": "big-data-hbase-a",
-							},
-						},
-						Spec: v1.PodSpec{
-							NodeName: "node5",
-						},
-					},
-				},
-				nodes: []*v1.Node{
-					{
-						ObjectMeta: metav1.ObjectMeta{
-							Name: "node5",
-						},
-					}, {
-						ObjectMeta: metav1.ObjectMeta{
-							Name: "node4",
-							Labels: map[string]string{
-								"fluid.io/s-default-hbase-a": "true",
-							},
-						},
-					},
-				},
-			},
-			nodeNames: []string{},
-		},
+			})
+		}
+
+		return ret
 	}
+
+	fields1 := makeDatasetResourcesFn("spark", testNodeNamespace, []string{"node1"})
+	fields1.nodes = append(fields1.nodes, &v1.Node{ObjectMeta: metav1.ObjectMeta{Name: "node1"}})
+	testcases = append(testcases, struct {
+		name      string
+		fields    fields
+		nodeNames []string
+	}{
+		name:      "create",
+		fields:    fields1,
+		nodeNames: []string{"node1"},
+	})
+
+	fields2 := makeDatasetResourcesFn("hbase", testNodeNamespace, []string{"node2", "node3"})
+	fields2.nodes = append(fields2.nodes,
+		&v1.Node{ObjectMeta: metav1.ObjectMeta{Name: "node3"}},
+		&v1.Node{ObjectMeta: metav1.ObjectMeta{Name: "node2", Labels: map[string]string{"fluid.io/s-big-data-hbase": "true"}}},
+	)
+	testcases = append(testcases, struct {
+		name      string
+		fields    fields
+		nodeNames []string
+	}{
+		name:      "add",
+		fields:    fields2,
+		nodeNames: []string{"node2", "node3"},
+	})
+
+	fields3 := makeDatasetResourcesFn("hbase-a", testNodeNamespace, []string{"node4", "node5"})
+	fields3.pods[1].OwnerReferences = []metav1.OwnerReference{}
+	fields3.nodes = append(fields3.nodes,
+		&v1.Node{ObjectMeta: metav1.ObjectMeta{Name: "node5"}},
+		&v1.Node{ObjectMeta: metav1.ObjectMeta{Name: "node4", Labels: map[string]string{"fluid.io/s-big-data-hbase-a": "true"}}},
+	)
+	testcases = append(testcases, struct {
+		name      string
+		fields    fields
+		nodeNames []string
+	}{
+		name:      "noController",
+		fields:    fields3,
+		nodeNames: []string{"node4"},
+	})
+
+	fields4 := makeDatasetResourcesFn("hbase-b", testNodeNamespace, []string{})
+	fields4.nodes = append(fields4.nodes,
+		&v1.Node{ObjectMeta: metav1.ObjectMeta{Name: "node6", Labels: map[string]string{"fluid.io/s-big-data-hbase-b": "true", "fluid.io/s-goosefs-big-data-hbase-b": "true"}}},
+	)
+	testcases = append(testcases, struct {
+		name      string
+		fields    fields
+		nodeNames []string
+	}{
+		name:      "remove",
+		fields:    fields4,
+		nodeNames: []string{},
+	})
 
 	runtimeObjs := []runtime.Object{}
 
 	for _, testcase := range testcases {
 		runtimeObjs = append(runtimeObjs, testcase.fields.worker)
 
-		if testcase.fields.ds != nil {
-			runtimeObjs = append(runtimeObjs, testcase.fields.ds)
-		}
 		for _, pod := range testcase.fields.pods {
 			runtimeObjs = append(runtimeObjs, pod)
 		}
@@ -265,7 +202,6 @@ func TestSyncScheduleInfoToCacheNodes(t *testing.T) {
 		for _, node := range testcase.fields.nodes {
 			runtimeObjs = append(runtimeObjs, node)
 		}
-		// runtimeObjs = append(runtimeObjs, testcase.fields.pods)
 	}
 	c := fake.NewFakeClientWithScheme(testScheme, runtimeObjs...)
 
@@ -273,21 +209,23 @@ func TestSyncScheduleInfoToCacheNodes(t *testing.T) {
 		engine := getTestGooseFSEngineNode(c, testcase.fields.name, testcase.fields.namespace, true)
 		err := engine.SyncScheduleInfoToCacheNodes()
 		if err != nil {
-			t.Errorf("Got error %t.", err)
+			t.Errorf("testcase %s: Got error %v", testcase.name, err)
+			continue
 		}
 
 		nodeList := &v1.NodeList{}
-		datasetLabels, err := labels.Parse(fmt.Sprintf("%s=true", engine.runtimeInfo.GetCommonLabelName()))
-		if err != nil {
-			return
+		datasetLabels, parseErr := labels.Parse(fmt.Sprintf(testNodeLabelSelector, engine.runtimeInfo.GetCommonLabelName()))
+		if parseErr != nil {
+			t.Fatalf("testcase %s: Got error parsing labels: %v", testcase.name, parseErr)
 		}
 
-		err = c.List(context.TODO(), nodeList, &client.ListOptions{
+		listErr := c.List(context.TODO(), nodeList, &client.ListOptions{
 			LabelSelector: datasetLabels,
 		})
 
-		if err != nil {
-			t.Errorf("Got error %t.", err)
+		if listErr != nil {
+			t.Errorf("testcase %s: Got error listing nodes: %v", testcase.name, listErr)
+			continue
 		}
 
 		nodeNames := []string{}
