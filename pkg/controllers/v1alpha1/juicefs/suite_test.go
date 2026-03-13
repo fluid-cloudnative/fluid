019a8249d5abfb462bed09c5f265456e7bdeb72c
Harsh <harshmastic@gmail.com>
2026-03-13 16:45:11 +0530

    test(juicefs): migrate controller tests to Ginkgo v2
    - Update suite_test.go to Ginkgo v2 (remove deprecated Done channel)
    - Add implement_test.go: getRuntime, GetOrCreateEngine, RemoveEngine
    - Add juicefsruntime_controller_test.go: ControllerName, ManagedResource,
      NewRuntimeReconciler, NewCacheOption
    - Coverage: 79.1% (gate >75% PASS)
    
    Signed-off-by: Harsh <harshmastic@gmail.com>

--- pkg/controllers/v1alpha1/juicefs/suite_test.go
+++ pkg/controllers/v1alpha1/juicefs/suite_test.go
@@ -17,63 +17,23 @@ limitations under the License.
 package juicefs
 
 import (
-	"path/filepath"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
-	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
 	"github.com/fluid-cloudnative/fluid/pkg/utils/fake"
-	//+kubebuilder:scaffold:imports
 )
 
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var cfg *rest.Config
-var k8sClient client.Client
-var testEnv *envtest.Environment
-
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
-
-	RunSpecs(t,
-		"Controller Suite")
+	RunSpecs(t, "JuiceFS Controller Suite")
 }
 
-var _ = BeforeSuite(func(done Done) {
+var _ = BeforeSuite(func() {
 	logf.SetLogger(fake.NullLogger())
-
-	By("bootstrapping test environment")
-	testEnv = &envtest.Environment{
-		CRDDirectoryPaths: []string{filepath.Join("..", "config", "crd", "bases")},
-	}
-
-	var err error
-	cfg, err = testEnv.Start()
-	Expect(err).ToNot(HaveOccurred())
-	Expect(cfg).ToNot(BeNil())
-
-	err = datav1alpha1.AddToScheme(scheme.Scheme)
-	Expect(err).NotTo(HaveOccurred())
-
-	//+kubebuilder:scaffold:scheme
-
-	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
-	Expect(err).ToNot(HaveOccurred())
-	Expect(k8sClient).ToNot(BeNil())
-
-	close(done)
-}, 60)
-
-var _ = AfterSuite(func() {
-	By("tearing down the test environment")
-	err := testEnv.Stop()
-	Expect(err).ToNot(HaveOccurred())
 })
