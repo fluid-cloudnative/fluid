2625d292a7a0e55104f97ce7e7d7a860094f7ed4
Harsh <harshmastic@gmail.com>
2026-03-18 20:15:25 +0530

    test(jindo): migrate controller tests to Ginkgo/Gomega
    Migrate pkg/controllers/v1alpha1/jindo/ unit tests to Ginkgo v2/Gomega.
    Fix broken suite bootstrap (Ginkgo v1 Unknown Decorator error) and add
    full coverage for implement.go and jindoruntime_controller.go (86.0%).
    
    13/13 specs pass.
    
    Signed-off-by: Harsh <harshmastic@gmail.com>

---
+++ pkg/controllers/v1alpha1/jindo/controller_test.go
@@ -0,0 +1,153 @@
+/*
+Copyright 2026 The Fluid Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package jindo
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
+	"github.com/fluid-cloudnative/fluid/pkg/utils/fake"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/tools/record"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+var _ = Describe("JindoRuntime Controller", func() {
+	Describe("NewRuntimeReconciler", func() {
+		It("should create a RuntimeReconciler with non-nil fields", func() {
+			s := runtime.NewScheme()
+			_ = datav1alpha1.AddToScheme(s)
+			mockClient := fake.NewFakeClientWithScheme(s)
+			recorder := record.NewFakeRecorder(16)
+
+			r := NewRuntimeReconciler(mockClient, fake.NullLogger(), s, recorder)
+
+			Expect(r).NotTo(BeNil())
+			Expect(r.Scheme).NotTo(BeNil())
+			Expect(r.engines).NotTo(BeNil())
+			Expect(r.mutex).NotTo(BeNil())
+			Expect(r.RuntimeReconciler).NotTo(BeNil())
+		})
+	})
+
+	Describe("ControllerName", func() {
+		It("should return the correct controller name", func() {
+			s := runtime.NewScheme()
+			mockClient := fake.NewFakeClientWithScheme(s)
+			recorder := record.NewFakeRecorder(16)
+
+			r := NewRuntimeReconciler(mockClient, fake.NullLogger(), s, recorder)
+
+			Expect(r.ControllerName()).To(Equal(controllerName))
+		})
+	})
+
+	Describe("ManagedResource", func() {
+		It("should return a JindoRuntime object with correct TypeMeta", func() {
+			s := runtime.NewScheme()
+			mockClient := fake.NewFakeClientWithScheme(s)
+			recorder := record.NewFakeRecorder(16)
+
+			r := NewRuntimeReconciler(mockClient, fake.NullLogger(), s, recorder)
+			obj := r.ManagedResource()
+
+			Expect(obj).NotTo(BeNil())
+			jr, ok := obj.(*datav1alpha1.JindoRuntime)
+			Expect(ok).To(BeTrue())
+			Expect(jr.Kind).To(Equal(datav1alpha1.JindoRuntimeKind))
+			Expect(jr.APIVersion).To(ContainSubstring(datav1alpha1.GroupVersion.Group))
+		})
+
+		It("should populate APIVersion and Kind correctly", func() {
+			s := runtime.NewScheme()
+			mockClient := fake.NewFakeClientWithScheme(s)
+			recorder := record.NewFakeRecorder(16)
+
+			r := NewRuntimeReconciler(mockClient, fake.NullLogger(), s, recorder)
+			obj := r.ManagedResource()
+
+			jr, ok := obj.(*datav1alpha1.JindoRuntime)
+			Expect(ok).To(BeTrue())
+			Expect(jr.TypeMeta).To(Equal(metav1.TypeMeta{
+				Kind:       datav1alpha1.JindoRuntimeKind,
+				APIVersion: datav1alpha1.GroupVersion.Group + "/" + datav1alpha1.GroupVersion.Version,
+			}))
+		})
+	})
+
+	Describe("Reconcile", func() {
+		It("should return empty result when runtime is not found", func() {
+			s := runtime.NewScheme()
+			_ = datav1alpha1.AddToScheme(s)
+			mockClient := fake.NewFakeClientWithScheme(s)
+			recorder := record.NewFakeRecorder(16)
+
+			r := NewRuntimeReconciler(mockClient, fake.NullLogger(), s, recorder)
+
+			req := ctrl.Request{
+				NamespacedName: types.NamespacedName{
+					Name:      "nonexistent-runtime",
+					Namespace: "default",
+				},
+			}
+
+			result, err := r.Reconcile(context.TODO(), req)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(result).To(Equal(ctrl.Result{}))
+		})
+
+		It("should requeue with error when runtime name is invalid DNS label", func() {
+			s := runtime.NewScheme()
+			_ = datav1alpha1.AddToScheme(s)
+
+			// Runtime with invalid DNS name (starts with number) triggers validation
+			invalidName := "20-hbase"
+			jindoRuntime := &datav1alpha1.JindoRuntime{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      invalidName,
+					Namespace: "default",
+				},
+			}
+			dataset := &datav1alpha1.Dataset{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      invalidName,
+					Namespace: "default",
+				},
+			}
+
+			mockClient := fake.NewFakeClientWithScheme(s, jindoRuntime, dataset)
+			recorder := record.NewFakeRecorder(16)
+			r := NewRuntimeReconciler(mockClient, fake.NullLogger(), s, recorder)
+
+			req := ctrl.Request{
+				NamespacedName: types.NamespacedName{
+					Name:      invalidName,
+					Namespace: "default",
+				},
+			}
+
+			_, err := r.Reconcile(context.TODO(), req)
+			// Invalid runtime name causes an error path
+			Expect(err).To(HaveOccurred())
+		})
+	})
+})
