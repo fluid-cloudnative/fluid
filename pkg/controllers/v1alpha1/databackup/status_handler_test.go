3641e71d715e6ca0e7c88394c588e2ba19fbb9f5
Harsh <harshmastic@gmail.com>
2026-03-24 16:53:32 +0530

    test(databackup): migrate controller tests to Ginkgo/Gomega
    - Add suite_test.go with package-level Ginkgo bootstrap (TestAPIs)
    - Migrate status_handler_test.go from stdlib testing to Ginkgo/Gomega
    - Add databackup_controller_test.go: NewDataBackupReconciler, Build, ControllerName
    - Add implement_test.go: covers all dataBackupOperation methods including
      GetOperationObject, GetChartsDirectory, HasPrecedingOperation, Validate,
      UpdateStatusInfoForCompleted, GetTTL, GetStatusHandler, GetTargetDataset,
      UpdateOperationApiStatus, GetOperationType, GetPossibleTargetDatasetNamespacedNames,
      GetReleaseNameSpacedName, GetParallelTaskNumber, SetTargetDatasetStatusInProgress,
      RemoveTargetDatasetStatusInProgress
    
    Coverage: 73.2% -> 77.5% (exceeds 75% gate)
    Signed-off-by: Harsh <harshmastic@gmail.com>

--- pkg/controllers/v1alpha1/databackup/status_handler_test.go
+++ pkg/controllers/v1alpha1/databackup/status_handler_test.go
@@ -1,23 +1,24 @@
 /*
-  Copyright 2023 The Fluid Authors.
+Copyright 2026 The Fluid Authors.
 
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-      http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 package databackup
 
 import (
-	"testing"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 
 	"github.com/fluid-cloudnative/fluid/api/v1alpha1"
 	"github.com/fluid-cloudnative/fluid/pkg/common"
@@ -29,73 +30,156 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 )
 
-func TestOnceGetOperationStatus(t *testing.T) {
-	testScheme := runtime.NewScheme()
-	_ = v1alpha1.AddToScheme(testScheme)
-	_ = corev1.AddToScheme(testScheme)
+var _ = Describe("OnceHandler", func() {
+	var (
+		testScheme     *runtime.Scheme
+		mockDataBackup *v1alpha1.DataBackup
+	)
 
-	mockDataBackup := v1alpha1.DataBackup{
-		ObjectMeta: v1.ObjectMeta{
-			Name:      "test",
-			Namespace: "default",
-		},
-		Spec: v1alpha1.DataBackupSpec{},
-	}
+	BeforeEach(func() {
+		testScheme = runtime.NewScheme()
+		_ = v1alpha1.AddToScheme(testScheme)
+		_ = corev1.AddToScheme(testScheme)
 
-	mockPod := corev1.Pod{
-		ObjectMeta: v1.ObjectMeta{
-			Name:      "test-pod",
-			Namespace: "default",
-		},
-		Status: corev1.PodStatus{
-			Phase: corev1.PodSucceeded,
-		},
-	}
-
-	mockFailedPod := corev1.Pod{
-		ObjectMeta: v1.ObjectMeta{
-			Name:      "test-pod",
-			Namespace: "default",
-		},
-		Status: corev1.PodStatus{
-			Phase: corev1.PodFailed,
-		},
-	}
-
-	testcases := []struct {
-		name          string
-		pod           corev1.Pod
-		expectedPhase common.Phase
-	}{
-		{
-			name:          "job success",
-			pod:           mockPod,
-			expectedPhase: common.PhaseComplete,
-		},
-		{
-			name:          "job failed",
-			pod:           mockFailedPod,
-			expectedPhase: common.PhaseFailed,
-		},
-	}
-
-	for _, testcase := range testcases {
-		client := fake.NewFakeClientWithScheme(testScheme, &mockDataBackup, &testcase.pod)
-		onceStatusHandler := &OnceHandler{dataBackup: &mockDataBackup}
-		ctx := cruntime.ReconcileRequestContext{
-			NamespacedName: types.NamespacedName{
+		mockDataBackup = &v1alpha1.DataBackup{
+			ObjectMeta: v1.ObjectMeta{
+				Name:      "test",
 				Namespace: "default",
-				Name:      "",
 			},
-			Client: client,
-			Log:    fake.NullLogger(),
+			Spec: v1alpha1.DataBackupSpec{},
 		}
-		opStatus, err := onceStatusHandler.GetOperationStatus(ctx, &mockDataBackup.Status)
-		if err != nil {
-			t.Errorf("fail to GetOperationStatus with error %v", err)
-		}
-		if opStatus.Phase != testcase.expectedPhase {
-			t.Error("Failed to GetOperationStatus", "expected phase", testcase.expectedPhase, "get", opStatus.Phase)
-		}
-	}
-}
+	})
+
+	Describe("GetOperationStatus", func() {
+		It("should return PhaseComplete when backup pod succeeded", func() {
+			mockPod := corev1.Pod{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test-pod",
+					Namespace: "default",
+				},
+				Status: corev1.PodStatus{
+					Phase: corev1.PodSucceeded,
+				},
+			}
+			client := fake.NewFakeClientWithScheme(testScheme, mockDataBackup, &mockPod)
+			onceStatusHandler := &OnceHandler{dataBackup: mockDataBackup}
+			ctx := cruntime.ReconcileRequestContext{
+				NamespacedName: types.NamespacedName{
+					Namespace: "default",
+					Name:      "",
+				},
+				Client: client,
+				Log:    fake.NullLogger(),
+			}
+
+			opStatus, err := onceStatusHandler.GetOperationStatus(ctx, &mockDataBackup.Status)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(opStatus.Phase).To(Equal(common.PhaseComplete))
+		})
+
+		It("should return PhaseFailed when backup pod failed", func() {
+			mockFailedPod := corev1.Pod{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test-pod",
+					Namespace: "default",
+				},
+				Status: corev1.PodStatus{
+					Phase: corev1.PodFailed,
+				},
+			}
+			client := fake.NewFakeClientWithScheme(testScheme, mockDataBackup, &mockFailedPod)
+			onceStatusHandler := &OnceHandler{dataBackup: mockDataBackup}
+			ctx := cruntime.ReconcileRequestContext{
+				NamespacedName: types.NamespacedName{
+					Namespace: "default",
+					Name:      "",
+				},
+				Client: client,
+				Log:    fake.NullLogger(),
+			}
+
+			opStatus, err := onceStatusHandler.GetOperationStatus(ctx, &mockDataBackup.Status)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(opStatus.Phase).To(Equal(common.PhaseFailed))
+		})
+
+		It("should return unchanged status when backup pod is still running", func() {
+			runningPod := corev1.Pod{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test-pod",
+					Namespace: "default",
+				},
+				Status: corev1.PodStatus{
+					Phase: corev1.PodRunning,
+				},
+			}
+			client := fake.NewFakeClientWithScheme(testScheme, mockDataBackup, &runningPod)
+			onceStatusHandler := &OnceHandler{dataBackup: mockDataBackup}
+			ctx := cruntime.ReconcileRequestContext{
+				NamespacedName: types.NamespacedName{
+					Namespace: "default",
+					Name:      "",
+				},
+				Client: client,
+				Log:    fake.NullLogger(),
+			}
+
+			opStatus, err := onceStatusHandler.GetOperationStatus(ctx, &mockDataBackup.Status)
+			Expect(err).NotTo(HaveOccurred())
+			// Pod is still running, so status should not change phase
+			Expect(opStatus.Phase).To(Equal(mockDataBackup.Status.Phase))
+		})
+
+		It("should return unchanged status when backup pod does not exist", func() {
+			// No pod in the fake client: GetPodByName returns (nil, nil) for not-found
+			// IsFinishedPod(nil) returns false, so status is returned unchanged
+			client := fake.NewFakeClientWithScheme(testScheme, mockDataBackup)
+			onceStatusHandler := &OnceHandler{dataBackup: mockDataBackup}
+			ctx := cruntime.ReconcileRequestContext{
+				NamespacedName: types.NamespacedName{
+					Namespace: "default",
+					Name:      "",
+				},
+				Client: client,
+				Log:    fake.NullLogger(),
+			}
+
+			opStatus, err := onceStatusHandler.GetOperationStatus(ctx, &mockDataBackup.Status)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(opStatus.Phase).To(Equal(mockDataBackup.Status.Phase))
+		})
+
+		It("should use conditions LastTransitionTime when conditions are set on succeeded pod", func() {
+			conditionTime := v1.Now()
+			mockPodWithConditions := corev1.Pod{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test-pod",
+					Namespace: "default",
+				},
+				Status: corev1.PodStatus{
+					Phase: corev1.PodSucceeded,
+					Conditions: []corev1.PodCondition{
+						{
+							LastTransitionTime: conditionTime,
+						},
+					},
+				},
+			}
+			client := fake.NewFakeClientWithScheme(testScheme, mockDataBackup, &mockPodWithConditions)
+			onceStatusHandler := &OnceHandler{dataBackup: mockDataBackup}
+			ctx := cruntime.ReconcileRequestContext{
+				NamespacedName: types.NamespacedName{
+					Namespace: "default",
+					Name:      "",
+				},
+				Client: client,
+				Log:    fake.NullLogger(),
+			}
+
+			opStatus, err := onceStatusHandler.GetOperationStatus(ctx, &mockDataBackup.Status)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(opStatus.Phase).To(Equal(common.PhaseComplete))
+			Expect(opStatus.Conditions).To(HaveLen(1))
+		})
+	})
+})
