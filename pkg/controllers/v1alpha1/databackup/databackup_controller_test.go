58feb8207e61b10d733b6915168325ffd0495892
harsh kumar <135993950+hxrshxz@users.noreply.github.com>
2026-03-30 07:58:11 +0530

    test(databackup): migrate controller tests to Ginkgo/Gomega (#5713)
    * test(databackup): migrate controller tests to Ginkgo/Gomega
    
    - Add suite_test.go with package-level Ginkgo bootstrap (TestAPIs)
    - Migrate status_handler_test.go from stdlib testing to Ginkgo/Gomega
    - Add databackup_controller_test.go: NewDataBackupReconciler, Build, ControllerName
    - Add implement_test.go: covers all dataBackupOperation methods including
      GetOperationObject, GetChartsDirectory, HasPrecedingOperation, Validate,
      UpdateStatusInfoForCompleted, GetTTL, GetStatusHandler, GetTargetDataset,
      UpdateOperationApiStatus, GetOperationType, GetPossibleTargetDatasetNamespacedNames,
      GetReleaseNameSpacedName, GetParallelTaskNumber, SetTargetDatasetStatusInProgress,
      RemoveTargetDatasetStatusInProgress
    
    Coverage: 73.2% -> 77.5% (exceeds 75% gate)
    Signed-off-by: Harsh <harshmastic@gmail.com>
    
    * test(databackup): assert LastTransitionTime in status handler test
    
    Strengthen the 'should use conditions LastTransitionTime' test to
    actually verify the transition time value, not just the condition
    count. Uses BeTemporally to handle metav1.Time second-precision
    truncation.
    
    Signed-off-by: Harsh <harshmastic@gmail.com>
    
    ---------
    
    Signed-off-by: Harsh <harshmastic@gmail.com>

--- pkg/controllers/v1alpha1/databackup/databackup_controller_test.go
+++ pkg/controllers/v1alpha1/databackup/databackup_controller_test.go
@@ -1,26 +1,83 @@
 /*
-Copyright 2023 The Fluid Authors.
+Copyright 2026 The Fluid Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-
 */
 
 package databackup
 
 import (
-	"testing"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/record"
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	datav1alpha1 "github.com/fluid-cloudnative/fluid/api/v1alpha1"
+	"github.com/fluid-cloudnative/fluid/pkg/utils/fake"
 )
 
-func TestAPIs(t *testing.T) {
+var _ = Describe("DataBackupReconciler", func() {
 
-}
+	Describe("ControllerName", func() {
+		It("should return the constant controller name", func() {
+			r := &DataBackupReconciler{}
+			Expect(r.ControllerName()).To(Equal(controllerName))
+		})
+	})
+
+	Describe("NewDataBackupReconciler", func() {
+		It("should initialize reconciler with all required fields set", func() {
+			s := runtime.NewScheme()
+			_ = datav1alpha1.AddToScheme(s)
+			fakeClient := fake.NewFakeClientWithScheme(s)
+			log := ctrl.Log.WithName("test")
+			recorder := record.NewFakeRecorder(10)
+
+			r := NewDataBackupReconciler(fakeClient, log, s, recorder)
+			Expect(r).NotTo(BeNil())
+			Expect(r.Scheme).To(Equal(s))
+			Expect(r.OperationReconciler).NotTo(BeNil())
+		})
+	})
+
+	Describe("Build", func() {
+		It("should return a dataBackupOperation for a valid DataBackup object", func() {
+			s := runtime.NewScheme()
+			_ = datav1alpha1.AddToScheme(s)
+			fakeClient := fake.NewFakeClientWithScheme(s)
+			log := ctrl.Log.WithName("test")
+			recorder := record.NewFakeRecorder(10)
+			r := NewDataBackupReconciler(fakeClient, log, s, recorder)
+
+			dataBackup := &datav1alpha1.DataBackup{}
+			op, err := r.Build(dataBackup)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(op).NotTo(BeNil())
+		})
+
+		It("should return an error for a non-DataBackup object", func() {
+			s := runtime.NewScheme()
+			fakeClient := fake.NewFakeClientWithScheme(s)
+			log := ctrl.Log.WithName("test")
+			recorder := record.NewFakeRecorder(10)
+			r := NewDataBackupReconciler(fakeClient, log, s, recorder)
+
+			dataset := &datav1alpha1.Dataset{}
+			op, err := r.Build(dataset)
+			Expect(err).To(HaveOccurred())
+			Expect(op).To(BeNil())
+		})
+	})
+})
---
+++ pkg/controllers/v1alpha1/databackup/suite_test.go
@@ -0,0 +1,36 @@
+/*
+Copyright 2026 The Fluid Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package databackup
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+
+	"github.com/fluid-cloudnative/fluid/pkg/utils/fake"
+)
+
+func TestAPIs(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "DataBackup Controller Suite")
+}
+
+var _ = BeforeSuite(func() {
+	logf.SetLogger(fake.NullLogger())
+})
