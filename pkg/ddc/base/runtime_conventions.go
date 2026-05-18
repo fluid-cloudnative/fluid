14a786fe49b3975f75ba38363f28536122d77bb4
Su Feng <3505543807@qq.com>
2026-05-18 10:13:42 +0800

    Add Notation to GetWorkerStatefulsetName in pkg/ddc/base/runtime_conventions.go. (#5884)
    Signed-off-by: Hilbert <3505543807@qq.com>

--- pkg/ddc/base/runtime_conventions.go
+++ pkg/ddc/base/runtime_conventions.go
@@ -4,6 +4,15 @@ import (
 	"github.com/fluid-cloudnative/fluid/pkg/common"
 )
 
+// GetWorkerStatefulsetName returns the worker StatefulSet name for the runtime.
+// This function determines the name of the worker StatefulSet based on the runtime type.
+// Jindo-related runtimes use the "-jindofs-worker" suffix, while other runtimes use the "-worker" suffix.
+//
+// Parameters:
+//   - None: This function does not take any parameters.
+//
+// Returns:
+//   - string: The generated worker StatefulSet name for the current runtime.
 func (info *RuntimeInfo) GetWorkerStatefulsetName() string {
 	switch info.runtimeType {
 	// JindoRuntime has a extra "-jindofs" suffix in worker statefulset name.
