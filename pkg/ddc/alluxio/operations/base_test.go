8ab539e0ed61aa0a1e83e7844f675b75cfbcf82d
Zree <82323521+Rain-Swift@users.noreply.github.com>
2026-06-01 11:50:57 +0800

    Add comments to function  TestAlluxioFileUtils_ExecMountScripts in pkg/ddc/alluxio/operations/base_test.go (#5893)
    Signed-off-by: Zhirui <c123zr@gmail.com>

--- pkg/ddc/alluxio/operations/base_test.go
+++ pkg/ddc/alluxio/operations/base_test.go
@@ -677,6 +677,7 @@ func TestAlluxioFileUtils_MasterPodName(t *testing.T) {
 }
 
 func TestAlluxioFileUtils_ExecMountScripts(t *testing.T) {
+	// Mock exec to avoid invoking the mounted script during the unit test.
 	ExecCommon := func(command []string, verbose bool) (stdout string, stderr string, err error) {
 		return strings.Join(command, " "), "", nil
 	}
@@ -687,6 +688,7 @@ func TestAlluxioFileUtils_ExecMountScripts(t *testing.T) {
 	a := &AlluxioFileUtils{log: fake.NullLogger()}
 	patch1 := gomonkey.ApplyPrivateMethod(*a, "exec", ExecErr)
 
+	// ExecMountScripts should return the error reported by exec.
 	err := a.ExecMountScripts()
 	if err == nil {
 		t.Error("check failure, want err, got nil")
@@ -694,6 +696,7 @@ func TestAlluxioFileUtils_ExecMountScripts(t *testing.T) {
 	patch1.Reset()
 
 	patch2 := gomonkey.ApplyPrivateMethod(*a, "exec", ExecCommon)
+	// ExecMountScripts should complete without error when exec succeeds.
 	err = a.ExecMountScripts()
 	if err != nil {
 		t.Errorf("check failure, want nil, got err: %v", err)
