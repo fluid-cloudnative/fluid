de66ec356234eb53c093b5e2d3f9ba94267279a3
东伝 <dongyun.xzh@alibaba-inc.com>
2026-06-08 18:50:18 +0800

    fix(e2e-test): load cacheruntime image to kind cluster
    Signed-off-by: 东伝 <dongyun.xzh@alibaba-inc.com>

--- .github/scripts/build-all-images.sh
+++ .github/scripts/build-all-images.sh
@@ -16,9 +16,8 @@ build_images() {
         ${IMG_REPO}/alluxioruntime-controller:${IMAGE_TAG}
         ${IMG_REPO}/jindoruntime-controller:${IMAGE_TAG}
         ${IMG_REPO}/juicefsruntime-controller:${IMAGE_TAG}
+        ${IMG_REPO}/cacheruntime-controller:${IMAGE_TAG}
         ${IMG_REPO}/thinruntime-controller:${IMAGE_TAG}
-        ${IMG_REPO}/efcruntime-controller:${IMAGE_TAG}
-        ${IMG_REPO}/vineyardruntime-controller:${IMAGE_TAG}
         ${IMG_REPO}/fluid-csi:${IMAGE_TAG}
         ${IMG_REPO}/fluid-webhook:${IMAGE_TAG}
         ${IMG_REPO}/fluid-crd-upgrader:${IMAGE_TAG}
