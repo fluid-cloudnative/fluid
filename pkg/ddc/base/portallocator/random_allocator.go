7a6445579565d529495f7a985a5285fbf2c36f70
sssq-max <3203599225@qq.com>
2026-06-20 19:07:20 +0800

    Update comment for Allocate in random_allocator.go.
    Signed-off-by: sssq-max <3203599225@qq.com>

--- pkg/ddc/base/portallocator/random_allocator.go
+++ pkg/ddc/base/portallocator/random_allocator.go
@@ -48,7 +48,8 @@ func newRandomAllocator(pr *net.PortRange, log logr.Logger) (*RandomAllocator, e
 	}, nil
 }
 
-// Allocate accepts the given port without performing allocation checks.
+// Allocate is a no-op for RandomAllocator.
+// Individual port allocation checks are not currently required by this allocator.
 func (r *RandomAllocator) Allocate(port int) error {
 	// not judge whether port can be allocated or not
 	return nil
